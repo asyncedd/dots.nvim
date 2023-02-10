c55af48b9d860baed38417652c05d5a8d4eb4796
asyncedd <i.sync@pissmail.com>
2023-02-10 18:32:56 +0900

    Fix error in lua.setup.options

--- lua/options/opts.lua
+++ lua/options/opts.lua
@@ -1,19 +1,13 @@
-local o = vim.opt
-local g = vim.g
-
 local M = {
   -- General
-
-  -- Set the leader key to <Space>
-  g.mapleader = " ",
   -- Set the shiftwidth to 2
-  o.shiftwidth = 2,
-  o.number = true, -- Enable line numbers
-  o.undofile = true, -- Enable persistent undo's
+  shiftwidth = 2,
+  number = true, -- Enable line numbers
+  undofile = true, -- Enable persistent undo's
   -- Don't store backup when overwriting the file,
-  o.backup = false,
-  o.writebackup = false,
-  o.mouse = "a", -- Enable mouse for all modes
+  backup = false,
+  writebackup = false,
+  mouse = "a", -- Enable mouse for all modes
   -- Apperance
 }
 
