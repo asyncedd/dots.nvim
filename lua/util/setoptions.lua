2b5270b6398c437c2f4c76510b361caa91650cdf
asyncedd <i.sync@pissmail.com>
2023-02-11 22:33:56 +0900

    remove my 1 hr of work

--- lua/util/setoptions.lua
+++
@@ -1,9 +0,0 @@
-local function set_vim_options(options)
-  for _, opt in ipairs(options) do
-    assert(type(opt.name) == "string", "Option name must be a string")
-    assert(type(opt.value) == "string" or type(opt.value) == "number" or type(opt.value) == "boolean", "Option value must be a string, number, or boolean")
-    opt.modifier[opt.name] = opt.value
-  end
-end
-
-return set_vim_options
