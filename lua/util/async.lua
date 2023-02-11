092f0c156f52bc8e62a7ceb6a990060cf1b3b9f5
asyncedd <i.sync@pissmail.com>
2023-02-11 17:38:23 +0900

    remove async

--- lua/util/async.lua
+++ lua/util/async.lua
@@ -1,13 +1,5 @@
 return function(opts)
-  local function set_options(opts)
-    for k, v in pairs(opts) do
-      vim.o[k] = v
-    end
+  for k, v in pairs(opts) do
+    vim.o[k] = v
   end
-
-  local co = coroutine.create(function()
-    set_options(opts)
-  end)
-
-  coroutine.resume(co)
 end
