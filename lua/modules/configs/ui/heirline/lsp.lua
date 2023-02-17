09ac5495b627fe6f081035bd75a23b6e05974dcf
asyncedd <i.sync@pissmail.com>
2023-02-18 08:59:38 +0900

    📝 remapping mappings

--- lua/modules/configs/ui/heirline/lsp.lua
+++ lua/modules/configs/ui/heirline/lsp.lua
@@ -1,21 +1,24 @@
 local conditions = require("heirline.conditions")
+local utils = require("heirline.utils")
 
 local LSPActive = {
-    condition = conditions.lsp_attached,
-    update = {'LspAttach', 'LspDetach'},
+  condition = conditions.lsp_attached,
+  update = {'LspAttach', 'LspDetach'},
 
-    -- You can keep it simple,
-    -- provider = " [LSP]",
+  -- You can keep it simple,
+  -- provider = " [LSP]",
 
-    -- Or complicate things a bit and get the servers names
-    provider  = function()
-        local names = {}
-        for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
-            table.insert(names, server.name)
-        end
-        return " [" .. table.concat(names, " ") .. "]"
-    end,
-    hl = { fg = "green", bold = true },
+  -- Or complicate things a bit and get the servers names
+  provider  = function()
+    local names = {}
+    for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+      table.insert(names, server.name)
+    end
+    return " [" .. table.concat(names, " ") .. "]"
+  end,
+  hl = { fg = "green", bold = true },
 }
+
+-- utils.surround({ "", "" }, "bright_bg", { LSPActive })
 
 return LSPActive
