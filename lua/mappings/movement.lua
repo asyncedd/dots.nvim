40fa32668433049a04f6e26f303445071dd7a864
asyncedd <i.sync@pissmail.com>
2023-02-24 19:00:03 +0900

    Initalize mappings.movement

---
+++ lua/mappings/movement.lua
@@ -0,0 +1,5 @@
+local map = vim.api.nvim_set_keymap
+
+-- Move up/down on VISUAL line instead of a ACTUAL line
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
