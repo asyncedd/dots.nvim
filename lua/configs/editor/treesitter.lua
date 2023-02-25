1c6f0b2028c30ef900a7217c976b685a17512550
asyncedd <i.sync@pissmail.com>
2023-02-25 09:50:00 +0900

    modified:   lua/configs/editor/treesitter.lua

--- lua/configs/editor/treesitter.lua
+++ lua/configs/editor/treesitter.lua
@@ -1,6 +1,6 @@
 return function()
-  local status, tressitter = pcall(require, "nvim-treesitter.configs")
-  if (not stauts) then return end
+  local status, treesitter = pcall(require, "nvim-treesitter.configs")
+  if not stauts then return end
 
   treesitter.setup({
     auto_install = true, -- Auto-install missing parsers. (once you open a file that requires it.)
