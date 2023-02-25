1bee58391010d04b8eb1079a8ec6ec9e2048cc39
asyncedd <i.sync@pissmail.com>
2023-02-25 12:00:08 +0900

    modified:   lazy-lock.json 	modified:   lua/configs/ui/animate.lua 	new file:   lua/plugins/animate.lua

---
+++ lua/plugins/animate.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    config = require("configs.ui.animate"),
+    dependencies = {
+      { "tamton-aquib/flirt.nvim" }
+    }
+  }
+}
