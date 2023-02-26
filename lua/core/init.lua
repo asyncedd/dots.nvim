c55fdcfd841a7a744976c6a9aa73d244596bc308
Async <a@sync>
2023-02-26 15:31:41 +0900

    modified:   lua/configs/editor/treesitter.lua 	modified:   lua/core/init.lua 	modified:   lua/core/load_ui_plugins.lua 	modified:   lua/core/options.lua 	modified:   lua/plugins/colorscheme.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,10 +1,13 @@
+local vim = vim -- Avoid expensive global searching.
+
 local M = {}
 
 M.setup = function()
-  require("core.options")
+  vim.g.mapleader = " "
   require("core.lazy")
   require("core.load_ui_plugins")
   vim.defer_fn(function ()
+    require("core.options")
     -- simulate some heavy plugin loading
     require("mappings.movement")
   end, 0)
