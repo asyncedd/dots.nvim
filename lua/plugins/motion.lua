25fbd52e61e358e27b4e5ade7a6842f6568f569e
Async <a@sync>
2023-02-26 15:18:03 +0900

    modified:   lazy-lock.json 	modified:   lua/core/init.lua 	new file:   lua/core/load_ui_plugins.lua 	modified:   lua/plugins/animate.lua 	modified:   lua/plugins/editor.lua 	modified:   lua/plugins/motion.lua 	modified:   lua/plugins/treesitter.lua 	modified:   lua/plugins/ui.lua 	deleted:    lua/plugins/utils.lua

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -1,13 +1,11 @@
 return {
   {
     "ggandor/leap.nvim",
-    event = "VeryLazy",
     dependencies = {
       { "ggandor/flit.nvim" },
       { "ggandor/leap-spooky.nvim" },
       { "tpope/vim-repeat" },
       { "echasnovski/mini.ai" },
     },
-    config = require("configs.editor.motion")
   }
 }
