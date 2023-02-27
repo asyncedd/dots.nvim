193a2d7ebb16cfe05b30d873e1cc82968e05c135
Async <a@sync>
2023-02-27 17:43:03 +0900

    modified:   lazy-lock.json new file:   lua/configs/ui/heirline/colors.lua new file:   lua/configs/ui/heirline/filename.lua new file:   lua/configs/ui/heirline/init.lua new file:   lua/configs/ui/heirline/vimode.lua modified:   lua/configs/ui/indent.lua modified:   lua/core/load_ui_plugins.lua modified:   lua/core/options.lua new file:   lua/plugins/core.lua new file:   lua/plugins/heirline.lua

--- lua/configs/ui/indent.lua
+++ lua/configs/ui/indent.lua
@@ -6,6 +6,8 @@ indent_blankline.setup({
   -- show_current_context_start = true,
   show_end_of_line = true,
   space_char_blankline = " ",
+  enabled = true,
+  colored_indent_levels = false,
   -- show_trailing_blankline_indent = true,
 })
 
