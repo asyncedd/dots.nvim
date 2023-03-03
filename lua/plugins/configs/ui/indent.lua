614e1776d0ea21f5e90e047cd14193185ea4d487
Async <i.sync@pissmail.com>
2023-03-03 15:53:18 +0900

    modified:   lua/core/pluginLoader/other_plugins.lua modified:   lua/core/pluginLoader/ui_plugins.lua renamed:    lua/configs/editor/comment.lua -> lua/plugins/configs/editor/comment.lua renamed:    lua/configs/editor/gitsigns.lua -> lua/plugins/configs/editor/gitsigns.lua renamed:    lua/configs/editor/motion.lua -> lua/plugins/configs/editor/motion.lua renamed:    lua/configs/editor/treesitter.lua -> lua/plugins/configs/editor/treesitter.lua renamed:    lua/configs/editor/ufo.lua -> lua/plugins/configs/editor/ufo.lua renamed:    lua/configs/ui/heirline/colors.lua -> lua/plugins/configs/ui/heirline/colors.lua renamed:    lua/configs/ui/heirline/file.lua -> lua/plugins/configs/ui/heirline/file.lua renamed:    lua/configs/ui/heirline/filename.lua -> lua/plugins/configs/ui/heirline/filename.lua renamed:    lua/configs/ui/heirline/git.lua -> lua/plugins/configs/ui/heirline/git.lua renamed:    lua/configs/ui/heirline/init.lua -> lua/plugins/configs/ui/heirline/init.lua renamed:    lua/configs/ui/heirline/scrollbar.lua -> lua/plugins/configs/ui/heirline/scrollbar.lua renamed:    lua/configs/ui/heirline/vimode.lua -> lua/plugins/configs/ui/heirline/vimode.lua renamed:    lua/configs/ui/indent.lua -> lua/plugins/configs/ui/indent.lua renamed:    lua/configs/ui/matcher.lua -> lua/plugins/configs/ui/matcher.lua new file:   lua/plugins/spec/editor.lua

--- lua/plugins/configs/ui/indent.lua
+++ lua/plugins/configs/ui/indent.lua
@@ -2,15 +2,11 @@ local status, indent_blankline = pcall(require, "indent_blankline")
 if not status then return end
 
 indent_blankline.setup({
-  -- show_current_context = true,
-  -- show_current_context_start = true,
+  show_current_context = true,
+  show_current_context_start = true,
   show_end_of_line = true,
   space_char_blankline = " ",
   enabled = true,
   colored_indent_levels = false,
-  -- show_trailing_blankline_indent = true,
-})
-
-require("mini.indentscope").setup({
-  symbol = "│",
+  show_trailing_blankline_indent = true,
 })
