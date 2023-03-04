00e3ccb37aae894180dd9d0c78532a81226ee51e
Async <i.sync@pissmail.com>
2023-03-04 17:09:41 +0900

    deleted:    lua/plugins/configs/editor/comment.lua deleted:    lua/plugins/configs/editor/gitsigns.lua modified:   lua/plugins/configs/editor/ufo.lua modified:   lua/plugins/spec/editor.lua

--- lua/plugins/spec/editor.lua
+++ lua/plugins/spec/editor.lua
@@ -1,12 +1,25 @@
 return {
   {
     "numToStr/Comment.nvim",
-    config = require("plugins.configs.editor.comment"),
+    config = true,
     event = "VeryLazy",
   },
   {
     "lewis6991/gitsigns.nvim",
-    config = require("plugins.configs.editor.gitsigns"),
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    },
+    config = true,
     event = "BufReadPost",
   },
   {
