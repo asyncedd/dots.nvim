31d0bd242e9c58fd0a13682dfbd59f4993ad8bbb
Async <i.sync@pissmail.com>
2023-03-04 12:23:39 +0900

    modified:   lua/core/options.lua modified:   lua/plugins/configs/ui/heirline/bufferline.lua modified:   lua/plugins/configs/ui/heirline/init.lua new file:   lua/plugins/configs/ui/heirline/tablineoffset.lua new file:   lua/plugins/configs/ui/heirline/tablinepicker.lua new file:   lua/plugins/configs/ui/heirline/tablist.lua

---
+++ lua/plugins/configs/ui/heirline/tablinepicker.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+M.TablinePicker = {
+  condition = function(self)
+    return self._show_picker
+  end,
+  init = function(self)
+    local bufname = vim.api.nvim_buf_get_name(self.bufnr)
+    bufname = vim.fn.fnamemodify(bufname, ":t")
+    local label = bufname:sub(1, 1)
+    local i = 2
+    while self._picker_labels[label] do
+      if i > #bufname then
+        break
+      end
+      label = bufname:sub(i, i)
+      i = i + 1
+    end
+    self._picker_labels[label] = self.bufnr
+    self.label = label
+  end,
+  provider = function(self)
+    return self.label
+  end,
+  hl = { fg = "red", bold = true },
+}
+
+vim.keymap.set("n", "gbp", function()
+    local tabline = require("heirline").tabline
+    local buflist = tabline._buflist[1]
+    buflist._picker_labels = {}
+    buflist._show_picker = true
+    vim.cmd.redrawtabline()
+    local char = vim.fn.getcharstr()
+    local bufnr = buflist._picker_labels[char]
+    if bufnr then
+        vim.api.nvim_win_set_buf(0, bufnr)
+    end
+    buflist._show_picker = false
+    vim.cmd.redrawtabline()
+end)
+
+return M
