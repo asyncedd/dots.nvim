ddec339ea28760c76da4ecb1116c761d548efc85
Async <i.sync@pissmail.com>
2023-03-04 16:32:05 +0900

    modified:   init.lua modified:   lua/core/init.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -3,20 +3,18 @@ local o = vim.api.nvim_set_option_value
 
 local M = {}
 
-M.setup = function()
-  vim.g.mapleader = " "
-  o("number", true, {}) -- Enable Line numbers
-  o("cursorline", true, {})
-  o("list", true, {})
-  o("listchars", "eol:↴,space:⋅", {})
-  o("termguicolors", true, {})
+vim.g.mapleader = " "
+o("number", true, {}) -- Enable Line numbers
+o("cursorline", true, {})
+o("list", true, {})
+o("listchars", "eol:↴,space:⋅", {})
+o("termguicolors", true, {})
 
-  require("core.lazy")
-  require("core.options")
-  vim.api.nvim_command("colorscheme catppuccin")
-  require("mappings.movement")
-  require("mappings.lspsaga")
-  require("core.autocmd")
-end
+require("core.lazy")
+require("core.options")
+vim.api.nvim_command("colorscheme catppuccin")
+require("mappings.movement")
+require("mappings.lspsaga")
+require("core.autocmd")
 
 return M
