85a79d6fd959d52771bfe77ba4c476a49e91bfb3
Async <i.sync@pissmail.com>
2023-03-11 14:12:24 +0900

    modified:   lua/core/init.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,32 +1,36 @@
+-- lua/core/init.lua
+-- This is the place where the magic happens!
+
 -- Set the <leader> key to: <Space>
 -- So we can go for ex. -> `<leader>ff` to open Telescope.nvim.
 -- Quite useful ain't it? :)
 vim.g.mapleader = " "
 
 -- Set some options.
--- I tried setting all the options there so we can make it nice and clean :)
+-- Now, we can use shortcuts like `<leader>ff` to open Telescope.nvim.
+-- This will make out lives much easier! :D
 require("core.options")
 
--- Initalize lazy.nvim. The plugin manager we're using.
+-- Initalize the plugin manager, lazy.nvim.
+-- We're gonna use the best tools for the job!
 require("core.lazy")
 
--- Also we're going to load some plugins
--- Ofcourse we can go ahead and do that in out plugins spec (via core.lazy)
--- But, I want that find control so here we go!
+-- Let's load some plugins!
+-- We could do this in the plugins spec, but let's take the finest control!
 require("plugins.loader")
 
--- Ofcourse, setting our beautiful `catppuccin` colorscheme.
--- It's clean and cute! (CC)
--- Plus it's customizable. (CCC)
--- So here it is!
-vim.api.nvim_command("colorscheme catppuccin")
+-- Time to set out beautiful `catppuccin` colorscheme!
+-- It's Clean, cute and customizable!
+-- Perfect for our programming lives! As we need the highest quality coffee!
+vim.cmd("colorscheme catppuccin")
 
 -- Do some stuff that aren't required in our UI. (mappings, autocmd etc...)
--- Also without blocking the main thread.
+-- Also without blocking the main thread for the maximum efficiency.
 vim.schedule(function()
-  -- Add some autocmds. You can checkit out!
+  -- Add some autocmds.
+  -- Check it out, it's quite neat!
   require("core.autocmd")
-  -- Add some mappings...
+  -- Add some mappings... We need these more maximum productivity!
   -- TODO merge `mappings.lspsaga` with `mappings.plugins`
   require("mappings.movement")
   require("mappings.lspsaga")
