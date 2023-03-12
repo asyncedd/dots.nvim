7003c46fae72bf77a2451f2934ef3e0e1513f658
Async <i.sync@pissmail.com>
2023-03-12 11:36:05 +0900

    (Add utils.icons) Add icons for modularized configurarion. new file:   lua/core/utils/icons.lua

---
+++ lua/core/utils/icons.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+M.diagnostics = {
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " ",
+}
+
+M.kind_icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = ""
+}
+
+return M
