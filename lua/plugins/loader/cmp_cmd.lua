b37855b14ce8c1931575d1501a3d5b59bd02c88c
Async <i.sync@pissmail.com>
2023-03-12 10:14:40 +0900

    modified:   init.lua modified:   lua/plugins/loader/cmp_cmd.lua modified:   lua/plugins/loader/editor.lua modified:   lua/plugins/loader/init.lua

--- lua/plugins/loader/cmp_cmd.lua
+++ lua/plugins/loader/cmp_cmd.lua
@@ -1,3 +1,12 @@
-vim.defer_fn(function()
-  require("plugins.configs.completion.cmpcmd")
-end, 0)
+-- Creating autocmds has a slight overhead so, we avoid that overead via vim.schedule.
+-- Here is TJ explaining what it does: https://www.youtube.com/watch?v=GMS0JvS7W1Y
+vim.schedule(function()
+  -- Create a autocmd that will be executed when entering the cmdline.
+  vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
+    callback  = function ()
+      -- Setup the completion for only the cmdline completion in contrast to Neovim's builtin omnifunc.
+      require("plugins.configs.completion.cmpcmd")
+    end
+  })
+end)
+
