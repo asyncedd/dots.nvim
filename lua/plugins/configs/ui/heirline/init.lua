9a7c7bf57b3b7485b43e5f14c6970c5b042c7f0f
Async <i.sync@pissmail.com>
2023-03-12 20:02:55 +0900

    modified:   lua/plugins/configs/ui/heirline/init.lua

--- lua/plugins/configs/ui/heirline/init.lua
+++ lua/plugins/configs/ui/heirline/init.lua
@@ -1,92 +1,15 @@
 local lazy_require = require("snips.lazy-require").require_on_index
 local heirline = lazy_require("heirline")
-local conditions = lazy_require("heirline.conditions")
-local utils = lazy_require("heirline.utils")
-local ViMode = require("plugins.configs.ui.heirline.vimode")
--- Unfortunately lazy_requiring ViMode doesn't work ;-;
-local FileName = lazy_require("plugins.configs.ui.heirline.filename")
-local Gitsigns = require("plugins.configs.ui.heirline.git")
-local Scrollbar = lazy_require("plugins.configs.ui.heirline.scrollbar")
-local File = lazy_require("plugins.configs.ui.heirline.file")
-local LSP = lazy_require("plugins.configs.ui.heirline.lsp")
-local Lazy = lazy_require("plugins.configs.ui.heirline.lazy")
-local SearchResults = require("plugins.configs.ui.heirline.search")
 local Bufferline = lazy_require("plugins.configs.ui.heirline.bufferline")
+local StatusLines = require("plugins.configs.ui.heirline.statusline")
 -- local StatusColumn = require("plugins.configs.ui.heirline.statuscolumn")
 
 require("plugins.configs.ui.heirline.colors")
-
-local Align = { provider = "%=", hl = { bg = "normal" } }
-local Space = { provider = " ", hl = { bg = "normal" } }
-
-local surround = function (color, item)
-  return utils.surround({ "", "" }, color, item)
-end
-
-local DefaultStatusLine = {
-  Lazy, Space, ViMode, Space, SearchResults,
-  surround("bright_bg", {
-    FileName.WorkDir,
-    surround(function(self) return self:mode_color() end, { FileName.FileNameBlock }),
-  }),
-  Space,
-  {
-    Gitsigns,
-    hl = { bg = "normal" }
-  },
-  Space,
-  {
-    LSP.LSPActive,
-    hl = { bg = "normal" }
-  },
-  Space,
-  {
-    LSP.Diagnostics,
-    hl = { bg = "normal" }
-  },
-  Align,
-  { { File.FileType, Space, Scrollbar.Ruler, Space, Scrollbar.ScrollBar }, hl = { bg = 'normal' } },
-}
 
 local bufferline = {
   require("plugins.configs.ui.heirline.tablineoffset"),
   Bufferline,
   require("plugins.configs.ui.heirline.tablist").Tabpages,
-}
-
-local StatusLines = {
-
-  hl = function()
-    return conditions.is_active() and "StatusLine" or "StatusLineNC"
-  end,
-
-  static = {
-    mode_colors_map = {
-      n = "red",
-      i = "green",
-      v = "cyan",
-      V ="cyan",
-      ["\22"] = "cyan",
-      c = "orange",
-      s = "purple",
-      S = "purple",
-      ["\19"] = "purple",
-      R = "orange",
-      r = "orange",
-      ["!"] = "red",
-      t = "green",
-    },
-    mode_color = function(self)
-      local mode = conditions.is_active() and vim.fn.mode() or "n"
-      return self.mode_colors_map[mode]
-    end,
-  },
-
-  -- the first statusline with no condition, or which condition returns true is used.
-  -- think of it as a switch case with breaks to stop fallthrough.
-  fallthrough = false,
-
-  DefaultStatusLine,
 }
 
 -- local statuscolumn = {
