8e8d8c45d085a499228346b7098c7b8a5acc6892
Async <i.sync@pissmail.com>
2023-03-14 21:51:54 +0900

    modified:   lua/plugins/configs/completion/cmp.lua deleted:    lua/plugins/configs/completion/cmpcmd.lua deleted:    lua/plugins/configs/completion/luasnip.lua modified:   lua/plugins/specs/lsp.lua

--- lua/plugins/configs/completion/luasnip.lua
+++
@@ -1,9 +0,0 @@
-local status, luasnip = pcall(require, "luasnip")
-if (not status) then return end
-
-require("luasnip.loaders.from_vscode").lazy_load()
-
-luasnip.setup({
-  history = true,
-  delete_check_events = "TextChanged",
-})
