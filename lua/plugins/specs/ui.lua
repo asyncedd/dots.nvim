bd92c793b87b729cc9fb695a3d6d839d401e4b24
Async <i.sync@pissmail.com>
2023-03-14 15:38:14 +0900

    modified:   lua/plugins/specs/lsp.lua modified:   lua/plugins/specs/ui.lua

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -2,17 +2,45 @@ return {
   -- Colorscheme
   "catppuccin/nvim",
   -- Indent blanklines
-  "lukas-reineke/indent-blankline.nvim",
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = true,
+    event = "BufReadPost"
+  },
   -- Colorizer
-  "NvChad/nvim-colorizer.lua",
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = true,
+    event = "BufReadPost"
+  },
   -- CCC.nvim
-  "uga-rosa/ccc.nvim",
+  {
+    "uga-rosa/ccc.nvim",
+    config = true,
+    event = "BufReadPost"
+  },
   -- Heirline
-  "rebelot/heirline.nvim",
-  -- Folding
-  "anuvyklack/pretty-fold.nvim",
+  {
+    "rebelot/heirline.nvim",
+    config = function ()
+      require("plugins.configs.ui.heirline")
+    end,
+    event = { "BufReadPost", "VeryLazy" }
+  },
   -- Cursorword
-  "echasnovski/mini.cursorword",
+  {
+    "echasnovski/mini.cursorword",
+    config = function ()
+      require("mini.cursorword").setup()
+    end,
+    event = "BufReadPost",
+  },
   -- Indentscope
-  "echasnovski/mini.indentscope",
+  {
+    "echasnovski/mini.indentscope",
+    config = function ()
+      require("mini.indentscope").setup()
+    end,
+    event = "BufReadPost"
+  },
 }
