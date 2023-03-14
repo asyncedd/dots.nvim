2fc4413d008f635366f5054ebcdfa75a9a956a39
Async <i.sync@pissmail.com>
2023-03-14 17:46:05 +0900

    modified:   lazy-lock.json modified:   lua/plugins/specs/editor.lua

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -65,4 +65,17 @@ return {
     config = true,
     event = "VeryLazy",
   },
+  -- I got harpooned, blazingly fast!
+  {
+    "theprimeagen/harpoon",
+    config = function ()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+      local keymap = vim.keymap.set
+
+      keymap("n", "<leader>a", mark.add_file)
+      keymap("n", "<leader>e", ui.toggle_quick_menu)
+    end,
+    event = "VeryLazy"
+  }
 }
