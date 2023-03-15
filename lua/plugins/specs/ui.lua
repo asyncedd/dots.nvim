4c9d9384d5a179f935889410818f7fc91b3dc56f
Async <i.sync@pissmail.com>
2023-03-15 19:57:25 +0900

    modified:   lua/plugins/specs/ui.lua

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -38,18 +38,6 @@ return {
     end,
     event = "BufReadPost",
   },
-  -- Indentscope
-  {
-    "echasnovski/mini.indentscope",
-    opts = {
-      symbol = "│",
-      options = { try_as_border = true },
-    },
-    config = function (_, opts)
-      require("mini.indentscope").setup(opts)
-    end,
-    event = "BufReadPost"
-  },
   -- Dashboard
   {
     'glepnir/dashboard-nvim',
