8d77875a040ee7d23da84d2681bbcfa71b9cbf3b
Async <i.sync@pissmail.com>
2023-03-15 22:30:30 +0900

    modified:   lua/plugins/configs/tools/tree.lua modified:   lua/plugins/specs/tools.lua

--- lua/plugins/configs/tools/tree.lua
+++ lua/plugins/configs/tools/tree.lua
@@ -1,14 +1,16 @@
-require("nvim-tree").setup({
-  renderer = {
-    full_name = true,
-    group_empty = true,
-    symlink_destination = false,
-    indent_markers = {
-      enable = true,
+return function()
+  require("nvim-tree").setup({
+    renderer = {
+      full_name = true,
+      group_empty = true,
+      symlink_destination = false,
+      indent_markers = {
+        enable = true,
+      },
+      highlight_git = true,
     },
-    highlight_git = true,
-  },
-  diagnostics = {
-    enable = true,
-  }
-})
+    diagnostics = {
+      enable = true,
+    }
+  })
+end
