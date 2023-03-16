ee8dcf8e9f3dade1c276f5b9d40923a4f4fb2a6a
Async <i.sync@pissmail.com>
2023-03-16 17:46:42 +0900

    modified:   lua/plugins/configs/editor/treesitter.lua

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -1,95 +1,94 @@
 return function ()
-  vim.schedule(function()
-    -- Checking if nvim-treesitter is installed properly.
-    local treesitter = require("nvim-treesitter.configs")
+  -- Checking if nvim-treesitter is installed properly.
+  local treesitter = require("nvim-treesitter.configs")
 
-    -- Set the Markid's colors.
-    local markid_config = {
-      bright = {
-        "#f5c2e7",
-        "#fab387",
-        "#f9e2af",
-        "#f5c0e8",
-        "#94e2d5",
-        "#89dceb",
-        "#74c7ec",
-        "#b4befe",
-        "#cba6f7",
-        "#c5e1a5",
+  -- Set the Markid's colors.
+  local markid_config = {
+    bright = {
+      "#f5c2e7",
+      "#fab387",
+      "#f9e2af",
+      "#f5c0e8",
+      "#94e2d5",
+      "#89dceb",
+      "#74c7ec",
+      "#b4befe",
+      "#cba6f7",
+      "#c5e1a5",
+    },
+  }
+
+  -- Configure nvim-treesitter
+  treesitter.setup({
+
+    -- Auto-install missing parsers. (once you open a file that requires it.)
+    auto_install = true,
+
+    -- Enable beautiful syntax highlighting!!!
+    ensure_installed = { "markdown", "markdown_inline" },
+
+    highlight = {
+      enable = true,
+      additional_regex_highlighting = true,
+    },
+
+    -- Enable nvim-treesitter indention.
+    indent = {
+      enable = true,
+    },
+
+    -- Configure Markid.
+    markid = {
+      enable = true,
+      colors = markid_config.bright,
+    },
+
+    matchup = {
+      enable = true,
+    },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>',
+        scope_incremental = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
       },
-    }
+    },
 
-    -- Configure nvim-treesitter
-    treesitter.setup({
+    -- Enable rainbow
+    rainbow = {
+      enable =true,
+      query = "rainbow-parens",
+      strategy = require("ts-rainbow.strategy.global"),
+    },
 
-      -- Auto-install missing parsers. (once you open a file that requires it.)
-      auto_install = true,
-
-      -- Enable beautiful syntax highlighting!!!
-      ensure_installed = { "markdown", "markdown_inline" },
-
-      highlight = {
+    -- Be smarter!
+    refactor = {
+      highlight_definations = {
         enable = true,
-        additional_regex_highlighting = true,
+        clear_on_cursor_move = true,
       },
-
-      -- Enable nvim-treesitter indention.
-      indent = {
+      highlight_current_scope = {
         enable = true,
       },
-
-      -- Configure Markid.
-      markid = {
-        enable = true,
-        colors = markid_config.bright,
-      },
-
-      matchup = {
-        enable = true,
-      },
-
-      incremental_selection = {
+      smart_rename = {
         enable = true,
         keymaps = {
-          init_selection = '<CR>',
-          scope_incremental = '<CR>',
-          node_incremental = '<TAB>',
-          node_decremental = '<S-TAB>',
+          smart_rename = "grr",
         },
       },
-
-      -- Enable rainbow
-      rainbow = {
-        enable =true,
-        query = "rainbow-parens"
-      },
-
-      -- Be smarter!
-      refactor = {
-        highlight_definations = {
-          enable = true,
-          clear_on_cursor_move = true,
-        },
-        highlight_current_scope = {
-          enable = true,
-        },
-        smart_rename = {
-          enable = true,
-          keymaps = {
-            smart_rename = "grr",
-          },
-        },
-        navigation = {
-          enable = true,
-          keyamsp = {
-            goto_defination = "gnd",
-            list_defination = "gnD",
-            list_defination_toc = "g0",
-            goto_next_usage = "<a-*>",
-            goto_previous_usage = "<a-#>",
-          },
+      navigation = {
+        enable = true,
+        keyamsp = {
+          goto_defination = "gnd",
+          list_defination = "gnD",
+          list_defination_toc = "g0",
+          goto_next_usage = "<a-*>",
+          goto_previous_usage = "<a-#>",
         },
       },
-    })
-  end)
+    },
+  })
 end
