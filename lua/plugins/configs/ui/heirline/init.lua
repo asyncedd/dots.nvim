13a3fda202b8dde70e387c196bb3aa6613bbf39e
Async <i.sync@pissmail.com>
2023-03-17 16:55:24 +0900

    modified:   lua/plugins/configs/ui/heirline/init.lua

--- lua/plugins/configs/ui/heirline/init.lua
+++ lua/plugins/configs/ui/heirline/init.lua
@@ -1,15 +1,11 @@
-local lazy_require = require("snips.lazy-require").require_on_index
-
-local heirline = lazy_require("heirline")
 local bufferline = require("plugins.configs.ui.heirline.bufferline")
 local statusline = require("plugins.configs.ui.heirline.statusline")
 -- local statuscolumn = require("plugins.configs.ui.heirline.statuscolumn")
-local colors = require("plugins.configs.ui.heirline.colors")
+require("plugins.configs.ui.heirline.colors")
 
 -- Unpack the results and call the setup function with the appropriate arguments
-heirline.setup({
+require("heirline").setup({
   statusline = statusline,
   tabline = bufferline,
   -- statuscolumn = statuscolumn,
 })
-
