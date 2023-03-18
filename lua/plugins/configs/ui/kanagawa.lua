f154da5a88d2ef507b950c130928347a4ff7f102
Async <i.sync@pissmail.com>
2023-03-18 13:44:37 +0900

    (test) Tweaking around with some settings. modified:   lua/plugins/configs/ui/kanagawa.lua modified:   lua/plugins/configs/ui/tokyonight.lua

--- lua/plugins/configs/ui/kanagawa.lua
+++ lua/plugins/configs/ui/kanagawa.lua
@@ -7,8 +7,24 @@ return function()
     keywordStyle = { italic = true},
     statementStyle = { bold = true },
     typeStyle = {},
-    dimInactive = true,
+    -- dimInactive = true,
     terminalColors = true,
     theme = "wave",
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+        PmenuSbar = { bg = theme.ui.bg_m1 },
+        PmenuThumb = { bg = theme.ui.bg_p2 },
+        TelescopeTitle = { fg = theme.ui.special, bold = true },
+        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+      }
+    end,
   })
 end
