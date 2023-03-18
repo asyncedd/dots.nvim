fb1cbb34897fc4d4ce1c03ac029f403033d23450
Async <i.sync@pissmail.com>
2023-03-18 13:44:37 +0900

    (test) Tweaking around with some settings. modified:   lua/plugins/configs/ui/kanagawa.lua modified:   lua/plugins/configs/ui/tokyonight.lua

--- lua/plugins/configs/ui/tokyonight.lua
+++ lua/plugins/configs/ui/tokyonight.lua
@@ -8,5 +8,35 @@ return function()
       functions = { italic = true, },
       variables = { italic = true, },
     },
+    on_highlights = function(hl, c)
+      local prompt = "#2d3149"
+      hl.TelescopeNormal = {
+        bg = c.bg_dark,
+        fg = c.fg_dark,
+      }
+      hl.TelescopeBorder = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePromptNormal = {
+        bg = prompt,
+      }
+      hl.TelescopePromptBorder = {
+        bg = prompt,
+        fg = prompt,
+      }
+      hl.TelescopePromptTitle = {
+        bg = prompt,
+        fg = prompt,
+      }
+      hl.TelescopePreviewTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopeResultsTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+    end,
   })
 end
