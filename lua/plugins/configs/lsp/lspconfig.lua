abba6bb7733090bc9c3707c4abbaf559dd292f93
Async <i.sync@pissmail.com>
2023-03-19 08:22:30 +0900

    (mod) I'll not forget you, LSP! modified:   lua/core/init.lua new file:   lua/plugins/configs/lsp/lsp modified:   lua/plugins/configs/lsp/lspconfig.lua modified:   lua/plugins/configs/lsp/servers/lua_ls.lua

--- lua/plugins/configs/lsp/lspconfig.lua
+++ lua/plugins/configs/lsp/lspconfig.lua
@@ -24,7 +24,25 @@ return function()
     automatic_installation = true,
   })
 
-  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+  capabilities.textDocument.completion.completionItem = {
+    documentationFormat = { "markdown", "plaintext" },
+    snippetSupport = true,
+    preselectSupport = true,
+    insertReplaceSupport = true,
+    labelDetailsSupport = true,
+    deprecatedSupport = true,
+    commitCharactersSupport = true,
+    tagSupport = { valueSet = { 1 } },
+    resolveSupport = {
+      properties = {
+        "documentation",
+        "detail",
+        "additionalTextEdits",
+      },
+    },
+  }
 
   lspconfig.lua_ls.setup({
     capabilities = capabilities,
