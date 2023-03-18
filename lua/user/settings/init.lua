9c6ac45245ac1b7e536fad9fe233b491a3065b93
Async <i.sync@pissmail.com>
2023-03-18 12:48:21 +0900

    (notice): Add more documentation. modified:   lua/user/settings/init.lua

--- lua/user/settings/init.lua
+++ lua/user/settings/init.lua
@@ -2,9 +2,12 @@ local M = {}
 
 -- Set the colorscheme you want to use here.
 -- For eg. Tokyonight, edge.
+-- Currently you can use:
+-- "catppuccin", "tokyonight", "edge"
+-- If something breaks open an issue.
 -- If you want to use some other colorscheme, you gotta include it in the plugins spec.
 -- At: `lua/user/settings/colorscheme`.
 -- The Default is "catppuccin".
-M.colorscheme = "catppuccin"
+M.colorscheme = "tokyonight"
 
 return M
