8cd7e3c1a8f720caea72fe23211a6cc5a8922689
asyncedd <i.sync@pissmail.com>
2023-03-22 07:03:21 +0900

    feat: Harpooning might be the beat way to hunt!

---
+++ lua/plugins/configs/tools/harpoon.lua
@@ -0,0 +1,14 @@
+return function()
+  local keymap = vim.keymap.set
+
+  keymap("n", "<leader>af", require("harpoon.mark").add_file)
+
+  local hui = require("harpoon.ui")
+  keymap("n", "<leader>mn", hui.toggle_quick_menu)
+
+  keymap("n", "<leader>1", function() hui.nav_file(1)end)
+  keymap("n", "<leader>2", function() hui.nav_file(2)end)
+  keymap("n", "<leader>3", function() hui.nav_file(3)end)
+
+  keymap("n", "<leader>nh", function() hui.nav_next()end)
+end
