77a408dfe66e31b9e5d001947972894b8d95ac14
asyncedd <i.sync@pissmail.com>
2023-03-21 23:48:23 +0900

    feat: Syntax highlighting with nvim-treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,5 @@
 local plugins = {}
+local disabledPlugins = require("user.builtinPlugins")
 
 plugins[1] = {
   "catppuccin/nvim",
@@ -6,6 +7,24 @@ plugins[1] = {
 
 plugins[2] = {
   "nvim-treesitter/nvim-treesitter",
+  lazy = require("core.utils.lazyLoad")("nvim-treesitter"),
+  config = require("plugins.configs.treesitter")
 }
 
-return plugins
+require("lazy").setup({
+  plugins,
+}, {
+  performance = {
+    rtp = {
+      disabled_plugins = disabledPlugins
+    },
+  },
+  install = {
+    colorscheme = { "catppuccin" }
+  },
+  defaults = {
+    lazy = true,
+    version = false,
+  },
+})
+
