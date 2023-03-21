38f7cbbc556c8d981317702789848a516f4e477a
asyncedd <i.sync@pissmail.com>
2023-03-22 00:03:23 +0900

    feat: Auto-install markdown and, markdown_inline parsers.

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,6 +1,13 @@
 return function()
   require("nvim-treesitter.configs").setup({
+
+    -- Make sure we've installed:
+    ensure_installed = { "markdown", "markdown_inline" },
+
+    -- Auto-install mssing parsers
     auto_install = { enable = true },
+
+    -- Enable highlighting.
     highlight = {
       enable = true,
       use_languagetree = true,
