e604ec8cfc16d514738887dfd67bb48be3ce0b73
asyncedd <i.sync@pissmail.com>
2023-03-21 22:06:47 +0900

    feat: Sync clipboards

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -5,6 +5,7 @@ local config = require("user.settings")
 
 -- Disable builtin Plugins.
 require("core.disableBuiltinPlugins")
+
 -- Set the options.
 require("user.options")
 
@@ -18,3 +19,7 @@ require("core.lazy")
 local colorscheme = require("user.settings.colorscheme")
 vim.api.nvim_command("colorscheme " .. colorscheme)
 
+vim.schedule(function ()
+  -- Load options that don't require any UI
+  vim.opt.clipboard = "unnamed,unnamedplus" -- Let's sync clipboards across platforms (Win###s, MacOS and, *Nix based systems)
+end)
