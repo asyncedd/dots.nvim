dce7e37c12f232a8a50d459c93bea077a8879b64
asyncedd <i.sync@pissmail.com>
2023-03-21 22:01:49 +0900

    perf: Doing some fine-tuning.

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -3,33 +3,10 @@
 -- Load the configuration file.
 local config = require("user.settings")
 
--- Define a function that takes a thunk and executes it in a separate coroutine
-local function runInCoroutine(thunk)
-  local co = coroutine.create(thunk)
-  coroutine.resume(co)
-end
-
--- Define a thunk that performs some task
-local function disableBuiltinPlugins()
-  -- Disable builtin Plugins.
-  require("core.disableBuiltinPlugins")
-end
-
-local function options()
-  -- Set the options.
-  require("user.options")
-end
-
--- Create multiple thunks to execute in parallel
-local thunks = {
-  disableBuiltinPlugins,
-  options,
-}
-
--- Execute each thunk in a separate coroutine
-for _, thunk in ipairs(thunks) do
-  runInCoroutine(thunk)
-end
+-- Disable builtin Plugins.
+require("core.disableBuiltinPlugins")
+-- Set the options.
+require("user.options")
 
 -- Bootstrap Lazy.nvim
 require("core.bootstrap")
