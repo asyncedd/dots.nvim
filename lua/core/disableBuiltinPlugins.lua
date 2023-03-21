3873db016f75e42bae38f238a416a9f8adb914a6
asyncedd <i.sync@pissmail.com>
2023-03-21 19:16:10 +0900

    feat: Do base configuration.

--- lua/core/disableBuiltinPlugins.lua
+++ lua/core/disableBuiltinPlugins.lua
@@ -1,26 +1,5 @@
-local g = vim.g
+local disableBuiltinPlugins = require("user.builtinPlugins")
 
--- Disable some builtin plugins.
-g["2html_plugin"] = 1
-g["getscript"] = 1
-g["getscriptPlugin"] = 1
-g["gzip"] = 1
-g["logipat"] = 1
-g["matchit"] = 1
-g["matchparen"] = 1
-g["netrw"] = 1
-g["netrwFileHandlers"] = 1
-g["loaded_remote_plugins"] = 1
-g["loaded_tutor_mode_plugin"] = 1
-g["netrwPlugin"] = 1
-g["netrwSettings"] = 1
-g["rrhelper"] = 1
-g["spellfile_plugin"] = 1
-g["tar"] = 1
-g["tarPlugin"] = 1
-g["vimball"] = 1
-g["vimballPlugin"] = 1
-g["zip"] = 1
-g["zipPlugin"] = 1
-
-g.do_filetype_lua = 1
+for _, plugin in pairs(disableBuiltinPlugins) do
+  vim.g["loaded_" .. plugin] = 1
+end
