6bec226f85f4b8abd536d3531d50900c66760e28
asyncedd <i.sync@pissmail.com>
2023-03-23 07:16:26 +0900

    perf: Potentially fix perf.

---
+++ lua/plugins/configs/mini.lua
@@ -0,0 +1,15 @@
+return function()
+  -- Enable highlighitng of cursorwords.
+  require("mini.cursorword").setup()
+  -- Enable splitjoins.
+  require("mini.splitjoin").setup()
+  -- Enable surround.
+  require("mini.surround").setup()
+  -- Enable AI.
+  require("mini.ai").setup()
+  -- Indentscope.
+  require("mini.indentscope").setup({
+    symbol = "│",
+    options = { try_as_border = true },
+  })
+end
