ec0fca5aa779650fd5881f3f1f2e376c94749b9c
asyncedd <i.sync@pissmail.com>
2023-03-24 03:49:32 +0900

    feat: Make catppuccin more beautiful.

--- lua/user/colorschemes.lua
+++ lua/user/colorschemes.lua
@@ -1,10 +1,15 @@
 return {
-  "catppuccin/nvim",
+  {
+    "catppuccin/nvim",
+    config = function()
+      require("ui.catppuccin")
+    end,
+  },
   "FrenzyExists/aquarium-vim",
   {
     "wuelnerdotexe/vim-enfocado",
     config = function ()
       vim.g.enfocado_style = "nature" -- You can go for "neon" too.
-    end
+    end,
   },
 }
