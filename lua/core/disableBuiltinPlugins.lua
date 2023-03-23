100a88c73ec8cf4145622d0e85c598cb54b3a1a5
asyncedd <i.sync@pissmail.com>
2023-03-23 16:31:05 +0900

    feat: SUPER tab!

--- lua/core/disableBuiltinPlugins.lua
+++ lua/core/disableBuiltinPlugins.lua
@@ -1,5 +1,10 @@
 local disableBuiltinPlugins = require("user.builtinPlugins")
+local o = vim.opt
 
 for _, plugin in pairs(disableBuiltinPlugins) do
   vim.g["loaded_" .. plugin] = 1
 end
+
+o.spell = false
+o.undofile = false
+o.shadafile = "no"
