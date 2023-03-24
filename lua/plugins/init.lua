cb412ed25428fbda550939136d8070640f74e5a6
asyncedd <i.sync@pissmail.com>
2023-03-24 20:44:13 +0900

    perf: Lazy-load on keymaps.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -199,27 +199,23 @@ plugins[21] = {
 
 plugins[22] = {
   "folke/trouble.nvim",
-  config = function ()
-    require("trouble").setup()
-    vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-    {silent = true, noremap = true}
-    )
-    vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-    {silent = true, noremap = true}
-    )
-    vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-    {silent = true, noremap = true}
-    )
-    vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-    {silent = true, noremap = true}
-    )
-    vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-    {silent = true, noremap = true}
-    )
-    vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-    {silent = true, noremap = true}
-    )
-  end,
+  config = true,
+  keys = {
+    { "<leader>xx", "<cmd>TroubleToggle<cr>" },
+    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
+    { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
+    { "gR", "<cmd>TroubleToggle lsp_references<cr>" },
+  }
+}
+
+plugins[23] = {
+  "simrat39/symbols-outline.nvim",
+  config = true,
+  keys = {
+    { "<leader>so", "<cmd>SymbolsOutline<cr>" }
+  }
 }
 
 require("lazy").setup({
