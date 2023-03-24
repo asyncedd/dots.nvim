b91d316316f28188473d9ca0a0049c11709200b6
asyncedd <i.sync@pissmail.com>
2023-03-24 20:19:31 +0900

    fix: Fix some mini.surround mappings.

--- lua/plugins/configs/mini.lua
+++ lua/plugins/configs/mini.lua
@@ -3,7 +3,20 @@ require("mini.cursorword").setup()
 -- Enable splitjoins.
 require("mini.splitjoin").setup()
 -- Enable surround.
-require("mini.surround").setup()
+require("mini.surround").setup({
+  mappings = {
+    add = '<leader>sa', -- Add surrounding in Normal and Visual modes
+    delete = '<leader>sd', -- Delete surrounding
+    find = '<leader>sf', -- Find surrounding (to the right)
+    find_left = '<leader>sF', -- Find surrounding (to the left)
+    highlight = '<leader>sh', -- Highlight surrounding
+    replace = '<leader>sr', -- Replace surrounding
+    update_n_lines = '<leader>sn', -- Update `n_lines`
+
+    suffix_last = 'l', -- Suffix to search with "prev" method
+    suffix_next = 'n', -- Suffix to search with "next" method
+  },
+})
 -- Enable AI.
 require("mini.ai").setup()
 -- Indentscope.
