44beb8029106cf14d779d4d5b7f4b41e0781133d
asyncedd <i.sync@pissmail.com>
2023-03-27 07:51:20 +0900

    perf(spec): rm impatient.nvim
    im impatient with impatient.nvim so bye bye.
    anyways, it's a great plugin overall but, lazy.nvim replaces this :(
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lua/plugins/init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -283,22 +283,6 @@ local plugins = {
       vim.g.startuptime_tries = 10
     end,
   },
-  {
-    "lewis6991/impatient.nvim",
-    init = function()
-      _G.__luacache_config = {
-        chunks = {
-          enable = true,
-          path = vim.fn.stdpath('cache')..'/luacache_chunks',
-        },
-        modpaths = {
-          enable = true,
-          path = vim.fn.stdpath('cache')..'/luacache_modpaths',
-        }
-      }
-      require("impatient")
-    end
-  },
 }
 
 require("lazy").setup({
