340e0fdc9b8669bc459cac8999ac2d2eefe408ec
asyncedd <i.sync@pissmail.com>
2023-03-26 14:29:25 +0900

    perf(init): reduce latency
    requiring a module and only using it once?
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   init.lua
    	modified:   lua/core/after/init.lua
    	deleted:    lua/core/disableBuiltinPlugins.lua
    	deleted:    lua/core/setIcons.lua
    	deleted:    lua/core/utils/lazyLoad.lua
    	modified:   lua/plugins/init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,15 @@
 
 require("user.autocmd")
 
-require("core.disableBuiltinPlugins")
+local disableBuiltinPlugins = require("user.builtinPlugins")
+local o = vim.opt
+
+for _, plugin in ipairs(disableBuiltinPlugins) do
+  vim.g["loaded_" .. plugin] = 1
+end
+
+o.spell = false
+o.undofile = false
 
 require("user.options")
 
