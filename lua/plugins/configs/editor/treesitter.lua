580f043ad97cf3a95d6075cafcbab745d4d15baa
asyncedd <i.sync@pissmail.com>
2023-03-27 07:21:02 +0900

    perf(ts): load treesitter via coruountnies
    I know it'll overhead like crazy
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   init.lua
    	modified:   lua/plugins/configs/editor/treesitter.lua

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -1,15 +1,26 @@
-require("nvim-treesitter.configs").setup({
+local coroutine = coroutine
 
-  -- Make sure we've installed:
-  ensure_installed = { "markdown", "markdown_inline" },
+-- Define a function to resume the coroutine until it completes
+local function resumeCoroutine(co)
+  if coroutine.status(co) ~= "dead" then
+    coroutine.resume(co)
+  end
+end
 
-  -- Auto-install mssing parsers
-  auto_install = true,
+resumeCoroutine(coroutine.create(function()
+  require("nvim-treesitter.configs").setup({
 
-  -- Enable highlighting.
-  highlight = {
-    enable = true,
-    use_languagetree = true,
-  },
-})
+    -- Make sure we've installed:
+    ensure_installed = { "markdown", "markdown_inline" },
+
+    -- Auto-install mssing parsers
+    auto_install = true,
+
+    -- Enable highlighting.
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+  })
+end))
 
