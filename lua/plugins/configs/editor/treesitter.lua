65e5f48e3a71f01087994831ee6b38f1d6c6d8ad
asyncedd <i.sync@pissmail.com>
2023-03-29 11:39:07 +0900

    feat(rainbow): rainbow parentheses.
    New rainbow parentheses.
    
    On branch dap
    Your branch is up to date with 'origin/dap'.
    
    Changes to be committed:
    	modified:   lazy-lock.json
    	modified:   lua/plugins/configs/editor/treesitter.lua
    	modified:   lua/plugins/configs/lsp/config.lua
    	modified:   lua/plugins/init.lua

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -11,5 +11,14 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     use_languagetree = true,
   },
+
+  -- Enable rainbow parentheses.
+  rainbow = {
+    enable = true,
+    -- Which query to use for finding delimiters
+    query = 'rainbow-parens',
+    -- Highlight the entire buffer all at once
+    strategy = require('ts-rainbow').strategy.global,
+  }
 })
 
