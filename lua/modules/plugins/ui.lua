72419a14e2150fd79b6ff7344006dc151294693e
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:25 +0900

    chore: Use another plugin spec. For the sake of readability.

--- lua/modules/plugins/ui.lua
+++ lua/modules/plugins/ui.lua
@@ -1,7 +1,13 @@
-return {
-  {
-    "catppuccin/nvim",
-    lazy = true,
-  }
+local ui = {}
+
+ui["catppuccin/nvim"] = {
+  lazy = true,
 }
 
+local lazy = {}
+
+for repo, config in pairs(ui) do
+  table.insert(lazy, vim.tbl_extend("force", { repo }, config))
+end
+
+return lazy
