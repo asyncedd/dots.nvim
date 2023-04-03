a1ac0f371da37275f9b68ff8ddb04a99a788d8e3
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:21 +0900

    (mod) I'll leave this as now. modified:   lazy-lock.json modified:   lua/plugins/configs/dap/dap.lua modified:   lua/plugins/specs/dap.lua

--- lua/plugins/configs/dap/dap.lua
+++ lua/plugins/configs/dap/dap.lua
@@ -2,17 +2,17 @@ return function()
   local dap = require("dap")
   local keymap = vim.keymap.set
 
-  -- dap.configurations.lua = ({
-  --   {
-  --     type = 'nlua',
-  --     request = 'attach',
-  --     name = "Attach to running Neovim instance",
-  --   }
-  -- })
+  dap.configurations.lua = ({
+    {
+      type = 'nlua',
+      request = 'attach',
+      name = "Attach to running Neovim instance",
+    }
+  })
 
-  -- dap.adapters.nlua = function(callback, config)
-  --   callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
-  -- end
+  dap.adapters.nlua = function(callback, config)
+    callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
+  end
 
   keymap("n", "<leader>db", function ()
     dap.toggle_breakpoint()
