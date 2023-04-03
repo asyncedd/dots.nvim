a4b8d4bc5bc9d3aa680519c8dc2aef6c9ad19e0d
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:14 +0900

    modified:   lua/core/init.lua modified:   lua/plugins/editor.lua modified:   lua/plugins/ui.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,11 +2,12 @@ local M = {}
 
 M.setup = function()
   require("core.options")
+  vim.cmd.colorscheme "habamax"
   vim.defer_fn(function ()
     -- simulate some heavy plugin loading
     require("core.lazy")
     require("mappings.movement")
-  end, 50)
+  end, 0)
 
 end
 
