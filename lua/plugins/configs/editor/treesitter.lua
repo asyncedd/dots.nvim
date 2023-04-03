d5ffda8721a4941a634db2b407df387255a2f1e0
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:17 +0900

    modified:   lazy-lock.json modified:   lua/plugins/configs/editor/treesitter.lua modified:   lua/plugins/spec/treesitter.lua

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -7,23 +7,23 @@ if not status then
 end
 
 -- Load Markid.
-local markid_config = require("markid").colors
+-- local markid_config = require("markid").colors
 
 -- Set the Markid's colors.
-markid_config = {
-  bright = {
-    "#f5c2e7",
-    "#fab387",
-    "#f9e2af",
-    "#f5c0e8",
-    "#94e2d5",
-    "#89dceb",
-    "#74c7ec",
-    "#b4befe",
-    "#cba6f7",
-    "#c5e1a5",
-  },
-}
+-- markid_config = {
+  -- bright = {
+    -- "#f5c2e7",
+    -- "#fab387",
+    -- "#f9e2af",
+    -- "#f5c0e8",
+    -- "#94e2d5",
+    -- "#89dceb",
+    -- "#74c7ec",
+    -- "#b4befe",
+    -- "#cba6f7",
+    -- "#c5e1a5",
+  -- },
+-- }
 
 -- Configure nvim-treesitter
 treesitter.setup({
@@ -44,10 +44,10 @@ treesitter.setup({
   },
 
   -- Configure Markid.
-  markid = {
-    enable = true,
-    colors = markid_config.bright,
-  },
+  -- markid = {
+  --   enable = true,
+  --   colors = markid_config.bright,
+  -- },
   incremental_selection = {
     enable = true,
     keymaps = {
