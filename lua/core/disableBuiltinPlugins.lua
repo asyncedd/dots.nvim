12457745b66141327051c267335ce6faca5b24bd
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:20 +0900

    modified:   lua/core/disableBuiltinPlugins.lua

--- lua/core/disableBuiltinPlugins.lua
+++ lua/core/disableBuiltinPlugins.lua
@@ -1,31 +1,26 @@
--- disable builtins plugins
-local builtins = {
-  "2html_plugin",
-  "getscript",
-  "getscriptPlugin",
-  "gzip",
-  "logipat",
-  "matchit",
-  "netrw",
-  "netrwFileHandlers",
-  "loaded_remote_plugins",
-  "loaded_tutor_mode_plugin",
-  "netrwPlugin",
-  "netrwSettings",
-  "rrhelper",
-  "spellfile_plugin",
-  "tar",
-  "tarPlugin",
-  "vimball",
-  "vimballPlugin",
-  "zip",
-  "zipPlugin",
-  "matchparen", -- matchparen.nvim disables the default
-}
+local g = vim.g
 
-for _, plugin in pairs(builtins) do
-  vim.g[plugin] = 1
-end
+-- Disable some builtin plugins.
+g["2html_plugin"] = 1
+g["getscript"] = 1
+g["getscriptPlugin"] = 1
+g["gzip"] = 1
+g["logipat"] = 1
+g["matchit"] = 1
+g["matchparen"] = 1
+g["netrw"] = 1
+g["netrwFileHandlers"] = 1
+g["loaded_remote_plugins"] = 1
+g["loaded_tutor_mode_plugin"] = 1
+g["netrwPlugin"] = 1
+g["netrwSettings"] = 1
+g["rrhelper"] = 1
+g["spellfile_plugin"] = 1
+g["tar"] = 1
+g["tarPlugin"] = 1
+g["vimball"] = 1
+g["vimballPlugin"]  = 1
+g["zip"] = 1
+g["zipPlugin"] = 1
 
-vim.g.do_filetype_lua = 1
--- vim.g.did_load_filetypes = 0
+g.do_filetype_lua = 1
