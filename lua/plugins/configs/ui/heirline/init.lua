075ef7e0f7d5efddcbbbf35a47d56b2e74b19ce5
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:19 +0900

    modified:   init.lua new file:   statuscolumn/init.lua renamed:    statuscolumn.lua -> statuscolumn/lineNumber.lua

--- lua/plugins/configs/ui/heirline/init.lua
+++ lua/plugins/configs/ui/heirline/init.lua
@@ -3,13 +3,13 @@ local lazy_require = require("snips.lazy-require").require_on_index
 local heirline = lazy_require("heirline")
 local bufferline = require("plugins.configs.ui.heirline.bufferline")
 local statusline = require("plugins.configs.ui.heirline.statusline")
-local statuscolumn = require("plugins.configs.ui.heirline.statuscolumn")
+-- local statuscolumn = require("plugins.configs.ui.heirline.statuscolumn")
 local colors = require("plugins.configs.ui.heirline.colors")
 
 -- Unpack the results and call the setup function with the appropriate arguments
 heirline.setup({
   statusline = statusline,
   tabline = bufferline,
-  statuscolumn = statuscolumn.line_number,
+  -- statuscolumn = statuscolumn,
 })
 
--- lua/plugins/configs/ui/heirline/statuscolumn/lineNumber.lua
+++ lua/plugins/configs/ui/heirline/statuscolumn/lineNumber.lua
@@ -1,16 +1,16 @@
-local M = {}
 local o = vim.opt
 
--- Line Number
-M.line_number = {
+local lineNumber = {
   condition = function() return (o.number:get() or o.relativenumber:get()) end,
   provider = function()
     local num = o.number:get()
     local relnum = o.relativenumber:get()
-    if (num and not relnum) then return "%l"
-    elseif (relnum and not num) then return "%r"
+    if (num and not relnum) then return "%#LineNr#%l%#" -- Highlight the line number using the "LineNr" highlight group
+    elseif (relnum and not num) then return " %r"
     else return "%{v:relnum?v:relnum:v:lnum}" end
   end,
 }
 
-return M
+vim.cmd()
+
+return lineNumber
