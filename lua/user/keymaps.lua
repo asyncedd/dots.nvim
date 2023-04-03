0117ccbee934ea488651bb5ff16f6b4407dda261
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:27 +0900

    feat(settings): tweak some indent-related options.
    As, it says, tweak some indent-related options.
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lua/plugins/configs/mini.lua
    	modified:   lua/user/keymaps.lua
    	modified:   lua/user/options.lua

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -22,10 +22,3 @@ map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
--- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
