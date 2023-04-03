e01a2aa9ec21759aa2ff2fd9a6574bac6dbea2fe
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:29 +0900

    feat(npairs): make npairs more "intelligent.".
    make npairs treesitter-syntax-aware
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lua/plugins/configs/editor/autopairs.lua

--- lua/plugins/configs/editor/autopairs.lua
+++ lua/plugins/configs/editor/autopairs.lua
@@ -1,1 +1,36 @@
-require("nvim-autopairs").setup()
+local Rule = require("nvim-autopairs.rule")
+local ts_conds = require("nvim-autopairs.ts-conds")
+local npairs = require("nvim-autopairs")
+
+npairs.setup({
+  check_ts = true,
+  ts_config = {
+    lua = {
+      "string",
+    },-- it will not add a pair on that treesitter node
+    javascript = {
+      "template_string",
+    },
+    java = false,-- don't check treesitter on java
+  },
+  enable_check_bracket_line = false,
+  fast_wrap = {
+    map = '<M-e>',
+    chars = { '{', '[', '(', '"', "'" },
+    pattern = [=[[%'%"%>%]%)%}%,]]=],
+    end_key = '$',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    highlight = 'Search',
+    highlight_grey='Comment'
+  },
+})
+
+-- press % => %% only while inside a comment or string
+npairs.add_rules({
+  Rule("%", "%", "lua")
+    :with_pair(ts_conds.is_ts_node({'string','comment'})),
+  Rule("$", "$", "lua")
+    :with_pair(ts_conds.is_not_ts_node({'function'}))
+})
+
