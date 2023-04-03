b108079e968d94bf7455bff808d90edac65ee1c3
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:15 +0900

    modified:   lazy-lock.json new file:   lua/plugins/configs/completion/cmp.lua new file:   lua/plugins/configs/completion/luasnip.lua new file:   lua/plugins/spec/completion.lua

---
+++ lua/plugins/configs/completion/cmp.lua
@@ -0,0 +1,43 @@
+return function()
+  local status, cmp = pcall(require, "cmp")
+  local status1, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+  if (not status) then return end
+  if (not status1) then return end
+
+  require("nvim-autopairs").setup {}
+
+  cmp.event:on(
+    "confirm_done",
+    cmp_autopairs.on_confirm_done()
+  )
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "buffer" },
+    }),
+    enabled = function()
+      -- disable completion in comments
+      local context = require 'cmp.config.context'
+      -- keep command mode completion enabled when cursor is in a comment
+      if vim.api.nvim_get_mode().mode == 'c' then
+        return true
+      else
+        return not context.in_treesitter_capture("comment") 
+          and not context.in_syntax_group("Comment")
+      end
+    end
+  })
+end
---
+++ lua/plugins/configs/completion/luasnip.lua
@@ -0,0 +1,11 @@
+return function()
+  local status, luasnip = pcall(require, "luasnip")
+  if (not status) then return end
+
+  require("luasnip.loaders.from_vscode").lazy_load()
+
+  luasnip.setup({
+    history = true,
+    delete_check_events = "TextChanged",
+  })
+end
