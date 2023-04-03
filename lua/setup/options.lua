b36cf480bacc3ac9e51beda5e8fd586d0d02ab35
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:11 +0900

    Update structure

--- lua/setup/options.lua
+++ lua/setup/options.lua
@@ -1,5 +1,48 @@
-local opts = require("options.opts")
+local o = vim.o
+local g = vim.g
 
-for k, v in pairs(opts) do
-  vim.o[k] = v
-end
+-- General
+g.mapleader = " "
+o.shiftwidth = 2 -- Set the indents to 2.
+undofile = true -- Enable persistent undo's
+-- Don't store backup when overwriting the file,
+o.backup = false
+o.writebackup = false
+o.mouse = "a" -- Enable mouse for all modes
+o.swapfile = false -- Disable Swap files.
+o.hidden = true -- Don't automatically save whilst changing buffers.
+
+-- Apperance
+o.breakindent = true -- Indent wrapped lines to match line start
+o.cursorline = true -- Highlight the line we're currently editting.
+o.linebreak = true -- Break long lines into multiple. visually.
+o.number = true -- Enable line numbers
+o.splitbelow = true -- Splits below.
+o.splitright = true -- Splits right.
+o.termguicolors = true -- Enable GUI colors.
+o.showmode = false -- Don't show the mode in the CLI.
+
+o.wrap = false -- Show long lines as one big line.
+
+o.signcolumn = "yes" -- Always show sign columns.
+o.fillchars = "eob: " -- Don't show the "~" outside of the buffers
+
+-- Editing
+o.ignorecase = true -- Ignore case when searching.
+o.incsearch = true -- Show search results whilst searching.
+o.infercase = true -- Infer letter cases.
+o.smartcase = true -- Don't ignore case if the pattern has an uppercase letter in it.
+o.smartindent = true -- Get smart indents.
+
+o.completeopt = "menuone,noinsert,noselect" -- This is mainly for hrsh7th/nvim-cmp.
+o.virtualedit = "onemore" -- Allow going one more.
+o.formatoptions = "qjl1" -- Don't autoformat commands.
+
+o.splitkeep = "screen" -- Reduce scroll if the window splits.
+
+o.pumbleend = 10 -- Make the builtin completion menu slightly transparent
+o.pumhieght = 10 -- Make it smaller
+o.winblend = 10 -- floating windows are sightly transparent.
+
+o.list = true -- Show them.
+o.listchars = "" -- Couldn't think about it.
