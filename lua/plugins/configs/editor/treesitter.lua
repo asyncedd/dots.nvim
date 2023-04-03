db16772deccd69c6321117aa656f03bd5e8a8af3
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:28 +0900

    feat(tsrainbow): Properly configure ts-rainbow2
    I forgot to configure both query and stragety

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -13,6 +13,8 @@ require("nvim-treesitter.configs").setup({
   },
   rainbow = {
     enable = true,
+    query = "rainbow-parens",
+    strategy = require("ts-rainbow").strategy.global,
   },
 })
 
