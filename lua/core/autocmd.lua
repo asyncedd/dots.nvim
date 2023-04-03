373f87a36c30e157e1d02c01b1d7364aba936a8c
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:18 +0900

    modified:   lua/core/autocmd.lua modified:   lua/core/init.lua modified:   lua/core/lazy.lua modified:   lua/core/options.lua modified:   lua/mappings/lspsaga.lua modified:   lua/mappings/movement.lua modified:   lua/mappings/plugins.lua

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,22 +1,25 @@
-local o = vim.api.nvim_set_option_value
-local autocmd = vim.api.nvim_create_autocmd
+-- Avoid expensive global variable searching by this simple LOC!
+local api = vim.api
+-- also this... awd
+local o = api.nvim_set_option_value
+-- Also this! :3
+local autocmd = api.nvim_create_autocmd
 
-local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
-end
-
+-- Alright! We're going to the Autocmds here!
+-- First, this autocmd disables realtivenumbers when we're in insert mode.
+-- Since, we really don't need those there.
 autocmd({ "InsertEnter" }, {
-  group = augroup("ln_on_ie"),
   callback = function()
     o("relativenumber", false, {})
   end
 })
 
+-- This enables relativenumbers when we exisited insert mode.
+-- This is because, realtivenumbers can be more useful somehow.
 autocmd({ "InsertLeave" }, {
-  group = augroup("ln_on_ie"),
   callback = function()
     o("relativenumber", true, {})
   end
 })
 
-
+-- This is about it!
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,21 +1,34 @@
-local o = vim.api.nvim_set_option_value
-
+-- Set the <leader> key to: <Space>
+-- So we can go for ex. -> `<leader>ff` to open Telescope.nvim.
+-- Quite useful ain't it? :)
 vim.g.mapleader = " "
-o("shadafile", "NONE", {})
 
+-- Set some options.
+-- I tried setting all the options there so we can make it nice and clean :)
 require("core.options")
 
+-- Initalize lazy.nvim. The plugin manager we're using.
 require("core.lazy")
 
+-- Also we're going to load some plugins
+-- Ofcourse we can go ahead and do that in out plugins spec (via core.lazy)
+-- But, I want that find control so here we go!
 require("plugins.loader")
 
+-- Ofcourse, setting our beautiful `catppuccin` colorscheme.
+-- It's clean and cute! (CC)
+-- Plus it's customizable. (CCC)
+-- So here it is!
 vim.api.nvim_command("colorscheme catppuccin")
 
-vim.defer_fn(function()
+-- Do some stuff that aren't required in our UI. (mappings, autocmd etc...)
+-- Also without blocking the main thread.
+vim.schedule(function()
+  -- Add some autocmds. You can checkit out!
   require("core.autocmd")
+  -- Add some mappings...
+  -- TODO merge `mappings.lspsaga` with `mappings.plugins`
   require("mappings.movement")
   require("mappings.lspsaga")
   require("mappings.plugins")
-
-  o("shadafile", "", {})
-end, 0)
+end)
