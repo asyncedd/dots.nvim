306b95f756618c467cc29e60cd40e959a07a34ca
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:13 +0900

    I don't know

--- lua/modules/configs/editor/ufo.lua
+++ lua/modules/configs/editor/ufo.lua
@@ -1,8 +1,6 @@
 return function()
   require("ufo").setup()
 
-  vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-  vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-  vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
-  vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+  vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+  vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 end
