27230c8a14db1cbb06f80eb5e33fc49f6693b575
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:20 +0900

    (feat) Remove Flit.nvim. modified:   lua/plugins/configs/editor/motion.lua

--- lua/plugins/configs/editor/motion.lua
+++ lua/plugins/configs/editor/motion.lua
@@ -1,6 +1,5 @@
 return function()
   local leap = require("leap")
-  local flit = require("flit")
   local miniAi = require("mini.ai")
 
   miniAi.setup()
@@ -31,7 +30,5 @@ return function()
   })
   -- Try it without this setting first, you might find you don't even miss it.
   -- require('leap').opts.highlight_unlabeled_phase_one_targets = true
-
-  flit.setup()
 
 end
