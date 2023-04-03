7649993d1fe189470fc1c2af724739238931097a
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:24 +0900

    (feat) Allow user to set the colorscheme to use. modified:   lazy-lock.json modified:   lua/core/init.lua new file:   lua/plugins/configs/ui/edge.lua modified:   lua/plugins/configs/ui/heirline/colors.lua modified:   lua/user/settings/colorscheme.lua new file:   lua/user/settings/init.lua

---
+++ lua/user/settings/init.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+-- Set the colorscheme you want to use here.
+-- For eg. Tokyonight, edge.
+-- If you want to use some other colorscheme, you gotta include it in the plugins spec.
+-- At: `lua/user/settings/colorscheme`.
+-- The Default is "catppuccin".
+M.colorscheme = "catppuccin"
+
+return M
