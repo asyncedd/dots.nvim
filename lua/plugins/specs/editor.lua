99333ea0d27f5cc00e91fb7a78670309935b6760
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:17 +0900

    modified:   lua/core/lazy.lua deleted:    lua/plugins/spec.lua new file:   lua/plugins/specs/editor.lua new file:   lua/plugins/specs/lsp.lua new file:   lua/plugins/specs/tools.lua new file:   lua/plugins/specs/ui.lua

---
+++ lua/plugins/specs/editor.lua
@@ -0,0 +1,75 @@
+return {
+  -- Comment
+  {
+    "numToStr/Comment.nvim",
+    config = true,
+    event = "BufReadPost",
+  },
+  -- UFO
+  {
+    "kevinhwang91/nvim-ufo",
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        return {"treesitter", "indent"}
+      end
+    },
+    config = true,
+    dependencies = {
+      "kevinhwang91/promise-async"
+    },
+    event = "BufReadPost",
+  },
+  -- Gitsigns!
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    },
+    config = true,
+    event = "BufReadPost",
+  },
+  -- Leap!
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require("plugins.configs.editor.motion")
+    end,
+    dependencies = {
+      {
+        "ggandor/flit.nvim",
+        keys = function()
+          local ret = {}
+          for _, key in ipairs({ "f", "F", "t", "T" }) do
+            ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+          end
+          return ret
+        end,
+        opts = { labeled_modes = "nx" },
+      },
+      "tpope/vim-repeat",
+      "echasnovski/mini.ai",
+    },
+    event = "BufReadPost",
+  },
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("plugins.configs.editor.treesitter")
+    end,
+    dependencies = {
+      "andymass/vim-matchup",
+    },
+    event = "BufReadPost",
+  },
+}
