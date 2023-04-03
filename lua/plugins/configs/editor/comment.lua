17de54bd1a0bf709986714975b204f6b57709b3c
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:15 +0900

    modified:   lazy-lock.json modified:   lua/plugins/configs/editor/comment.lua modified:   lua/plugins/configs/editor/gitsigns.lua modified:   lua/plugins/configs/editor/treesitter.lua modified:   lua/plugins/spec/editor.lua

--- lua/plugins/configs/editor/comment.lua
+++ lua/plugins/configs/editor/comment.lua
@@ -1,6 +1,10 @@
 return function()
+  -- Set Comment.nvim as a "comment" variable if installed properly.
   local status, comment = pcall(require, "Comment")
+
+  -- If it were not installed, return.
   if (not status) then return end
 
+  -- Enable Comment.nvim
   comment.setup()
 end
--- lua/plugins/spec/editor.lua
+++ lua/plugins/spec/editor.lua
@@ -3,6 +3,11 @@ local M = {
     "numToStr/Comment.nvim",
     config = require("plugins.configs.editor.comment"),
     event = "VeryLazy",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = require("plugins.configs.editor.gitsigns"),
+    event = "BufReadPost",
   }
 }
 
