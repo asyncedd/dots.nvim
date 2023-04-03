9223db353be25b97d7350a7c5c75ce924431b224
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:20 +0900

    modified:   lazy-lock.json new file:   lua/plugins/configs/editor/comment.lua new file:   lua/plugins/configs/editor/gitsigns.lua new file:   lua/plugins/configs/editor/harpoon.lua modified:   lua/plugins/configs/editor/motion.lua new file:   lua/plugins/configs/editor/splitjoin.lua new file:   lua/plugins/configs/editor/trailblazer.lua modified:   lua/plugins/configs/editor/treesitter.lua modified:   lua/plugins/specs/editor.lua

---
+++ lua/plugins/configs/editor/gitsigns.lua
@@ -0,0 +1,15 @@
+return function()
+  require("gitsigns").setup({
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  })
+end
--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -1,58 +1,60 @@
--- Checking if nvim-treesitter is installed properly.
-local treesitter = require("nvim-treesitter.configs")
+return function ()
+  -- Checking if nvim-treesitter is installed properly.
+  local treesitter = require("nvim-treesitter.configs")
 
--- Set the Markid's colors.
-local markid_config = {
-  bright = {
-    "#f5c2e7",
-    "#fab387",
-    "#f9e2af",
-    "#f5c0e8",
-    "#94e2d5",
-    "#89dceb",
-    "#74c7ec",
-    "#b4befe",
-    "#cba6f7",
-    "#c5e1a5",
-  },
-}
-
--- Configure nvim-treesitter
-treesitter.setup({
-
-  -- Auto-install missing parsers. (once you open a file that requires it.)
-  auto_install = true,
-
-  -- Enable beautiful syntax highlighting!!!
-  ensure_installed = { "markdown", "markdown_inline" },
-
-  highlight = {
-    enable = true,
-    additional_regex_highlighting = true,
-  },
-
-  -- Enable nvim-treesitter indention.
-  indent = {
-    enable = true,
-  },
-
-  -- Configure Markid.
-  markid = {
-    enable = true,
-    colors = markid_config.bright,
-  },
-
-  matchup = {
-    enable = true,
-  },
-
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<CR>',
-      scope_incremental = '<CR>',
-      node_incremental = '<TAB>',
-      node_decremental = '<S-TAB>',
+  -- Set the Markid's colors.
+  local markid_config = {
+    bright = {
+      "#f5c2e7",
+      "#fab387",
+      "#f9e2af",
+      "#f5c0e8",
+      "#94e2d5",
+      "#89dceb",
+      "#74c7ec",
+      "#b4befe",
+      "#cba6f7",
+      "#c5e1a5",
     },
-  },
-})
+  }
+
+  -- Configure nvim-treesitter
+  treesitter.setup({
+
+    -- Auto-install missing parsers. (once you open a file that requires it.)
+    auto_install = true,
+
+    -- Enable beautiful syntax highlighting!!!
+    ensure_installed = { "markdown", "markdown_inline" },
+
+    highlight = {
+      enable = true,
+      additional_regex_highlighting = true,
+    },
+
+    -- Enable nvim-treesitter indention.
+    indent = {
+      enable = true,
+    },
+
+    -- Configure Markid.
+    markid = {
+      enable = true,
+      colors = markid_config.bright,
+    },
+
+    matchup = {
+      enable = true,
+    },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>',
+        scope_incremental = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
+      },
+    },
+  })
+end
