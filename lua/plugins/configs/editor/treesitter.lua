41328bf60a5881a742da8576b853c58e93137fe5
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:15 +0900

    modified:   lua/core/init.lua deleted:    lua/core/pluginLoader/other_plugins.lua deleted:    lua/core/pluginLoader/ui_plugins.lua modified:   lua/plugins/configs/editor/comment.lua modified:   lua/plugins/configs/editor/gitsigns.lua modified:   lua/plugins/configs/editor/motion.lua modified:   lua/plugins/configs/editor/treesitter.lua modified:   lua/plugins/configs/editor/ufo.lua modified:   lua/plugins/configs/ui/heirline/init.lua modified:   lua/plugins/configs/ui/indent.lua modified:   lua/plugins/configs/ui/matcher.lua modified:   lua/plugins/spec/editor.lua modified:   lua/plugins/spec/folding.lua modified:   lua/plugins/spec/motion.lua modified:   lua/plugins/spec/treesitter.lua modified:   lua/plugins/spec/ui.lua

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -1,22 +1,24 @@
-local status, treesitter = pcall(require, "nvim-treesitter.configs")
-if not status then return end
+return function()
+  local status, treesitter = pcall(require, "nvim-treesitter.configs")
+  if not status then return end
 
-local markid = require("markid")
+  local markid = require("markid")
 
-markid.colors = {
-  bright = {"#f5c2e7", "#fab387", "#f9e2af", "#f5c0e8", "#94e2d5", "#89dceb", "#74c7ec", "#b4befe", "#cba6f7", "#c5e1a5" },
-}
-
-treesitter.setup({
-  auto_install = true, -- Auto-install missing parsers. (once you open a file that requires it.)
-  highlight = {
-    enable = true -- Enable beautiful syntax highlighting!!!
-  },
-  indent = {
-    enable = true
-  },
-  markid = {
-    enable = true,
-    colors = markid.colors.bright
+  markid.colors = {
+    bright = {"#f5c2e7", "#fab387", "#f9e2af", "#f5c0e8", "#94e2d5", "#89dceb", "#74c7ec", "#b4befe", "#cba6f7", "#c5e1a5" },
   }
-})
+
+  treesitter.setup({
+    auto_install = true, -- Auto-install missing parsers. (once you open a file that requires it.)
+    highlight = {
+      enable = true -- Enable beautiful syntax highlighting!!!
+    },
+    indent = {
+      enable = true
+    },
+    markid = {
+      enable = true,
+      colors = markid.colors.bright
+    }
+  })
+end
--- lua/plugins/configs/ui/heirline/init.lua
+++ lua/plugins/configs/ui/heirline/init.lua
@@ -1,71 +1,73 @@
-local status, heirline = pcall(require, "heirline")
-local status1, conditions = pcall(require, "heirline.conditions")
-local status2, colors = pcall(require, "configs.ui.heirline.colors")
-local status3, ViMode = pcall(require, "configs.ui.heirline.vimode")
-local status4, FileName = pcall(require, "configs.ui.heirline.filename")
-local status5, Gitsigns = pcall(require, "configs.ui.heirline.git")
-local status6, Scrollbar = pcall(require, "configs.ui.heirline.scrollbar")
-local status7, File = pcall(require, "configs.ui.heirline.file")
--- local Gitsigns = require("configs.ui.heirline.git")
-if (not status) then return end
-if (not status1) then return end
-if (not status2) then return end
-if (not status3) then return end
-if (not status4) then return end
-if (not status5) then return end
-if (not status6) then return end
-if (not status7) then return end
+return function()
+  local status, heirline = pcall(require, "heirline")
+  local status1, conditions = pcall(require, "heirline.conditions")
+  local status2, colors = pcall(require, "configs.ui.heirline.colors")
+  local status3, ViMode = pcall(require, "configs.ui.heirline.vimode")
+  local status4, FileName = pcall(require, "configs.ui.heirline.filename")
+  local status5, Gitsigns = pcall(require, "configs.ui.heirline.git")
+  local status6, Scrollbar = pcall(require, "configs.ui.heirline.scrollbar")
+  local status7, File = pcall(require, "configs.ui.heirline.file")
+  -- local Gitsigns = require("configs.ui.heirline.git")
+  if (not status) then return end
+  if (not status1) then return end
+  if (not status2) then return end
+  if (not status3) then return end
+  if (not status4) then return end
+  if (not status5) then return end
+  if (not status6) then return end
+  if (not status7) then return end
 
-heirline.load_colors(colors)
+  heirline.load_colors(colors)
 
-local Align = { provider = "%=" }
-local Space = { provider = " " }
+  local Align = { provider = "%=" }
+  local Space = { provider = " " }
 
-local DefaultStatusLine = {
-  ViMode, Space, FileName.WorkDir, Space, FileName.FileNameBlock , Space, Gitsigns,
-  Align,
-  Align, File.FileType, Space, Scrollbar.Ruler, Space, Scrollbar.ScrollBar,
-}
+  local DefaultStatusLine = {
+    ViMode, Space, FileName.WorkDir, Space, FileName.FileNameBlock , Space, Gitsigns,
+    Align,
+    Align, File.FileType, Space, Scrollbar.Ruler, Space, Scrollbar.ScrollBar,
+  }
 
-local StatusLines = {
+  local StatusLines = {
 
-  hl = function()
-    if conditions.is_active() then
-      return "StatusLine"
-    else
-      return "StatusLineNC"
-    end
-  end,
-
-  static = {
-    mode_colors_map = {
-      n = "red",
-      i = "green",
-      v = "cyan",
-      V = "cyan",
-      ["\22"] = "cyan",
-      c = "orange",
-      s = "purple",
-      S = "purple",
-      ["\19"] = "purple",
-      R = "orange",
-      r = "orange",
-      ["!"] = "red",
-      t = "green",
-    },
-    mode_color = function(self)
-      local mode = conditions.is_active() and vim.fn.mode() or "n"
-      return self.mode_colors_map[mode]
+    hl = function()
+      if conditions.is_active() then
+        return "StatusLine"
+      else
+        return "StatusLineNC"
+      end
     end,
-  },
 
-  -- the first statusline with no condition, or which condition returns true is used.
-  -- think of it as a switch case with breaks to stop fallthrough.
-  fallthrough = false,
+    static = {
+      mode_colors_map = {
+        n = "red",
+        i = "green",
+        v = "cyan",
+        V = "cyan",
+        ["\22"] = "cyan",
+        c = "orange",
+        s = "purple",
+        S = "purple",
+        ["\19"] = "purple",
+        R = "orange",
+        r = "orange",
+        ["!"] = "red",
+        t = "green",
+      },
+      mode_color = function(self)
+        local mode = conditions.is_active() and vim.fn.mode() or "n"
+        return self.mode_colors_map[mode]
+      end,
+    },
 
-  DefaultStatusLine
-}
+    -- the first statusline with no condition, or which condition returns true is used.
+    -- think of it as a switch case with breaks to stop fallthrough.
+    fallthrough = false,
 
-heirline.setup({
-  statusline = StatusLines
-})
+    DefaultStatusLine
+  }
+
+  heirline.setup({
+    statusline = StatusLines
+  })
+end
