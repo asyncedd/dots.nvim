b591af4de337597d29bc9825232b0bb418c202ce
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:21 +0900

    feat: Do base configuration.

--- lua/mappings/movement.lua
+++
@@ -1,8 +0,0 @@
--- Avoid expensive global variable searching, we already have it at home!
-local map = vim.api.nvim_set_keymap
-
--- Move up/down on VISUAL line instead of a ACTUAL line
--- Go down one visual line instead an actual line. (Useful for wrapped lines)
-map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
--- Go up one visual line instead an actual line. (Useful for wrapped lines)
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,34 +1,8 @@
--- Set some options for Neovim.
--- If you want a low-overhead version of this, directly set it in `core.options`.
--- I meant, you should really do it.
+-- lua/user/options
+-- Options for Neovim.
 
-local opts = {
+local o = vim.opt
+local g = vim.g
 
-  -- Set options by: (ex)
-  -- numbers = true,
-  -- To enable line numbers
-  -- https://neovim.io/doc/user/options.html
-  -- Btw, K1SS. (Keep it simple, stupid)
-  --
-  -- Some tips for troubleshooting.
-  -- - Does all lines ends with ","?
-  -- For ex.
-  -- a = a,
-  -- b = b,
-  -- c = c,
-  -- d = d <-- This!
-  -- e = e,
-  -- listchars = { -- See Spaces and eol's as a character.
-  --   eol = "↴",
-  --   space = "⋅",
-  --   tab = "▶ ",
-  --   -- trail = "•",
-  --   precedes = "«",
-  --   extends = "»",
-  --   nbsp = "␣",
-  -- }
-}
-
--- EOL
--- DO NOT "HACK" this line!
-return opts
+o.shiftwidth = 2
+o.number = true
