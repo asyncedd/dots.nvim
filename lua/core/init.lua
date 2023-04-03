fa4c872ec5c03cdda930656b38069117c8063dd6
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:14 +0900

    modified:   lua/core/init.lua 	lua/core/runtime.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -4,6 +4,7 @@ M.setup = function()
   require("core.options")
   require("mappings.movement")
   require("core.lazy")
+  require("core.runtime")
 
   vim.api.nvim_command("colorscheme catppuccin")
 end
