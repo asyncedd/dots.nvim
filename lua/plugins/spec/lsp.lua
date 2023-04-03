9b4e1560b46bd50d624ac411716e7e052680c5f7
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:15 +0900

    modified:   lazy-lock.json new file:   lua/plugins/configs/lsp/lspconfig.lua new file:   lua/plugins/configs/lsp/lspconfig.lua~ modified:   lua/plugins/configs/lsp/servers/lua_ls.lua modified:   lua/plugins/configs/lsp/servers/lua_ls.lua~ modified:   lua/plugins/spec/lsp.lua modified:   lua/plugins/spec/lsp.lua~

--- lua/plugins/spec/lsp.lua
+++ lua/plugins/spec/lsp.lua
@@ -4,9 +4,12 @@ M = {
   {
     "neovim/nvim-lspconfig",
     event = "BufReadPost",
+    config = require("plugins.configs.lsp.lspconfig"),
     dependencies = {
       "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim"
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
     }
   },
   {
