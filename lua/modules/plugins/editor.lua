a6a65765d9a207df3177ef5adef3deafb958de26
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:11 +0900

    Add gitsigns and vim-illuminate

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -30,4 +30,18 @@ table.insert(editor, {
   config = require("modules.configs.editor.twilight")
 })
 
+table.insert(editor, {
+  "RRethy/vim-illuminate",
+  event = "BufReadPost"
+})
+
+
+table.insert(editor, {
+  "lewis6991/gitsigns.nvim",
+  event = "BufReadPost",
+  config = require("modules.configs.editor.gitsigns")
+})
+
+
+
 return editor
