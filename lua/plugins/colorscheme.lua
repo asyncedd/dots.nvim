19ead8c3cb30d67b27455a7701882f7d9e278810
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:14 +0900

    modified:   init.lua new file:   lua/core/filetype_detector.lua modified:   lua/core/init.lua modified:   lua/plugins/colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,12 @@
 return {
-  "catppuccin/nvim",
+  {
+    "catppuccin/nvim",
+    config = function()
+      vim.api.nvim_command("colorscheme catppuccin")
+    end,
+    lazy = false,
+    piority = 1000
+  },
   "rebelot/kanagawa.nvim",
   "folke/tokyonight.nvim",
   "EdenEast/nightfox.nvim",
