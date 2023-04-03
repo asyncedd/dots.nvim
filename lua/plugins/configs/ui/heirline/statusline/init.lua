ab8380affa45c63aed53313c10e0eb4d031cd642
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:18 +0900

    modified:   lua/plugins/configs/ui/heirline/bufferline.lua modified:   lua/plugins/configs/ui/heirline/init.lua renamed:    lua/plugins/configs/ui/heirline/file.lua -> lua/plugins/configs/ui/heirline/statusline/file.lua renamed:    lua/plugins/configs/ui/heirline/filename.lua -> lua/plugins/configs/ui/heirline/statusline/filename.lua renamed:    lua/plugins/configs/ui/heirline/git.lua -> lua/plugins/configs/ui/heirline/statusline/git.lua new file:   lua/plugins/configs/ui/heirline/statusline/init.lua renamed:    lua/plugins/configs/ui/heirline/lazy.lua -> lua/plugins/configs/ui/heirline/statusline/lazy.lua renamed:    lua/plugins/configs/ui/heirline/lsp.lua -> lua/plugins/configs/ui/heirline/statusline/lsp.lua renamed:    lua/plugins/configs/ui/heirline/scrollbar.lua -> lua/plugins/configs/ui/heirline/statusline/scrollbar.lua renamed:    lua/plugins/configs/ui/heirline/search.lua -> lua/plugins/configs/ui/heirline/statusline/search.lua renamed:    lua/plugins/configs/ui/heirline/terminalname.lua -> lua/plugins/configs/ui/heirline/statusline/terminalname.lua renamed:    lua/plugins/configs/ui/heirline/vimode.lua -> lua/plugins/configs/ui/heirline/statusline/vimode.lua

---
+++ lua/plugins/configs/ui/heirline/statusline/init.lua
@@ -0,0 +1,86 @@
+local M
+
+local lazy_require = require("snips.lazy-require").require_on_index
+local heirline = lazy_require("heirline")
+local conditions = lazy_require("heirline.conditions")
+local utils = lazy_require("heirline.utils")
+local ViMode = require("plugins.configs.ui.heirline.statusline.vimode")
+-- Unfortunately lazy_requiring ViMode doesn't work ;-;
+local FileName = lazy_require("plugins.configs.ui.heirline.statusline.filename")
+local Gitsigns = require("plugins.configs.ui.heirline.statusline.git")
+local Scrollbar = lazy_require("plugins.configs.ui.heirline.statusline.scrollbar")
+local File = lazy_require("plugins.configs.ui.heirline.statusline.file")
+local LSP = lazy_require("plugins.configs.ui.heirline.statusline.lsp")
+local Lazy = lazy_require("plugins.configs.ui.heirline.statusline.lazy")
+local SearchResults = require("plugins.configs.ui.heirline.statusline.search")
+
+local Align = { provider = "%=", hl = { bg = "normal" } }
+local Space = { provider = " ", hl = { bg = "normal" } }
+
+local DefaultStatusLine = {
+  Lazy, Space, ViMode, Space, SearchResults,
+  utils.surround({ "", "" }, "bright_bg", {
+    FileName.WorkDir,
+    utils.surround({ "", "" }, function(self) return self:mode_color() end, { FileName.FileNameBlock }),
+  }),
+  Space,
+  {
+    Gitsigns,
+    hl = { bg = "normal" }
+  },
+  Space,
+  {
+    LSP.LSPActive,
+    hl = { bg = "normal" }
+  },
+  Space,
+  {
+    LSP.Diagnostics,
+    hl = { bg = "normal" }
+  },
+  Align,
+  { { File.FileType, Space, Scrollbar.Ruler, Space, Scrollbar.ScrollBar }, hl = { bg = 'normal' } },
+}
+
+local StatusLines = {
+
+  hl = function()
+    if conditions.is_active() then
+      return "StatusLine"
+    else
+      return "StatusLineNC"
+    end
+  end,
+
+  static = {
+    mode_colors_map = {
+      n = "red",
+      i = "green",
+      v = "cyan",
+      V ="cyan",
+      ["\22"] = "cyan",
+      c = "orange",
+      s = "purple",
+      S = "purple",
+      ["\19"] = "purple",
+      R = "orange",
+      r = "orange",
+      ["!"] = "red",
+      t = "green",
+    },
+    mode_color = function(self)
+      local mode = conditions.is_active() and vim.fn.mode() or "n"
+      return self.mode_colors_map[mode]
+    end,
+  },
+
+  -- the first statusline with no condition, or which condition returns true is used.
+  -- think of it as a switch case with breaks to stop fallthrough.
+  fallthrough = false,
+
+  DefaultStatusLine,
+}
+
+M = StatusLines
+
+return M
