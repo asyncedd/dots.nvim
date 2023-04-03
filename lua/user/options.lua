90018e2a0a4a85c287b4dc6250de2dd3956819f6
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:19 +0900

    new file:   lua/user/options.lua

---
+++ lua/user/options.lua
@@ -0,0 +1,12 @@
+-- Set some options for Neovim.
+-- If you want a low-overhead version of this, directly set it in `core.options`.
+-- I meant, you should really do it.
+
+local o = vim.opts
+
+-- Set options by: (ex)
+-- o.numbers = true
+-- https://neovim.io/doc/user/options.html
+-- Btw, K1SS. (Keep it simple stupid)
+
+-- EOL
