f86bb0e96639d49c76adc53bbdaca0b8c7d6390a
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:25 +0900

    feat: Flit around the buffer!

--- lua/plugins/configs/editor/leap.lua
+++ lua/plugins/configs/editor/leap.lua
@@ -21,4 +21,6 @@ return function()
   -- Try it without this setting first, you might find you don't even miss it.
   leap.opts.highlight_unlabeled_phase_one_targets = true
 
+  require("flit").setup()
+
 end
