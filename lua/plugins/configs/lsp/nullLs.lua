78fc4afd97fbba2558115418b5b0b4d6be47d4c2
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:24 +0900

    (fix) Only have Stylua since, I can't really set the globals option via the CLI. modified:   lua/plugins/configs/lsp/nullLs.lua

--- lua/plugins/configs/lsp/nullLs.lua
+++ lua/plugins/configs/lsp/nullLs.lua
@@ -4,13 +4,19 @@ return function()
   nls.setup({
     sources = {
       nls.builtins.formatting.stylua,
-      nls.builtins.diagnostics.luacheck,
-      nls.builtins.diagnostics.selene,
+      -- nls.builtins.diagnostics.luacheck,
+      -- nls.builtins.diagnostics.selene,
     },
   })
 
   require("mason-null-ls").setup({
     automatic_setup = {},
+    automatic_installation = true,
+    ensure_installed = {
+      "stylua",
+      -- "luacheck",
+      -- "selene"
+    }
   })
 
   require("mason-null-ls").setup_handlers({})
