22b283834f9eda8edcf0f96a5708d40a19cf39b1
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:15 +0900

    modified:   lua/plugins/configs/ui/heirline/init.lua modified:   lua/plugins/configs/ui/heirline/lsp.lua

--- lua/plugins/configs/ui/heirline/lsp.lua
+++ lua/plugins/configs/ui/heirline/lsp.lua
@@ -1,6 +1,7 @@
+local M = {}
 local conditions = require("heirline.conditions")
 
-local LSPActive = {
+M.LSPActive = {
   condition = conditions.lsp_attached,
   update = {'LspAttach', 'LspDetach'},
 
@@ -17,3 +18,58 @@ local LSPActive = {
   end,
   hl = { fg = "green", bold = true },
 }
+
+M.Diagnostics = {
+
+  condition = conditions.has_diagnostics,
+
+  static = {
+    error_icon = " ",
+    warn_icon = " ",
+    info_icon = " ",
+    hint_icon = " "
+  },
+
+  init = function(self)
+    self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+    self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+    self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+    self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+  end,
+
+  update = { "DiagnosticChanged", "BufEnter" },
+
+  {
+    provider = "![",
+  },
+  {
+    provider = function(self)
+      -- 0 is just another output, we can decide to print it or not!
+      return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+    end,
+    hl = { fg = "diag_error" },
+  },
+  {
+    provider = function(self)
+      return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+    end,
+    hl = { fg = "diag_warn" },
+  },
+  {
+    provider = function(self)
+      return self.info > 0 and (self.info_icon .. self.info .. " ")
+    end,
+    hl = { fg = "diag_info" },
+  },
+  {
+    provider = function(self)
+      return self.hints > 0 and (self.hint_icon .. self.hints)
+    end,
+    hl = { fg = "diag_hint" },
+  },
+  {
+    provider = "]",
+  },
+}
+
+return M
