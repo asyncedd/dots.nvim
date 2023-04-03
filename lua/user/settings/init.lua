50ed3815c9d6f018d245cc5204221c1d51cefe9f
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:24 +0900

    (feat) Have Kanagawa installed. modified:   lazy-lock.json new file:   lua/plugins/configs/ui/kanagawa.lua modified:   lua/user/settings/colorscheme.lua modified:   lua/user/settings/init.lua

--- lua/user/settings/init.lua
+++ lua/user/settings/init.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 -- Set the colorscheme you want to use here.
--- For eg. Tokyonight, edge.
+-- For eg. Tokyonight, Kanagawa.
 -- Currently you can use:
--- "catppuccin", "tokyonight"
+-- "catppuccin", "tokyonight", "kanagawa"
 -- If something breaks open an issue.
 -- If you want to use some other colorscheme, you gotta include it in the plugins spec.
 -- At: `lua/user/settings/colorscheme`.
 -- The Default is "catppuccin".
-M.colorscheme = "catppuccin"
+M.colorscheme = "kanagawa"
 
 return M
