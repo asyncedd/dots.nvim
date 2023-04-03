c68ebe7055b6d2bdd60d86ce0e40ef783264d817
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:19 +0900

    modified:   lua/plugins/configs/ui/heirline/init.lua modified:   lua/plugins/configs/ui/heirline/statuscolumn.lua

--- lua/plugins/configs/ui/heirline/statuscolumn.lua
+++ lua/plugins/configs/ui/heirline/statuscolumn.lua
@@ -1,7 +1,5 @@
 local M = {}
-local api = vim.api
 local o = vim.opt
-local gitsigns = require("gitsigns")
 
 -- Line Number
 M.line_number = {
@@ -13,41 +11,6 @@ M.line_number = {
     elseif (relnum and not num) then return "%r"
     else return "%{v:relnum?v:relnum:v:lnum}" end
   end,
-}
-
--- Signs
-M.signs = {
-  provider = "%s",
-  hl = {
-    bold = true
-  },
-}
-
-M.gitsigns = {
-  condition = function() return vim.b.nifoc_gitsigns_enabled == 1 end,
-  init = function(self)
-    local bufnr=api.nvim_get_current_buf()
-    local lnum=vim.v.lnum
-    local sign=vim.fn.sign_getplaced( bufnr, { group= "gitsigns_vimfn_signs_",id=lnum,lnum})[1].signs[1]
-    self.sign=sign
-    self.has_sign=(sign~=nil)
-  end,
-  provider = " ▏",
-  on_click = {
-    name = "heirline_statuscolumn_gitsigns",
-    callback = function(_)
-      local mouse=vim.fn.getmousepos()
-      local cursor_pos={mouse.line,0}
-      api.nvim_win_set_cursor(mouse.winid,cursor_pos)
-      vim.defer_fn(function() gitsigns.blame_line({full=true}) end, 100)
-    end
-  }
-}
-
-M.gitsigns_or_bar = {
-  condition = function() return not(vim.b.nifoc_gitsigns_enabled == 1) end,
-  provider = "▏",
-  M.gitsigns
 }
 
 return M
