6f569c380e1537c050bb4b3c161542fa7c6498e3
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:24 +0900

    test: Experiment with co-routines.

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,26 +1,55 @@
 -- lua/core/init.lua
 -- This is the place where the magic happens!
 
--- Set the <leader> key to: <Space>
--- So we can go for ex. -> `<leader>ff` to open Telescope.nvim.
--- Quite useful ain't it? :)
-vim.g.mapleader = " "
+local threads = {}
 
--- Disable builtin plugins (Using the g.loaded method so it's not real :()
-require("core.disableBuiltinPlugins")
+table.insert(threads, function ()
+  -- Set the <leader> key to: <Space>
+  -- So we can go for ex. -> `<leader>ff` to open Telescope.nvim.
+  -- Quite useful ain't it? :)
+  vim.g.mapleader = " "
+end)
 
--- Set the package.path.
--- Allow searching in `lua/plugins/configs/x.lua` or `lua/plugins/configs/x/init.lua`
-package.path = vim.fn.stdpath("config") .. "/lua/plugins/configs/?.lua;" .. package.path
-package.path = vim.fn.stdpath("config") .. "/lua/plugins/configs/?/init.lua;" .. package.path
+table.insert(threads, function ()
+  -- Disable builtin plugins (Using the g.loaded method so it's not real :()
+  require("core.disableBuiltinPlugins")
+end)
 
--- Set some options.
--- Now, we can use shortcuts like `<leader>ff` to open Telescope.nvim.
--- This will make out lives much easier! :D
-require("core.options")
+table.insert(threads, function ()
+  -- Set the package.path.
+  -- Allow searching in `lua/plugins/configs/x.lua` or `lua/plugins/configs/x/init.lua`
+  package.path = vim.fn.stdpath("config") .. "/lua/plugins/configs/?.lua;" .. package.path
+  package.path = vim.fn.stdpath("config") .. "/lua/plugins/configs/?/init.lua;" .. package.path
+end)
 
--- User provided stuff.
-require("core.user")
+table.insert(threads, function ()
+  -- Set some options.
+  -- Now, we can use shortcuts like `<leader>ff` to open Telescope.nvim.
+  -- This will make out lives much easier! :D
+  require("core.options")
+
+  -- User provided stuff.
+  require("core.user")
+end)
+
+local execute_threads = function ()
+  -- Loop through each function in the 'threads' table
+  for i, fn in ipairs(threads) do
+    -- Yield the co-routine to allow other work to be done while executing the function
+    coroutine.yield()
+
+    -- Execute the current function
+    fn()
+  end
+end
+
+-- Create a co-routine for executing the functions in the 'threads' table
+local thread_co = coroutine.create(execute_threads)
+
+-- Loop through the co-routine until it has finished executing all functions in the 'threads' table
+while coroutine.status(thread_co) ~= "dead" do
+  coroutine.resume(thread_co)
+end
 
 -- Initalize the plugin manager, lazy.nvim.
 -- We're gonna use the best tools for the job!
