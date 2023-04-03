ec3b126ef26b343fc7b5545428c9949b3efc374c
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:25 +0900

    feat: Install LSP servers via Mason.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,20 +5,30 @@ local lazyLoad = require("core.utils.lazyLoad")
 plugins[1] = {
   "catppuccin/nvim",
   name = "catppuccin",
-  lazy = lazyLoad("catppuccin"),
+  init = lazyLoad("catppuccin"),
   config = require("plugins.configs.ui.catppuccin"),
 }
 
 plugins[2] = {
   "nvim-treesitter/nvim-treesitter",
-  lazy = lazyLoad("nvim-treesitter"),
+  init = lazyLoad("nvim-treesitter"),
   config = require("plugins.configs.editor.treesitter"),
   build = ":TSUpdate",
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
 }
 
 plugins[3] = {
   "neovim/nvim-lspconfig",
-  lazy = lazyLoad("nvim-lspconfig") 
+  init = lazyLoad("nvim-lspconfig") 
+}
+
+plugins[4] = {
+  "williamboman/mason.nvim",
+  init = lazyLoad("mason.nvim"),
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = require("plugins.configs.lsp.mason"),
 }
 
 require("lazy").setup({
