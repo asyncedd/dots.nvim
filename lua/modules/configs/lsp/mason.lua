a4803858d6a527521b6f610541818aa03539933e
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:13 +0900

    edit heirline.nvim

--- lua/modules/configs/lsp/mason.lua
+++ lua/modules/configs/lsp/mason.lua
@@ -3,10 +3,11 @@ local lsp_config = require("modules.configs.lsp.lsp_config")
 return function()
   require("mason").setup()
   require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "rust_analyzer", "pyright", "astro" },
+    ensure_installed = { "lua_ls", "rust_analyzer", "pyright", "astro", "jsonls" },
   })
 
   lsp_config.setup({
     lua_ls = require("modules.configs.lsp.servers.null_ls"),
+    jsonls = {},
   })
 end
