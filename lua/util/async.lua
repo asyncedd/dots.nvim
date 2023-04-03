bd20c7cfb706b3f2073632382ea265651f4f5a0f
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:13 +0900

    revert to old async

--- lua/util/async.lua
+++ lua/util/async.lua
@@ -4,42 +4,50 @@ local M = {}
 function M.async(func)
   return function(...)
     local co = coroutine.create(func)
-    local nargs = select("#", ...)
-    local args = {...}
+    local args = ({...})
+    local resume = function()
+      local ok, result = xpcall(function()
+        return ({coroutine.resume(co, unpack(args, 1, #args))})
+      end, debug.traceback)
+      if ok then
+        return unpack(result, 1, #result)
+      else
+        error(result[1])
+      end
+    end
     return {
       await = function()
-        local ok, result = coroutine.resume(co, unpack(args, 1, nargs))
-        if ok then
-          return result
+        local result
+        vim.schedule(function()
+          result = ({resume()})
+        end)
+        coroutine.yield()
+        if #result == 0 then
+          return nil
+        elseif #result == 1 then
+          return result[1]
         else
-          error(result)
+          return unpack(result, 1, #result)
         end
       end
     }
   end
 end
 
+
 -- Run a table of functions asynchronously in parallel
 function M.parallel(funcs)
-  local co = coroutine.create(function()
-    local results = {}
-    for i, func in ipairs(funcs) do
-      results[i] = M.async(func)()
-    end
-    coroutine.yield(results)
-  end)
+  local results = {}
+  for i, func in ipairs(funcs) do
+    results[i] = M.async(coroutine.wrap(func))()
+  end
   return {
     await = function()
-      local ok, results = coroutine.resume(co)
-      if ok then
-        local final_results = {}
-        for i, result in ipairs(results) do
-          final_results[i] = result.await()
-        end
-        return final_results
-      else
-        error(results)
+      local final_results = {}
+      for i, result in ipairs(results) do
+        final_results[i] = result.await()
       end
+      return final_results
     end
   }
 end
