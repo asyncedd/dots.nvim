89254461c89ee413f8e69934db76db9d42770bbb
asyncedd <isynqquwu@proton.me>
2023-04-03 20:27:14 +0900

    modified:   lazy-lock.json 	modified:   lua/configs/ui/animate.lua 	new file:   lua/plugins/animate.lua

--- lua/configs/ui/animate.lua
+++ lua/configs/ui/animate.lua
@@ -1,6 +1,9 @@
 return function()
   local status, animate = pcall(require, "mini.animate")
+  local status1, flirt = pcall(require, "flirt")
   if (not status) then return end
+  if (not status1) then return end
 
   animate.setup()
+  flirt.setup()
 end
