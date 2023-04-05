968eb19f2a3f129f7c5df065f71ae71a31cbaada
asyncedd <isynqquwu@proton.me>
2023-04-05 09:58:26 +0900

    feat(sessions): move to olimorris/persisted.nvim
    Persisted.nvim is more modern :)

--- lua/plugins/configs/tools/telescope.lua
+++ lua/plugins/configs/tools/telescope.lua
@@ -1,17 +1,9 @@
 local telescope = require("telescope")
 
-telescope.setup({
-  extensions = {
-    undo = {
-      -- telescope-undo.nvim config, see below
-    },
-  },
-})
+telescope.setup({})
 
-telescope.load_extension("undo")
-telescope.load_extension("yank_history")
-telescope.load_extension("possession")
 telescope.load_extension("neoclip")
+telescope.load_extension("persisted")
 
 local keymap = vim.keymap.set
 
@@ -21,9 +13,6 @@ keymap("n", "<leader>ff", function()
   require("telescope.builtin").find_files()
 end)
 
-keymap("n", "<leader>u", "<cmd>Telescope undo<cr>")
-
-keymap("n", "<leader>yh", "<cmd>Telescope yank_history<cr>")
-keymap("n", "<leader>pn", "<cmd>Telescope possession list<cr>")
-keymap("n", "<leader>ps", ":PossessionSave ")
 keymap("n", "<leader>c", "<cmd>Telescope neoclip<cr>")
+
+keymap("n", "<leader>pe", "<cmd>Telescope persisted<cr>")
