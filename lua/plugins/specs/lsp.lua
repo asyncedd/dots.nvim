29c0713dae189c7c0f6700c3909aebba6631e1a4
asyncedd <isynqquwu@proton.me>
2023-04-05 09:44:35 +0900

    feat: Make plugin specs minimal via array[x]
    Make plugin specs minimal for readablity :)

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -1,68 +1,73 @@
 local lazyLoad = require("core.utils.lazyLoad")
 
-return {
-  {
-    "neovim/nvim-lspconfig",
-    init = lazyLoad("nvim-lspconfig"),
-    config = function()
-      require("lsp.config")
-    end,
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        config = function()
-          require("lsp.mason")
-        end,
-        -- init = lazyLoad("mason.nvim"),
-        build = "<cmd>Mason<cmd>",
-        cmd = {
-          "Mason",
-        },
-        dependencies = {
-          "williamboman/mason-lspconfig.nvim",
-        },
-        cond = vim.g.bootstrap,
-        -- event = "BufReadPre",
+local lsp = {}
+
+lsp[1] = {
+  "neovim/nvim-lspconfig",
+  init = lazyLoad("nvim-lspconfig"),
+  config = function()
+    require("lsp.config")
+  end,
+  dependencies = {
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        require("lsp.mason")
+      end,
+      -- init = lazyLoad("mason.nvim"),
+      build = "<cmd>Mason<cmd>",
+      cmd = {
+        "Mason",
       },
-      "folke/neodev.nvim",
-      {
-        "glepnir/lspsaga.nvim",
-        config = function()
-          require("lsp.saga")
-        end,
-        dependencies = {
-          { "nvim-tree/nvim-web-devicons" },
-          { "nvim-treesitter/nvim-treesitter" },
-        },
+      dependencies = {
+        "williamboman/mason-lspconfig.nvim",
       },
-      "MunifTanjim/nui.nvim",
-      "SmiteshP/nvim-navic",
-      {
-        "VidocqH/lsp-lens.nvim",
-        config = true,
+      cond = vim.g.bootstrap,
+      -- event = "BufReadPre",
+    },
+    "folke/neodev.nvim",
+    {
+      "glepnir/lspsaga.nvim",
+      config = function()
+        require("lsp.saga")
+      end,
+      dependencies = {
+        { "nvim-tree/nvim-web-devicons" },
+        { "nvim-treesitter/nvim-treesitter" },
       },
     },
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    init = lazyLoad("null-ls.nvim"),
-    config = function()
-      require("lsp.null")
-    end,
-    dependencies = {
-      "jay-babu/mason-null-ls.nvim",
+    "MunifTanjim/nui.nvim",
+    "SmiteshP/nvim-navic",
+    {
+      "VidocqH/lsp-lens.nvim",
+      config = true,
     },
-  },
-  {
-    "smjonas/inc-rename.nvim",
-    config = true,
-    keys = {
-      { "<leader>rn", ":IncRename " },
-    },
-  },
-  {
-    "chikko80/error-lens.nvim",
-    event = "VeryLazy",
-    config = true,
   },
 }
+
+lsp[2] = {
+  "jose-elias-alvarez/null-ls.nvim",
+  init = lazyLoad("null-ls.nvim"),
+  config = function()
+    require("lsp.null")
+  end,
+  dependencies = {
+    "jay-babu/mason-null-ls.nvim",
+  },
+}
+
+lsp[3] = {
+  "smjonas/inc-rename.nvim",
+  config = true,
+  keys = {
+    { "<leader>rn", ":IncRename " },
+  },
+}
+
+lsp[4] = {
+  "chikko80/error-lens.nvim",
+  event = "VeryLazy",
+  config = true,
+}
+
+return lsp
