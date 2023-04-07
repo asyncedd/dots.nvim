35264e02328dea7ccbd1ce210f073dbfc0099d89
asyncedd <isynqquwu@proton.me>
2023-04-07 18:13:41 +0900

    (redo): Initial commit.

---
+++ lua/settings/options.lua
@@ -0,0 +1,15 @@
+local o = vim.opt
+
+-- 1. UI
+-- Enable line numbers.
+o.number = true
+
+-- Enable relative numbers.
+o.relativenumber = true
+
+-- enable TermGUI colors
+o.tgc = true
+
+-- 2. Editing
+-- Set the shift (indents) width.
+o.shiftwidth = 2
