19f8ce9b6a91f7140e5482cf20a815d96e8ad519
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:06 +0900

    modified:   lua/configs/editor/treesitter.lua modified:   lua/plugins/treesitter.lua

--- lua/configs/editor/treesitter.lua
+++ lua/configs/editor/treesitter.lua
@@ -1,6 +1,12 @@
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then return end
 
+local markid = require("markid")
+
+markid.colors = {
+  bright = {"#f5c2e7", "#fab387", "#f9e2af", "#f5c0e8", "#94e2d5", "#89dceb", "#74c7ec", "#b4befe", "#cba6f7", "#c5e1a5" },
+}
+
 treesitter.setup({
   auto_install = true, -- Auto-install missing parsers. (once you open a file that requires it.)
   highlight = {
@@ -10,6 +16,7 @@ treesitter.setup({
     enable = true
   },
   markid = {
-    enable = true
+    enable = true,
+    colors = markid.colors.bright
   }
 })
