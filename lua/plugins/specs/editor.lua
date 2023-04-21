28f492356286c42e9d15cbb5fc124076044d7ecb
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:24 +0900

    feat(spider): dont web on delete
    Don't web on delete since, it's only useful for motions. Sorry.

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -121,28 +121,28 @@ editor[5] = {
       function()
         require("spider").motion("w")
       end,
-      mode = { "n", "o", "x" },
+      mode = { "n", "o" },
     },
     {
       "e",
       function()
         require("spider").motion("e")
       end,
-      mode = { "n", "o", "x" },
+      mode = { "n", "o" },
     },
     {
       "b",
       function()
         require("spider").motion("b")
       end,
-      mode = { "n", "o", "x" },
+      mode = { "n", "o" },
     },
     {
       "ge",
       function()
         require("spider").motion("ge")
       end,
-      mode = { "n", "o", "x" },
+      mode = { "n", "o" },
     },
   },
 }
