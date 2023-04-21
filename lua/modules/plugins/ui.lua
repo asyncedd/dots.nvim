d3437cb5b84bb132287556a221e2836a2afb8b91
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:14 +0900

    perf: Don't add that extra lazy!

--- lua/modules/plugins/ui.lua
+++ lua/modules/plugins/ui.lua
@@ -1,13 +1,12 @@
 local ui = {}
 
-ui["catppuccin/nvim"] = {
-  lazy = true,
-}
+ui["catppuccin/nvim"] = {}
 
 local lazy = {}
 
 for repo, _ in pairs(ui) do
-  lazy[#lazy+1] = {repo, lazy=true}
+  lazy[#lazy+1] = {repo, lazy = true}
 end
 
 return lazy
+
