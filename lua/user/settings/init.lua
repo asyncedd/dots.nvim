becdf9108f6f0f1381e34d333ebfbae0367ce80a
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:13 +0900

    (fix) Dropped support for edge.nvim modified:   lua/plugins/configs/ui/heirline/colors.lua modified:   lua/user/settings/colorscheme.lua modified:   lua/user/settings/init.lua

--- lua/user/settings/init.lua
+++ lua/user/settings/init.lua
@@ -3,7 +3,7 @@ local M = {}
 -- Set the colorscheme you want to use here.
 -- For eg. Tokyonight, edge.
 -- Currently you can use:
--- "catppuccin", "tokyonight", "edge"
+-- "catppuccin", "tokyonight"
 -- If something breaks open an issue.
 -- If you want to use some other colorscheme, you gotta include it in the plugins spec.
 -- At: `lua/user/settings/colorscheme`.
