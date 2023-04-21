855548dd78e0ab90fc9979e9b71d434177f7a10c
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:10 +0900

    modified:   lua/plugins/configs/completion/cmp.lua

--- lua/plugins/configs/completion/cmp.lua
+++ lua/plugins/configs/completion/cmp.lua
@@ -51,9 +51,16 @@ cmp.setup({
     },
   },
   mapping = cmp.mapping.preset.insert({
-    ["<CR>"] = cmp.mapping.confirm({
-      select = true,
-      behavior = cmp.ConfirmBehavior.Replace,
+    ["<CR>"] = cmp.mapping({
+      i = function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+        else
+          fallback()
+        end
+      end,
+      s = cmp.mapping.confirm({ select = true }),
+      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
     }),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
