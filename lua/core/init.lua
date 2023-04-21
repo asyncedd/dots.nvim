f2625d222af526ed7401dbf996f67d98f080ea6e
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:06 +0900

    modified:   lazy-lock.json 	modified:   lua/configs/editor/treesitter.lua 	modified:   lua/core/init.lua 	modified:   lua/core/load_ui_plugins.lua modified:   lua/core/options.lua 	modified:   lua/plugins/treesitter.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,9 +1,15 @@
 local vim = vim -- Avoid expensive global searching.
+local o = vim.api.nvim_set_option_value
 
 local M = {}
 
 M.setup = function()
   vim.g.mapleader = " "
+  o("number", true, {}) -- Enable Line numbers
+  o("cursorline", true, {})
+  o("list", true, {})
+  o("listchars", "eol:↴,space:⋅", {})
+
   require("core.lazy")
   require("core.load_ui_plugins")
   vim.defer_fn(function ()
--- lua/core/load_ui_plugins.lua
+++ lua/core/load_ui_plugins.lua
@@ -1,10 +1,6 @@
 local vim = vim -- Avoid using expensive global searching.
 local o = vim.api.nvim_set_option_value
 
-o("number", true, {}) -- Enable Line numbers
-o("cursorline", true, {})
-o("list", true, {})
-o("listchars", "eol:↴,space:⋅", {})
 
 vim.api.nvim_command("colorscheme catppuccin")
 
@@ -15,4 +11,3 @@ vim.api.nvim_create_autocmd({ "BufReadPost" }, {
     end, 0)
   end
 })
-
