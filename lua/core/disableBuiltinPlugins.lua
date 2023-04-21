0cdf45fa0198ba1456769dcfe8fe53b52fcbf412
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:11 +0900

    new file:   lua/core/disableBuiltinPlugins.lua modified:   lua/core/init.lua

---
+++ lua/core/disableBuiltinPlugins.lua
@@ -0,0 +1,28 @@
+-- disable builtins plugins
+local disabled_built_ins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "matchit",
+  "netrw",
+  "netrwFileHandlers",
+  "loaded_remote_plugins",
+  "loaded_tutor_mode_plugin",
+  "netrwPlugin",
+  "netrwSettings",
+  "rrhelper",
+  "spellfile_plugin",
+  "tar",
+  "tarPlugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+  "matchparen", -- matchparen.nvim disables the default
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+  vim.g["loaded_" .. plugin] = 1
+end
