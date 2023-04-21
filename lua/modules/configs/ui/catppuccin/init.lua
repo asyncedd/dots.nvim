d6fc5980e9deb56783eed464f4bf754b0ca380e2
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:04 +0900

    setuping catppuccin takes time

--- lua/modules/configs/ui/catppuccin/init.lua
+++ lua/modules/configs/ui/catppuccin/init.lua
@@ -1,17 +1,17 @@
 return function()
-  require("catppuccin").setup({
-    flavour = "mocha", -- Can be one of: latte, frappe, macchiato, mocha
-    background = { light = "latte", dark = "mocha" },
-    dim_inactive = {
-      enabled = true,
-      -- Dim inactive splits/windows/buffers.
-      -- NOT recommended if you use old palette (a.k.a., mocha).
-      shade = "dark",
-      percentage = 0.15,
-    },
-    highlight_overrides = {
-      mocha = require("ui.catppuccin.highlight_overrides")
-    },
-    integrations = require("ui.catppuccin.integrations")
-  })
+  -- require("catppuccin").setup({
+  --   flavour = "mocha", -- Can be one of: latte, frappe, macchiato, mocha
+  --   background = { light = "latte", dark = "mocha" },
+  --   dim_inactive = {
+  --     enabled = true,
+  --     -- Dim inactive splits/windows/buffers.
+  --     -- NOT recommended if you use old palette (a.k.a., mocha).
+  --     shade = "dark",
+  --     percentage = 0.15,
+  --   },
+  --   highlight_overrides = {
+  --     mocha = require("ui.catppuccin.highlight_overrides")
+  --   },
+  --   integrations = require("ui.catppuccin.integrations")
+  -- })
 end
