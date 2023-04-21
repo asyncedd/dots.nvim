ac1ae876025d8e461373ad1f581bb8a90ebb2dc1
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:02 +0900

    Edit opts.lua

--- lua/options/opts.lua
+++ lua/options/opts.lua
@@ -1,13 +1,17 @@
 local M = {
   -- General
   shiftwidth = 2, -- Set the indents to 2.
-  number = true, -- Enable line numbers
   undofile = true, -- Enable persistent undo's
   -- Don't store backup when overwriting the file,
   backup = false,
   writebackup = false,
   mouse = "a", -- Enable mouse for all modes
+
   -- Apperance
+  breakindent = true, -- Indent wrapped lines to match line start
+  cursorline = true -- Highlight the line we're currently editting.
+  linebreak = true -- Break long lines into multiple. visually.
+  number = true -- Enable line numbers
 }
 
 return M
