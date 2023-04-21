3df8901cef4e6217dcd3249f976f2f443fdee2f5
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:24 +0900

    feat(catppuccin): highlight variables in pink
    Highlight variables in pink!

---
+++ lua/plugins/configs/ui/catppuccin.lua
@@ -0,0 +1,14 @@
+require("catppuccin").setup({
+  custom_highlights = function(ctp)
+    return {
+      Pmenu = { bg = ctp.crust, fg = "" },
+
+      ["@lsp.type.parameter"] = { fg = ctp.red },
+      ["@lsp.type.variable"] = { fg = ctp.pink },
+      ["@lsp.type.function"] = { fg = ctp.blue },
+
+      ["@variable"] = { fg = ctp.pink },
+    }
+  end,
+})
+
