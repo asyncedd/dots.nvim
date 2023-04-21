1675328eb6a9e807c543a5b54ef49370d1780366
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:24 +0900

    feat(yamlls): add yamlls
    Add yamlls :)

--- lua/plugins/configs/lsp/config.lua
+++ lua/plugins/configs/lsp/config.lua
@@ -74,6 +74,10 @@ lspconfig.emmet_ls.setup({
   },
 })
 
+lspconfig.yamlls.setup({
+  capabilities = capabilities,
+})
+
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
