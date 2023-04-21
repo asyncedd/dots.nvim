277ea8fd656ab86fb61e3b1c5eb162600bb6650d
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:23 +0900

    feat: Make plugin specs minimal via array[x]
    Make plugin specs minimal for readablity :)

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -16,258 +16,275 @@ local fileLazyLoad = function(plugin, ft) -- CREDIT: NvChad.
   })
 end
 
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    init = lazyLoad("nvim-treesitter"),
-    config = function()
-      require("editor.treesitter")
-    end,
-    build = ":TSUpdate",
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    dependencies = {
-      "HiPhish/nvim-ts-rainbow2",
-    },
-  },
-  {
-    "andymass/vim-matchup",
-    config = function()
-      -- Don't show the matchup's matchparen.
-      vim.g.matchup_matchparen_offscreen = { method = "off" }
-    end,
-    init = lazyLoad("vim-matchup"),
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    event = {
-      "InsertEnter",
-      "CmdlineEnter",
-    },
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-          {
-            "rafamadriz/friendly-snippets",
-            config = function()
-              require("luasnip.loaders.from_vscode").lazy_load()
-            end,
-          },
-          {
-            "honza/vim-snippets",
-            config = function()
-              require("luasnip.loaders.from_snipmate").lazy_load()
-            end,
-          },
-        },
-        config = function()
-          require("completion.luasnip")
-        end,
-      },
-      {
-        "onsails/lspkind.nvim",
-        config = function()
-          require("completion.lspkind")
-        end,
-      },
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-buffer",
-      {
-        "jcdickinson/codeium.nvim",
-        config = true,
-        dependencies = {
-          "nvim-lua/plenary.nvim",
-          {
-            "jcdickinson/http.nvim",
-            build = "cargo build --workspace --release",
-          },
-        },
-      },
-      {
-        "tzachar/cmp-tabnine",
-        build = "./install.sh",
-        dependencies = {
-          "hrsh7th/nvim-cmp",
-        },
-        config = function()
-          require("completion.cmpTabnine")
-        end,
-      },
-      "hrsh7th/cmp-nvim-lua",
-      "windwp/nvim-autopairs",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-path",
-    },
-    config = function()
-      require("completion.cmp")
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("editor.gitsigns")
-    end,
-    init = lazyLoad("gitsigns.nvim"),
-  },
-  {
-    "echasnovski/mini.nvim",
-    config = function()
-      require("plugins.configs.mini")
-    end,
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    init = lazyLoad("mini.nvim"),
-    event = "InsertEnter",
-  },
-  {
-    "chrisgrieser/nvim-spider",
-    keys = {
-      {
-        "w",
-        function()
-          require("spider").motion("w")
-        end,
-        { "n", "o", "x" },
-      },
-      {
-        "e",
-        function()
-          require("spider").motion("e")
-        end,
-        { "n", "o", "x" },
-      },
-      {
-        "b",
-        function()
-          require("spider").motion("b")
-        end,
-        { "n", "o", "x" },
-      },
-      {
-        "ge",
-        function()
-          require("spider").motion("ge")
-        end,
-        { "n", "o", "x" },
-      },
-    },
-  },
-  {
-    "ggandor/leap.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("editor.leap")
-    end,
-    dependencies = {
-      "ggandor/flit.nvim",
-      config = true,
-      dependencies = "tpope/vim-repeat",
-    },
-  },
-  {
-    "LeonHeidelbach/trailblazer.nvim",
-    config = true,
-    event = "VeryLazy",
-  },
-  {
-    "LudoPinelli/comment-box.nvim",
-    config = true,
-    keys = {
-      {
-        "<leader>cb",
-        function()
-          require("comment-box").lcbox(10)
-        end,
-      },
-    },
-  },
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("editor.autopairs")
-    end,
-    event = "InsertEnter",
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("editor.autotag")
-    end,
-    event = "InsertEnter",
-  },
-  {
-    "gbprod/yanky.nvim",
-    keys = {
-      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" } },
-      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" } },
-      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" } },
-      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" } },
-      { "<c-n>", "<Plug>(YankyCycleForward)", mode = { "n", "x" } },
-      { "<c-p>", "<Plug>(YankyCycleBackward)", mode = { "n", "x" } },
-    },
-    config = true,
-  },
-  {
-    "nvim-neorg/neorg",
-    config = function()
-      vim.schedule(function()
-        require("neorg").setup({
-          load = {
-            ["core.defaults"] = {}, -- Loads default behaviour
-            ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-            ["core.norg.dirman"] = {
-              config = {
-                workspaces = {
-                  notes = "~/notes",
-                },
-              },
-            }, -- Manages Neorg workspaces
-          },
-        })
-      end)
-    end,
-    ft = { "norg" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-      "kevinhwang91/promise-async",
-    },
-    keys = {
-      {
-        "zR",
-        function()
-          require("ufo").openAllFolds()
-        end,
-      },
-      {
-        "zM",
-        function()
-          require("ufo").closeAllFolds()
-        end,
-      },
-    },
-    config = function()
-      require("editor.ufo")
-    end,
-    event = "VeryLazy",
-  },
-  {
-    "sbdchd/neoformat",
-    cmd = {
-      "Neoformat",
-    },
-  },
-  {
-    "fladson/vim-kitty",
-    init = function()
-      fileLazyLoad("vim-kitty", "conf")
-    end,
+local editor = {}
+
+editor[1] = {
+  "nvim-treesitter/nvim-treesitter",
+  init = lazyLoad("nvim-treesitter"),
+  config = function()
+    require("editor.treesitter")
+  end,
+  build = ":TSUpdate",
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+  dependencies = {
+    "HiPhish/nvim-ts-rainbow2",
   },
 }
+
+editor[2] = {
+  "andymass/vim-matchup",
+  config = function()
+    -- Don't show the matchup's matchparen.
+    vim.g.matchup_matchparen_offscreen = { method = "off" }
+  end,
+  init = lazyLoad("vim-matchup"),
+}
+
+editor[3] = {
+  "hrsh7th/nvim-cmp",
+  event = {
+    "InsertEnter",
+    "CmdlineEnter",
+  },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    {
+      "L3MON4D3/LuaSnip",
+      dependencies = {
+        {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
+        },
+        {
+          "honza/vim-snippets",
+          config = function()
+            require("luasnip.loaders.from_snipmate").lazy_load()
+          end,
+        },
+      },
+      config = function()
+        require("completion.luasnip")
+      end,
+    },
+    {
+      "onsails/lspkind.nvim",
+      config = function()
+        require("completion.lspkind")
+      end,
+    },
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-buffer",
+    {
+      "jcdickinson/codeium.nvim",
+      config = true,
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+          "jcdickinson/http.nvim",
+          build = "cargo build --workspace --release",
+        },
+      },
+    },
+    {
+      "tzachar/cmp-tabnine",
+      build = "./install.sh",
+      dependencies = {
+        "hrsh7th/nvim-cmp",
+      },
+      config = function()
+        require("completion.cmpTabnine")
+      end,
+    },
+    "hrsh7th/cmp-nvim-lua",
+    "windwp/nvim-autopairs",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-path",
+  },
+  config = function()
+    require("completion.cmp")
+  end,
+}
+
+editor[4] = {
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("editor.gitsigns")
+  end,
+  init = lazyLoad("gitsigns.nvim"),
+}
+
+editor[5] = {
+  "echasnovski/mini.nvim",
+  config = function()
+    require("plugins.configs.mini")
+  end,
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  init = lazyLoad("mini.nvim"),
+  event = "InsertEnter",
+}
+
+editor[6] = {
+  "chrisgrieser/nvim-spider",
+  keys = {
+    {
+      "w",
+      function()
+        require("spider").motion("w")
+      end,
+      { "n", "o", "x" },
+    },
+    {
+      "e",
+      function()
+        require("spider").motion("e")
+      end,
+      { "n", "o", "x" },
+    },
+    {
+      "b",
+      function()
+        require("spider").motion("b")
+      end,
+      { "n", "o", "x" },
+    },
+    {
+      "ge",
+      function()
+        require("spider").motion("ge")
+      end,
+      { "n", "o", "x" },
+    },
+  },
+}
+
+editor[7] = {
+  "ggandor/leap.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("editor.leap")
+  end,
+  dependencies = {
+    "ggandor/flit.nvim",
+    config = true,
+    dependencies = "tpope/vim-repeat",
+  },
+}
+
+editor[8] = {
+  "LeonHeidelbach/trailblazer.nvim",
+  config = true,
+  event = "VeryLazy",
+}
+
+editor[9] = {
+  "LudoPinelli/comment-box.nvim",
+  config = true,
+  keys = {
+    {
+      "<leader>cb",
+      function()
+        require("comment-box").lcbox(10)
+      end,
+    },
+  },
+}
+
+editor[10] = {
+  "windwp/nvim-autopairs",
+  config = function()
+    require("editor.autopairs")
+  end,
+  event = "InsertEnter",
+}
+
+editor[11] = {
+  "windwp/nvim-ts-autotag",
+  config = function()
+    require("editor.autotag")
+  end,
+  event = "InsertEnter",
+}
+
+editor[12] = {
+  "gbprod/yanky.nvim",
+  keys = {
+    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" } },
+    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" } },
+    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" } },
+    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" } },
+    { "<c-n>", "<Plug>(YankyCycleForward)", mode = { "n", "x" } },
+    { "<c-p>", "<Plug>(YankyCycleBackward)", mode = { "n", "x" } },
+  },
+  config = true,
+}
+
+editor[13] = {
+  "nvim-neorg/neorg",
+  config = function()
+    vim.schedule(function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.norg.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          }, -- Manages Neorg workspaces
+        },
+      })
+    end)
+  end,
+  ft = { "norg" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
+
+editor[14] = {
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+  },
+  keys = {
+    {
+      "zR",
+      function()
+        require("ufo").openAllFolds()
+      end,
+    },
+    {
+      "zM",
+      function()
+        require("ufo").closeAllFolds()
+      end,
+    },
+  },
+  config = function()
+    require("editor.ufo")
+  end,
+  event = "VeryLazy",
+}
+
+editor[15] = {
+  "sbdchd/neoformat",
+  cmd = {
+    "Neoformat",
+  },
+}
+
+editor[16] = {
+  "fladson/vim-kitty",
+  init = function()
+    fileLazyLoad("vim-kitty", "conf")
+  end,
+}
+
+return editor
--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -1,117 +1,125 @@
-return {
-  {
-    "nvim-telescope/telescope.nvim",
-    config = function()
-      require("tools.telescope")
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "debugloop/telescope-undo.nvim",
-      {
-        "jedrzejboczar/possession.nvim",
-        config = function()
-          require("possession").setup({})
-        end,
-      },
-      {
-        "AckslD/nvim-neoclip.lua",
-        dependencies = {
-          {
-            "kkharji/sqlite.lua",
-            module = "sqlite",
-          },
-          -- you'll need at least one of these
-          -- {'nvim-telescope/telescope.nvim'},
-          -- {'ibhagwan/fzf-lua'},
+local tools = {}
+
+tools[1] = {
+  "nvim-telescope/telescope.nvim",
+  config = function()
+    require("tools.telescope")
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "debugloop/telescope-undo.nvim",
+    {
+      "jedrzejboczar/possession.nvim",
+      config = function()
+        require("possession").setup({})
+      end,
+    },
+    {
+      "AckslD/nvim-neoclip.lua",
+      dependencies = {
+        {
+          "kkharji/sqlite.lua",
+          module = "sqlite",
         },
-        config = function()
-          require("neoclip").setup({
-            history = 100000,
-            enable_persistent_history = true,
-            length_limit = 1048576,
-            continuous_sync = true,
-            db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
-          })
-        end,
+        -- you'll need at least one of these
+        -- {'nvim-telescope/telescope.nvim'},
+        -- {'ibhagwan/fzf-lua'},
       },
-    },
-    event = "VeryLazy",
-  },
-  {
-    "thePrimeagen/harpoon",
-    config = function()
-      require("tools.harpoon")
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    event = "VeryLazy",
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-      require("tools.nvimtree")
-    end,
-    cmd = {
-      "NvimTreeToggle",
-      "NvimTreeFocus",
-    },
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      { "<leader>tr", "<cmd>NvimTreeToggle<cr>" },
+      config = function()
+        require("neoclip").setup({
+          history = 100000,
+          enable_persistent_history = true,
+          length_limit = 1048576,
+          continuous_sync = true,
+          db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
+        })
+      end,
     },
   },
-  {
-    "folke/trouble.nvim",
-    config = true,
-    keys = {
-      { "<leader>xx", "<cmd>TroubleToggle<cr>" },
-      { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
-      { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
-      { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
-      { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
-      { "gR", "<cmd>TroubleToggle lsp_references<cr>" },
+  event = "VeryLazy",
+}
+
+tools[2] = {
+  "thePrimeagen/harpoon",
+  config = function()
+    require("tools.harpoon")
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  event = "VeryLazy",
+}
+
+tools[3] = {
+  "nvim-tree/nvim-tree.lua",
+  config = function()
+    require("tools.nvimtree")
+  end,
+  cmd = {
+    "NvimTreeToggle",
+    "NvimTreeFocus",
+  },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    { "<leader>tr", "<cmd>NvimTreeToggle<cr>" },
+  },
+}
+
+tools[4] = {
+  "folke/trouble.nvim",
+  config = true,
+  keys = {
+    { "<leader>xx", "<cmd>TroubleToggle<cr>" },
+    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
+    { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
+    { "gR", "<cmd>TroubleToggle lsp_references<cr>" },
+  },
+}
+
+tools[5] = {
+  "TimUntersberger/neogit",
+  cmd = { "Neogit" },
+  config = true,
+  keys = {
+    {
+      "<leader>gt",
+      function()
+        require("neogit").open({ kind = "split" })
+      end,
     },
   },
-  {
-    "TimUntersberger/neogit",
-    cmd = { "Neogit" },
-    config = true,
-    keys = {
-      {
-        "<leader>gt",
-        function()
-          require("neogit").open({ kind = "split" })
-        end,
-      },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
   },
-  {
-    "rafcamlet/nvim-luapad",
-    lazy = true,
-    cmd = {
-      "Luapad",
-      "LuaRun",
-      "Lua",
-    },
+}
+
+tools[6] = {
+  "rafcamlet/nvim-luapad",
+  lazy = true,
+  cmd = {
+    "Luapad",
+    "LuaRun",
+    "Lua",
   },
-  {
-    "sindrets/diffview.nvim",
-    config = true,
-    keys = {
-      { "<leader>dv", "<cmd>DiffviewOpen<cr>" },
-    },
+}
+
+tools[7] = {
+  "sindrets/diffview.nvim",
+  config = true,
+  keys = {
+    { "<leader>dv", "<cmd>DiffviewOpen<cr>" },
   },
+}
+
+tools[8] =
   {
     "ThePrimeagen/vim-be-good",
     cmd = { "VimBeGood" },
-  },
-  {
+  }, {
     "kdheepak/lazygit.nvim",
     cmd = {
       "LazyGit",
@@ -120,37 +128,40 @@ return {
     keys = {
       { "<leader>lg", "<cmd>LazyGit<cr>" },
     },
+  }
+
+tools[9] = {
+  "pwntester/octo.nvim",
+  config = true,
+  cmd = {
+    "Octo",
   },
-  {
-    "pwntester/octo.nvim",
-    config = true,
-    cmd = {
-      "Octo",
-    },
-    keys = {
-      { "<leader>gh", ":Octo " },
-    },
-  },
-  {
-    "ibhagwan/fzf-lua",
-    -- config = true,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    build = "fzf#install()",
-    keys = {
-      { "<leader>gr", "<cmd>FzfLua grep<cr>" },
-      { "<leader>sw", "<cmd>FzfLua grep_cword<cr>" },
-      { "<leader>sW", "<cmd>FzfLua grep_cWORD<cr>" },
-      { "<leader>sv", "<cmd>FzfLua grep_visual<cr>", mode = "v" },
-      { "<leader>lr", "<cmd>FzfLua live_grep_native<cr>" },
-      { "<leader>lr", "<cmd>FzfLua lsp_references<cr>" },
-      { "<leader>ld", "<cmd>FzfLua lsp_definitions<cr>" },
-      { "<leader>gc", "<cmd>FzfLua git_commits<cr>" },
-      { "<leader>gs", "<cmd>FzfLua git_status<cr>" },
-      { "<leader>gf", "<cmd>FzfLua git_files<cr>" },
-      { "<leader>gb", "<cmd>FzfLua git_bcommits<cr>" },
-      { "<leader>br", "<cmd>FzfLua git_branches<cr>" },
-    },
+  keys = {
+    { "<leader>gh", ":Octo " },
   },
 }
+
+tools[10] = {
+  "ibhagwan/fzf-lua",
+  -- config = true,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  build = "fzf#install()",
+  keys = {
+    { "<leader>gr", "<cmd>FzfLua grep<cr>" },
+    { "<leader>sw", "<cmd>FzfLua grep_cword<cr>" },
+    { "<leader>sW", "<cmd>FzfLua grep_cWORD<cr>" },
+    { "<leader>sv", "<cmd>FzfLua grep_visual<cr>", mode = "v" },
+    { "<leader>lr", "<cmd>FzfLua live_grep_native<cr>" },
+    { "<leader>lr", "<cmd>FzfLua lsp_references<cr>" },
+    { "<leader>ld", "<cmd>FzfLua lsp_definitions<cr>" },
+    { "<leader>gc", "<cmd>FzfLua git_commits<cr>" },
+    { "<leader>gs", "<cmd>FzfLua git_status<cr>" },
+    { "<leader>gf", "<cmd>FzfLua git_files<cr>" },
+    { "<leader>gb", "<cmd>FzfLua git_bcommits<cr>" },
+    { "<leader>br", "<cmd>FzfLua git_branches<cr>" },
+  },
+}
+
+return tools
