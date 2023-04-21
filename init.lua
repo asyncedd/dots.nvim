d7d3d35f217d9146a0a82bf9d019d313c3645d94
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:06 +0900

    Add Dein.nvim bootstrapper 	modified:   init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
--- vim.go.loadplugins = false
+vim.go.loadplugins = false
 -- Call setup
-require("core").setup()
+local deinpath = vim.fn.stdpath("cache") .. "dein"
+if not vim.loop.fs_stat(deinpath) then
+  -- stylua: ignore
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/Shougo/dein.vim",
+    -- "--branch=stable",
+    deinpath
+  })
+end
+
+vim.opt.rtp:prepend(deinpath)
+-- require("core").setup()
