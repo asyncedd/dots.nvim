d95276777de55935dcdffa1bc872682062f55e57
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:15 +0900

    chore: small fix.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -199,21 +199,27 @@ plugins[21] = {
 
 plugins[22] = {
   "folke/trouble.nvim",
-  config = true,
-  keys = {
-    { "n", "<leader>xx", "<cmd>TroubleToggle<cr>", {
-      silent = true, noremap = true } },
-    { "n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", {
-      silent = true, noremap = true } },
-    { "n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", {
-      silent = true, noremap = true }},
-    { "n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", {
-      silent = true, noremap = true } },
-    { "n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-    { silent = true, noremap = true } },
-    { "n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-    { silent = true, noremap = true } }
-  }
+  config = function ()
+    require("trouble").setup()
+    vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+    {silent = true, noremap = true}
+    )
+    vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+    {silent = true, noremap = true}
+    )
+    vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+    {silent = true, noremap = true}
+    )
+    vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+    {silent = true, noremap = true}
+    )
+    vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+    {silent = true, noremap = true}
+    )
+    vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+    {silent = true, noremap = true}
+    )
+  end,
 }
 
 require("lazy").setup({
