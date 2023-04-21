baa55b52b0838460e9c8ec750ef31d24cd030017
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:18 +0900

    perf: Improve performance.

--- lua/plugins/configs/lsp/config.lua
+++ lua/plugins/configs/lsp/config.lua
@@ -1,6 +1,8 @@
 -- lua/plugins/configs/lsp/config.lua - Get it? lsp/config.lua? I'm so boring :(
 
 return function()
+  require("neodev").setup()
+
   local capabilities = vim.lsp.protocol.make_client_capabilities()
 
   capabilities.textDocument.completion.completionItem = {
