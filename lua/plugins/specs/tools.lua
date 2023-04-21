73e04eb471f4cf3a95bf314a9cbd04a8dfd99c15
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:25 +0900

    feat(telescope): Initial commit.
    Add telescope.nvim

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -6,4 +6,14 @@ return {
     },
     event = "BufReadPost",
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", function() require("telescope.builtin").find_files() end },
+      { "<leader>fg", function() require("telescope.builtin").live_grep() end },
+      { "<leader>fb", function() require("telescope.builtin").buffers() end },
+      { "<leader>fh", function() require("telescope.builtin").help_tags() end },
+    },
+  },
 }
