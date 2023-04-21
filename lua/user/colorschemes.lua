bb2037708c589591ecd86bdd7ff238e95c7b008f
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:19 +0900

    feat: Add enfocado-vim.

--- lua/user/colorschemes.lua
+++ lua/user/colorschemes.lua
@@ -1,4 +1,10 @@
 return {
-    "catppuccin/nvim",
-    "FrenzyExists/aquarium-vim",
+  "catppuccin/nvim",
+  "FrenzyExists/aquarium-vim",
+  {
+    "wuelnerdotexe/vim-enfocado",
+    config = function ()
+      vim.g.enfocado_style = "nature" -- You can go for "neon" too.
+    end
+  },
 }
