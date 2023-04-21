3bc3d9d7996ce93f0bbed6f6311c9bd5c6af4f7f
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:14 +0900

    fix: Auto-run ":TSUpdate" after re-building.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,7 +8,8 @@ plugins[1] = {
 plugins[2] = {
   "nvim-treesitter/nvim-treesitter",
   lazy = require("core.utils.lazyLoad")("nvim-treesitter"),
-  config = require("plugins.configs.treesitter")
+  config = require("plugins.configs.treesitter"),
+  build = ":TSUpdate",
 }
 
 require("lazy").setup({
