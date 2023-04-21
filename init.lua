a71e006f0005c077d1b3292b3318c2b7f7390f86
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:21 +0900

    refactor(init): actually refactor so cool wow
    ^^
    
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   init.lua

--- init.lua
+++ init.lua
@@ -17,7 +17,9 @@ require("user.options")
 local config = vim.fn.stdpath("config")
 
 -- Allow searching in `lua/plugins/configs/x.lua` or `lua/plugins/configs/x/init.lua`
-package.path = config .. "/lua/plugins/configs/?.lua;" .. config .. "/lua/plugins/configs/?/init.lua;" .. package.path
+package.path = config .. "/lua/plugins/configs/?.lua;" ..
+  config .. "/lua/plugins/configs/?/init.lua;" ..
+  package.path
 
 require("plugins")
 
