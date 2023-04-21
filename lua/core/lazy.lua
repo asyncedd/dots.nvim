0d1c05330d6b69e684362693dba9758f1775d7ce
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:12 +0900

    (feat) Fix startup time. modified:   lua/core/lazy.lua deleted:    lua/plugins/configs/core/lazy.lua modified:   lua/plugins/specs/lsp.lua

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -19,5 +19,70 @@ end
 -- And then, prepend it the runtime plugins list.
 vim.opt.rtp:prepend(lazypath)
 
+local icons = require("core.utils.icons.lazy")
+
 -- Now we're going to initalize lazy.nvim
-require("plugins.configs.core.lazy")
+require("lazy").setup({
+  -- First, we're going to get the specs table from a directory.
+  { import = "plugins.specs" },
+  -- Next is the options for lazy.nvim
+}, {
+    performance = {
+      rtp = {
+        -- We're going to disable some runtime plugins that we don't need.
+        disabled_plugins = {
+          "2html_plugin",
+          -- "getscript",
+          -- "getscriptPlugin",
+          "gzip",
+          -- "logipat",
+          "netrw",
+          -- "netrwFileHandlers",
+          -- "loaded_remote_plugins",
+          -- "loaded_tutor_mode_plugin",
+          "netrwPlugin",
+          "netrwSettings",
+          -- "rrhelper",
+          "spellfile_plugin",
+          "tar",
+          "tarPlugin",
+          "vimball",
+          "vimballPlugin",
+          "zip",
+          "zipPlugin",
+          "rplugin",
+          "tohtml",
+          "tutor",
+          -- "shada",
+          "spellfile",
+          "matchit",
+          "matchparen",
+          "man",
+          "nvim",
+          "editorconfig",
+        },
+      },
+    },
+    ui = {
+      -- size = { width = 0.8, height = 0.8 },
+      -- We're gonna make the GUI (actually, it's a TUI) curved!
+      -- Look at those curves! (WTF. Add +1 to the WTF/Min)
+      border = "rounded",
+      -- I dunno, lazy.nvim doesn't really seem to find this so here, uwu!
+      icons = {
+        lazy = icons.lazy,
+      },
+      -- throttle = 50,
+    },
+    install = {
+      -- When we're installing plugins it'll default to the habamax colorscheme but, we like our catppuccino mocha catppuccin so we're going to default to that.
+      -- And also Tokyonight.nvim, even though we don't have it installed (+1 WTF/min)
+      colorscheme = { "catppuccin", "tokyonight", "habamx" },
+    },
+    -- Oh yeah, we're going to lazy-laod every plugin by default.
+    -- because I'm too lazy to add that 12 characters!
+    -- (+1 WTF/Min)
+    defaults = {
+      lazy = true,
+    },
+  })
