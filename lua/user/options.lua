f7301b2909e2c850fbba727ce7635d81a4831818
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:12 +0900

    modified:   lua/user/options.lua

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -2,11 +2,26 @@
 -- If you want a low-overhead version of this, directly set it in `core.options`.
 -- I meant, you should really do it.
 
-local o = vim.opts
+local opts = {
 
--- Set options by: (ex)
--- o.numbers = true
--- https://neovim.io/doc/user/options.html
--- Btw, K1SS. (Keep it simple stupid)
+  -- Set options by: (ex)
+  -- numbers = true,
+  -- To enable line numbers
+  -- https://neovim.io/doc/user/options.html
+  -- Btw, K1SS. (Keep it simple, stupid)
+  --
+  -- Some tips for troubleshooting.
+  -- - Does all lines ends with ","?
+  -- For ex.
+  -- a = a,
+  -- b = b,
+  -- c = c,
+  -- d = d <-- This!
+  -- e = e,
+  --
+
+}
 
 -- EOL
+-- DO NOT "HACK" this line!
+return opts
