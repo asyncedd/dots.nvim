6200069f3a0ada69c95499eaab1b3effeb05e443
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:03 +0900

    add lsp stuff to heirline

---
+++ lua/modules/configs/ui/heirline/scrollbar.lua
@@ -0,0 +1,16 @@
+local ScrollBar ={
+    static = {
+        sbar = { '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█' }
+        -- Another variant, because the more choice the better.
+        -- sbar = { '🭶', '🭷', '🭸', '🭹', '🭺', '🭻' }
+    },
+    provider = function(self)
+        local curr_line = vim.api.nvim_win_get_cursor(0)[1]
+        local lines = vim.api.nvim_buf_line_count(0)
+        local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
+        return string.rep(self.sbar[i], 2)
+    end,
+    hl = { fg = "blue", bg = "bright_bg" },
+}
+
+return ScrollBar
