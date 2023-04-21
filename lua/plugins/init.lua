e18351505da2fd671f55179f6faf7c2ba32061ac
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:21 +0900

    feat(dap): cool dap debugging.
    Use DAP to debug Lua code.
    
    On branch dap
    Changes to be committed:
    	modified:   lazy-lock.json
    	modified:   lua/plugins/init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -344,6 +344,25 @@ local plugins = {
       vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>')
     end,
     event = "VeryLazy",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "jbyuki/one-small-step-for-vimkind",
+      "rcarriga/nvim-dap-ui",
+    },
+    keys = {
+      { "<leader>bp", function() require("dap").toggle_breakpoint() end },
+      { "<leader>dc", function() require("dap").continue() end },
+      { "<leader>so", function() require("dap").step_over() end },
+      { "<leader>si", function() require("dap").step_into() end },
+      { "<leader>uh", function() require("dap.ui.widgets").hover() end },
+      { "<leader>ol", function() require("osv").launch({ port = 8086} ) end },
+      { "<leader>runit", function() require("osv").run_this() end },
+    },
+    config = function()
+      require("dap.dap")
+    end,
   }
 }
 
