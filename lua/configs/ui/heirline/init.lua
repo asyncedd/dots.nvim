428c0d27f20cd534c90d239113abb2ad747ff272
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:06 +0900

    modified:   lua/configs/ui/heirline/init.lua new file:   lua/configs/ui/heirline/scrollbar.lua

--- lua/configs/ui/heirline/init.lua
+++ lua/configs/ui/heirline/init.lua
@@ -4,6 +4,7 @@ local status2, colors = pcall(require, "configs.ui.heirline.colors")
 local status3, ViMode = pcall(require, "configs.ui.heirline.vimode")
 local status4, FileName = pcall(require, "configs.ui.heirline.filename")
 local status5, Gitsigns = pcall(require, "configs.ui.heirline.git")
+local status6, Scrollbar = pcall(require, "configs.ui.heirline.scrollbar")
 -- local Gitsigns = require("configs.ui.heirline.git")
 if (not status) then return end
 if (not status1) then return end
@@ -11,6 +12,7 @@ if (not status2) then return end
 if (not status3) then return end
 if (not status4) then return end
 if (not status5) then return end
+if (not status6) then return end
 
 heirline.load_colors(colors)
 
@@ -18,7 +20,9 @@ local Align = { provider = "%=" }
 local Space = { provider = " " }
 
 local DefaultStatusLine = {
-  ViMode, Space, FileName.FileNameBlock , Space, Gitsigns
+  ViMode, Space, FileName.FileNameBlock , Space, Gitsigns,
+  Align, 
+  Align, Scrollbar.Ruler, Scrollbar.ScrollBar,
 }
 
 local StatusLines = {
