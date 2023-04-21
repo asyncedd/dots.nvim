0792f41be2663effcd7df901b469a70801e36eca
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:17 +0900

    feat: Take that Windows XP!

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -4,7 +4,14 @@
 local o = vim.opt
 local g = vim.g
 
+-- Set the mapleader key to <Space>
+g.mapleader = " "
+-- As, 0.2x Devs, we need our 2 space indentation
 o.shiftwidth = 2
+-- Tabs take less file space, thus you need only 1 floppy disk, so take that to Windows XP!
+o.expandtab = false
+-- Set lines numbers.
 o.number = true
+-- Have those glorious 24 bit colors! Take that Windows XP!
 o.termguicolors = true
 
