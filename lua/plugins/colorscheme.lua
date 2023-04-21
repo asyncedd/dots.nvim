64d853f9d12d86107c5e6d5abeb619d6ad73fa3b
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:06 +0900

    modified:   lua/configs/editor/treesitter.lua 	modified:   lua/core/init.lua 	modified:   lua/core/load_ui_plugins.lua 	modified:   lua/core/options.lua 	modified:   lua/plugins/colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,6 @@
 return {
   {
     "catppuccin/nvim",
-    config = function()
-      vim.api.nvim_command("colorscheme catppuccin")
-    end,
-    lazy = false,
-    piority = 1000
   },
   "rebelot/kanagawa.nvim",
   "folke/tokyonight.nvim",
