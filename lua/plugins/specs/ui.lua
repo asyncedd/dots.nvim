c24d5c78295b67fd33b7527b218a2a5eb9ea7d77
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:22 +0900

    feat(spec): rm spec.nvim
    It doesn't really look the best with it.
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lua/plugins/specs/ui.lua

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -133,19 +133,5 @@ return {
     config = true,
     init = neovide("flirt.nvim")
   },
-  {
-    "edluffy/specs.nvim",
-    config = true,
-    event = "CursorMoved",
-    opts = {
-      popup = {
-        delay_ms = 0, -- delay before popup displays
-        inc_ms = 10, -- time increments used for fade/resize effects 
-        blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
-        width = 10,
-        winhl = "PMenu",
-      },
-    },
-  },
 }
 
