5d528303096b77d65c6f129839fa674c5726c80c
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:05 +0900

    modified:   lazy-lock.json 	new file:   lua/configs/ui/indent.lua 	deleted:    lua/configs/ui/indents.lua 	modified:   lua/plugins/ui.lua

--- lua/configs/ui/indents.lua
+++
@@ -1,9 +0,0 @@
-return function()
-  local status, indent_blankline = pcall(require, "indent_blankline")
-  if not status then return end
-
-  indent_blankline.setup({
-    show_current_context = true,
-    show_current_context_start = true,
-  })
-end
