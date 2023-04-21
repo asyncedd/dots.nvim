2267927a239ad89a5af2722cfeb1d15cdac4014f
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:24 +0900

    feat(cmp): add nvim-cmp
    Add a completion engine for fun purposes ;)

---
+++ lua/plugins/configs/coding/cmp.lua
@@ -0,0 +1,17 @@
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+  }),
+})
--- lua/plugins/specs/coding.lua
+++ lua/plugins/specs/coding.lua
@@ -49,5 +49,18 @@ return {
       "gcA",
     },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      require("plugins.configs.coding.cmp")
+    end,
+  },
 }
 
