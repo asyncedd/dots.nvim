b2d3c208c114b053709e224a87dc0e30710360f4
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:22 +0900

    feat(vim-fugitive): add vim-fugitive.
    I really wanted another holy tpope plugin so, I added vim-fugitive as an
    alternative to Neogit.
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lazy-lock.json
    	modified:   lua/plugins/specs/tools.lua

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -118,4 +118,22 @@ return {
     "ThePrimeagen/vim-be-good",
     cmd = { "VimBeGood" },
   },
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gedit",
+      "Gsplit",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GRename",
+      "GDelete",
+      "GRemove",
+      "GBrowse"
+    },
+  },
 }
