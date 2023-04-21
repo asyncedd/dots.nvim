c2b753bccee0a8208b3e4390b6bca84ae9048da8
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:24 +0900

    (redo): Initial commit.

--- init.lua
+++ init.lua
@@ -1,26 +1,3 @@
--- Neoinit v.0.1
+require("settings.options")
 
-vim.loader.enable()
-
-require("user.autocmd")
-
--- local disableBuiltinPlugins = require("user.builtinPlugins")
---
--- for _, plugin in ipairs(disableBuiltinPlugins) do
---   vim.g["loaded_" .. plugin] = 1
--- end
-
-require("user.options")
-
-local config = vim.fn.stdpath("config")
-
--- Allow searching in `lua/plugins/configs/x.lua` or `lua/plugins/configs/x/init.lua`
-package.path = config .. "/lua/plugins/configs/?.lua;" .. config .. "/lua/plugins/configs/?/init.lua;" .. package.path
-
-require("plugins")
-
--- vim.api.nvim_command("colorscheme tokyonight")
-vim.api.nvim_command("colorscheme catppuccin")
--- vim.api.nvim_command("colorscheme onedark")
--- vim.api.nvim_command("colorscheme kanagawa")
--- vim.api.nvim_command("colorscheme tundra")
+require("settings.autocmd")
--- lua/plugins/configs/ui/animate.lua
+++
@@ -1,1 +0,0 @@
-require("mini.animate").setup()
