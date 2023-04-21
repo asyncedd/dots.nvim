ca0eb65948a144f5d453be667e9d41f4c7105567
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:03 +0900

    Edit the refactoring

--- lua/modules/configs/editor/treesitter.lua
+++ lua/modules/configs/editor/treesitter.lua
@@ -33,6 +33,16 @@ return function()
           smart_rename = "grr",
         },
       },
+      navigation = {
+        enable = true,
+        keymaps = {
+          goto_definition = "gnd",
+          list_definitions = "gnD",
+          list_definitions_toc = "gO",
+          goto_next_usage = "<a-*>",
+          goto_previous_usage = "<a-#>",
+        },
+      },
     },
   })
 end
