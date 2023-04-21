6252c4b44e681fdffade1382a752c03855582ae1
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:14 +0900

    feat: Prepare nvim-lspconfig

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,23 @@
 local plugins = {}
 local disabledPlugins = require("user.builtinPlugins")
+local lazyLoad = require("core.utils.lazyLoad")
 
 plugins[1] = {
   "catppuccin/nvim",
-  -- config = require("plugins.configs.ui.catppuccin"),
+  name = "catppuccin",
+  lazy = lazyLoad("catppuccin"),
 }
 
 plugins[2] = {
   "nvim-treesitter/nvim-treesitter",
-  lazy = require("core.utils.lazyLoad")("nvim-treesitter"),
+  lazy = lazyLoad("nvim-treesitter"),
   config = require("plugins.configs.editor.treesitter"),
   build = ":TSUpdate",
+}
+
+plugins[3] = {
+  "neovim/nvim-lspconfig",
+  lazy = lazyLoad("nvim-lspconfig") 
 }
 
 require("lazy").setup({
