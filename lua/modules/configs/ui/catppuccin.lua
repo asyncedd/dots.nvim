683ae816f5fff42266e1af649b7441713b8addfa
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:03 +0900

    Add some dimming 🌚

--- lua/modules/configs/ui/catppuccin.lua
+++ lua/modules/configs/ui/catppuccin.lua
@@ -3,7 +3,7 @@ return function()
     flavour = "mocha", -- Can be one of: latte, frappe, macchiato, mocha
     background = { light = "latte", dark = "mocha" },
     dim_inactive = {
-      enabled = false,
+      enabled = true,
       -- Dim inactive splits/windows/buffers.
       -- NOT recommended if you use old palette (a.k.a., mocha).
       shade = "dark",
