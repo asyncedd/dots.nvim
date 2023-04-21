0b1ef5a39c346d8a8758ce56dca2a37b5efe698b
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:09 +0900

    modified:   lua/plugins/configs/ui/catppuccin.lua

--- lua/plugins/configs/ui/catppuccin.lua
+++ lua/plugins/configs/ui/catppuccin.lua
@@ -22,7 +22,6 @@ require("catppuccin").setup({
       sky = "#89DCEB",
       teal = "#B5E8E0",
       lavender = "#C9CBFF",
-
       text = "#D9E0EE",
       subtext1 = "#BAC2DE",
       subtext0 = "#A6ADC8",
@@ -32,7 +31,6 @@ require("catppuccin").setup({
       surface2 = "#6E6C7E",
       surface1 = "#575268",
       surface0 = "#302D41",
-
       base = "#1E1E2E",
       mantle = "#1A1826",
       crust = "#161320",
@@ -45,6 +43,7 @@ require("catppuccin").setup({
     -- telescope = true,
     -- gitsigns = true,
     treesitter = true,
+    which_key = true,
   },
   highlight_overrides = {
     mocha = function(cp)
@@ -58,59 +57,44 @@ require("catppuccin").setup({
         Type = { fg = cp.blue },
         Typedef = { fg = cp.yellow },
         StorageClass = { fg = cp.red, style = { "italic" } },
-
         -- For trouble.nvim
         TroubleNormal = { bg = cp.base },
-
         -- For treesitter.
-        ["@field"] = { fg = cp.rosewater },
-
-        ["@include"] = { fg = cp.teal },
-        ["@keyword.operator"] = { fg = cp.sky },
-        ["@punctuation.special"] = { fg = cp.maroon },
-
-        ["@constructor"] = { fg = cp.lavender },
-        ["@exception"] = { fg = cp.peach },
-
-        ["@constant.builtin"] = { fg = cp.lavender },
-        ["@type.qualifier"] = { link = "@keyword" },
-        ["@variable.builtin"] = { fg = cp.red, style = { "italic" } },
-
-        ["@function.macro"] = { fg = cp.red, style = {} },
-        ["@parameter"] = { fg = cp.rosewater },
-        ["@keyword"] = { fg = cp.red, style = { "italic" } },
-        ["@keyword.function"] = { fg = cp.maroon },
-        ["@keyword.return"] = { fg = cp.pink, style = {} },
-
-        ["@method"] = { fg = cp.blue, style = { "italic" } },
-        ["@namespace"] = { fg = cp.rosewater, style = {} },
-
-        ["@punctuation.delimiter"] = { fg = cp.teal },
-        ["@punctuation.bracket"] = { fg = cp.overlay2 },
-        ["@type"] = { fg = cp.yellow },
-        ["@variable"] = { fg = cp.text },
-        ["@tag.attribute"] = { fg = cp.mauve, style = { "italic" } },
-        ["@tag"] = { fg = cp.peach },
-        ["@tag.delimiter"] = { fg = cp.maroon },
-        ["@text"] = { fg = cp.text },
-
-        ["@function.builtin.bash"] = { fg = cp.red, style = { "italic" } },
-        ["@parameter.bash"] = { fg = cp.yellow, style = { "italic" } },
-
-        ["@field.lua"] = { fg = cp.lavender },
-        ["@constructor.lua"] = { fg = cp.flamingo },
-
-        ["@constant.java"] = { fg = cp.teal },
-
-        ["@property.typescript"] = { fg = cp.lavender, style = { "italic" } },
-
-        ["@type.css"] = { fg = cp.lavender },
-        ["@property.css"] = { fg = cp.yellow, style = { "italic" } },
-
-        ["@type.builtin.c"] = { fg = cp.yellow, style = {} },
-
-        ["@property.cpp"] = { fg = cp.text },
-        ["@type.builtin.cpp"] = { fg = cp.yellow, style = {} },
+            ["@field"] = { fg = cp.rosewater },
+            ["@include"] = { fg = cp.teal },
+            ["@keyword.operator"] = { fg = cp.sky },
+            ["@punctuation.special"] = { fg = cp.maroon },
+            ["@constructor"] = { fg = cp.lavender },
+            ["@exception"] = { fg = cp.peach },
+            ["@constant.builtin"] = { fg = cp.lavender },
+            ["@type.qualifier"] = { link = "@keyword" },
+            ["@variable.builtin"] = { fg = cp.red, style = { "italic" } },
+            ["@function.macro"] = { fg = cp.red, style = {} },
+            ["@parameter"] = { fg = cp.rosewater },
+            ["@keyword"] = { fg = cp.red, style = { "italic" } },
+            ["@keyword.function"] = { fg = cp.maroon },
+            ["@keyword.return"] = { fg = cp.pink, style = {} },
+            ["@method"] = { fg = cp.blue, style = { "italic" } },
+            ["@namespace"] = { fg = cp.rosewater, style = {} },
+            ["@punctuation.delimiter"] = { fg = cp.teal },
+            ["@punctuation.bracket"] = { fg = cp.overlay2 },
+            ["@type"] = { fg = cp.yellow },
+            ["@variable"] = { fg = cp.text },
+            ["@tag.attribute"] = { fg = cp.mauve, style = { "italic" } },
+            ["@tag"] = { fg = cp.peach },
+            ["@tag.delimiter"] = { fg = cp.maroon },
+            ["@text"] = { fg = cp.text },
+            ["@function.builtin.bash"] = { fg = cp.red, style = { "italic" } },
+            ["@parameter.bash"] = { fg = cp.yellow, style = { "italic" } },
+            ["@field.lua"] = { fg = cp.lavender },
+            ["@constructor.lua"] = { fg = cp.flamingo },
+            ["@constant.java"] = { fg = cp.teal },
+            ["@property.typescript"] = { fg = cp.lavender, style = { "italic" } },
+            ["@type.css"] = { fg = cp.lavender },
+            ["@property.css"] = { fg = cp.yellow, style = { "italic" } },
+            ["@type.builtin.c"] = { fg = cp.yellow, style = {} },
+            ["@property.cpp"] = { fg = cp.text },
+            ["@type.builtin.cpp"] = { fg = cp.yellow, style = {} },
       }
     end,
   },
