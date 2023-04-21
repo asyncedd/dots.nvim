1a65345ffacb462c197de10ad6d20748547881ca
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:13 +0900

    perf: Actually prevent some builtin plugins from even loading.

---
+++ lua/core/lazy/init.lua
@@ -0,0 +1,12 @@
+local pluginSpec = require("core.lazy.plugins")
+local disabledPlugins = require("user.builtinPlugins")
+
+require("lazy").setup({
+  pluginSpec
+}, {
+  performance = {
+    rtp = {
+      disabled_plugins = disabledPlugins
+    }
+  }
+})
