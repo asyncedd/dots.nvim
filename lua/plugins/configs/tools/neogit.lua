9d94d4d51b7ef9c59ca4d1850684a3a66baf81d5
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:22 +0900

    fix(neogits): gix neogit's configmation.
    Fix Neogit's configmation windows.
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   lua/plugins/configs/tools/neogit.lua
    	modified:   lua/plugins/specs/tools.lua

---
+++ lua/plugins/configs/tools/neogit.lua
@@ -0,0 +1,48 @@
+require("neogit").setup({
+  disable_signs = false,
+  disable_hint = false,
+  disable_context_highlighting = false,
+  disable_commit_confirmation = false,
+  auto_refresh = true,
+  disable_builtin_notifications = false,
+  use_magit_keybindings = false,
+  kind = "tab",
+  console_timeout = 2000,
+  auto_show_console = true,
+  popup = {
+    kind = "vsplit",
+  },
+  signs = {
+    -- { CLOSED, OPENED }
+    section = { ">", "v" },
+    item = { ">", "v" },
+    hunk = { "", "" },
+  },
+  integrations = {
+    diffview = true
+  },
+  sections = {
+    untracked = {
+      folded = false
+    },
+    unstaged = {
+      folded = false
+    },
+    staged = {
+      folded = false
+    },
+    stashes = {
+      folded = true
+    },
+    unpulled = {
+      folded = true
+    },
+    unmerged = {
+      folded = false
+    },
+    recent = {
+      folded = true
+    },
+  },
+})
+
