159e05cdc3adb38e2e748cc71df64ed8df45075f
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:05 +0900

    modified:   lua/core/init.lua 	deleted:    lua/mappings/init.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function()
   require("core.options")
-  require("mappings") -- As of now, it's requiring mappings that in turn requires modules inside it. It is of course not as efficent.
+  require("mappings.movement") -- As of now, it's requiring mappings that in turn requires modules inside it. It is of course not as efficent.
 end
 
 return M
