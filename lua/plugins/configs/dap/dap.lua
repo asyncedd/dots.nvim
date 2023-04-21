152c4944b3127af2e67237a9124b6052414ebcec
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:19 +0900

    perf: Lazy-load DAP on keys

--- lua/plugins/configs/dap/dap.lua
+++ lua/plugins/configs/dap/dap.lua
@@ -16,13 +16,3 @@ dap.adapters.nlua = function(callback, config)
   })
 end
 
-local keymap = vim.keymap.set
-
-keymap("n", "<leader>dtb", function() dap.toggle_breakpoint() end, { noremap = true })
-keymap("n", "<leader>dc", function() dap.continue() end, { noremap = true })
-keymap("n", "<leader>dso", function() dap.step_over() end, { noremap = true })
-keymap("n", "<leader>dsi", function() dap.step_into() end, { noremap = true })
-keymap("n", "<leader>dui", function() require("dap.ui.widgets").hover() end, { noremap = true })
-keymap("n", "<leader>dls", function() require("osv").launch({ port = 8086 }) end, { noremap = true })
-keymap("n", "<leader>ort", function() require("osv").run_this() end, { noremap = true })
-
