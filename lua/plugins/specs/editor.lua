67e57b8376dfe823a9926fc9f61c42b93411ce35
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:25 +0900

    feat(readline): add readline motions in insert mode
    I'm not really fammiliar with it but, lemme try!

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -131,5 +131,26 @@ return {
       "ThePrimeagen/harpoon",
     },
   },
+  {
+    "linty-org/readline.nvim",
+    keys = {
+      { "<C-k>", function() require("readline").kill_line() end, mode = { "i" } },
+      { "<C-u>", function() require("readline").backward_kill_line() end, mode = { "i" } },
+      { "<M-d>", function() require("readline").kill_word() end, mode = { "i" } },
+      { "<M-BS>", function() require("readline").backward_kill_word() end, mode = { "i" } },
+      { "<C-w>", function() require("readline").unix_word_rubout() end, mode = { "i" } },
+      { "<C-d>", "<Delete>", mode = { "i" } },
+      { "<C-h>", "<BS", mode = { "i" } },
+      { "<C-a>", function() require("readline").beginning_of_line() end, mode = { "i" } },
+      { "<M-m>", function() require("readline").back_to_indentation() end, mode = { "i" } },
+      { "<C-e>", function() require("readline").end_of_line() end, mode = { "i" } },
+      { "<M-f>", function() require("readline").forward_word() end, mode = { "i" } },
+      { "<M-b>", function() require("readline").backward_word() end, mode = { "i" } },
+      { "<C-f>", "<Right>", mode = { "i" } },
+      { "<C-b>", "<Left>", mode = { "i" } },
+      { "<C-n>", "<Down>", mode = { "i" } },
+      { "<C-p>", "<Up>", mode = { "i" } },
+    },
+  },
 }
 
