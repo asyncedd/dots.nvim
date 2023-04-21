c8a35924d60a6fac30cabec3d3c9b17c2e29cae2
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:06 +0900

    modified:   init.lua modified:   lua/configs/ui/heirline/init.lua

--- lua/configs/ui/heirline/init.lua
+++ lua/configs/ui/heirline/init.lua
@@ -37,6 +37,28 @@ local StatusLines = {
     end
   end,
 
+  static = {
+    mode_colors_map = {
+      n = "red",
+      i = "green",
+      v = "cyan",
+      V = "cyan",
+      ["\22"] = "cyan",
+      c = "orange",
+      s = "purple",
+      S = "purple",
+      ["\19"] = "purple",
+      R = "orange",
+      r = "orange",
+      ["!"] = "red",
+      t = "green",
+    },
+    mode_color = function(self)
+      local mode = conditions.is_active() and vim.fn.mode() or "n"
+      return self.mode_colors_map[mode]
+    end,
+  },
+
   -- the first statusline with no condition, or which condition returns true is used.
   -- think of it as a switch case with breaks to stop fallthrough.
   fallthrough = false,
