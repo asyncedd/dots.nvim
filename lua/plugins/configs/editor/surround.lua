84760d301fa5bb4b1f0c8a724bfafabce719c1be
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:23 +0900

    feat(mini): Add some mini.nvim plugins.
    Add some cool mini.nvim plugins.

---
+++ lua/plugins/configs/editor/surround.lua
@@ -0,0 +1,11 @@
+require("mini.surround").setup({
+  mappings = {
+    add = "<leader>sa", -- Add surrounding in Normal and Visual modes
+    delete = "<leader>sd", -- Delete surrounding
+    find = "<leader>sf", -- Find surrounding (to the right)
+    find_left = "<leader>sF", -- Find surrounding (to the left)
+    highlight = "<leader>sh", -- Highlight surrounding
+    replace = "<leader>sr", -- Replace surrounding
+    update_n_lines = "<leader>sn", -- Update `n_lines`
+  },
+})
