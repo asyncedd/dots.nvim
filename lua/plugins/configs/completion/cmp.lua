6160727c6dcb0adbcb4c8bffaadef1b0cb845773
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:07 +0900

    modified:   lazy-lock.json modified:   lua/plugins/configs/completion/cmp.lua

--- lua/plugins/configs/completion/cmp.lua
+++ lua/plugins/configs/completion/cmp.lua
@@ -1,3 +1,31 @@
+local kind_icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = ""
+}
+
 return function()
   local status, cmp = pcall(require, "cmp")
   local status1, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
@@ -38,7 +66,25 @@ return function()
         return not context.in_treesitter_capture("comment") 
           and not context.in_syntax_group("Comment")
       end
-    end
+    end,
+    view = {
+      entries = {name = 'custom', selection_order = 'near_cursor' }
+    },
+    formatting = {
+      format = function(entry, vim_item)
+        -- Kind icons
+        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+        -- Source
+        vim_item.menu = ({
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[Lua]",
+          latex_symbols = "[LaTeX]",
+        })[entry.source.name]
+        return vim_item
+      end
+    },
   })
 
   -- Set configuration for specific filetype.
