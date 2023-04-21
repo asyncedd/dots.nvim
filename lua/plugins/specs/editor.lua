fdb72927452918143cf49249495426e5cf695caa
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:11 +0900

    modified:   lua/plugins/specs/editor.lua

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -75,7 +75,12 @@ return {
 
       keymap("n", "<leader>a", mark.add_file)
       keymap("n", "<leader>e", ui.toggle_quick_menu)
+
+      keymap("n", "<C-h>", function () ui.nav_file(1) end)
+      keymap("n", "<C-t>", function () ui.nav_file(2) end)
+      keymap("n", "<C-n>", function () ui.nav_file(3) end)
+      keymap("n", "<C-s>", function () ui.nav_file(4) end)
     end,
     event = "VeryLazy"
-  }
+  },
 }
