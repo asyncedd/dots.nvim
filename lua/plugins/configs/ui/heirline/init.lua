95c433cc0a6471811cfbc50fcbd3121cfc941317
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:11 +0900

    modified:   lua/plugins/configs/ui/heirline/init.lua

--- lua/plugins/configs/ui/heirline/init.lua
+++ lua/plugins/configs/ui/heirline/init.lua
@@ -1,20 +1,32 @@
 local lazy_require = require("snips.lazy-require").require_on_index
-local heirline = lazy_require("heirline")
-local Bufferline = require("plugins.configs.ui.heirline.bufferline")
-local StatusLines = require("plugins.configs.ui.heirline.statusline")
--- local StatusColumn = require("plugins.configs.ui.heirline.statuscolumn")
 
-require("plugins.configs.ui.heirline.colors")
+local coroutine_list = {}
 
--- local statuscolumn = {
---
---   -- StatusColumn.diagnostic_signs,
---   StatusColumn.line_number,
---   StatusColumn.gitsigns_or_bar,
--- }
+-- Define a function to wrap require calls in a coroutine
+local function require_coroutine(module_name)
+  local module = require(module_name)
+  coroutine.yield(module)
+end
 
-heirline.setup({
-  statusline = StatusLines,
-  tabline = Bufferline,
-  -- statuscolumn = statuscolumn,
+-- Create a coroutine for each module you want to require
+table.insert(coroutine_list, coroutine.create(function() return lazy_require("heirline") end))
+table.insert(coroutine_list, coroutine.create(function() return require("plugins.configs.ui.heirline.bufferline") end))
+table.insert(coroutine_list, coroutine.create(function() return require("plugins.configs.ui.heirline.statusline") end))
+table.insert(coroutine_list, coroutine.create(function() return require("plugins.configs.ui.heirline.colors") end))
+
+-- Resume all coroutines in parallel and store the results
+local results = {}
+for _, co in ipairs(coroutine_list) do
+  local ok, result = coroutine.resume(co)
+  if not ok then
+    error(result)
+  end
+  table.insert(results, result)
+end
+
+-- Unpack the results and call the setup function with the appropriate arguments
+results[1].setup({
+  statusline = results[3],
+  tabline = results[2],
 })
+
