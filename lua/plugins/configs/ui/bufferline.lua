661df8fd676377f918d6c8f0b4d321490b1238c8
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:28 +0900

    fix(bufferline): fix weird error?
    Super weird, i dont know why its happening but, yeah.

--- lua/plugins/configs/ui/bufferline.lua
+++ lua/plugins/configs/ui/bufferline.lua
@@ -16,46 +16,46 @@ require("bufferline").setup({
       end
       return s
     end,
-    groups = {
-      options = {
-        toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-      },
-      items = {
-        groups.builtin.pinned:with({ icon = "" }),
-        groups.builtin.ungrouped,
-        {
-          name = "Tests", -- Mandatory
-          -- highlight = { underline = true, sp = "blue" }, -- Optional
-          priority = 2, -- determines where it will appear relative to other groups (Optional)
-          icon = "", -- Optional
-          matcher = function(buf) -- Mandatory
-            return buf.filename:match("%_test") or buf.filename:match("%_spec")
-          end,
-        },
-        {
-          name = "Docs",
-          -- highlight = { undercurl = true, sp = "green" },
-          auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
-          matcher = function(buf)
-            return buf.filename:match("%.md") or buf.filename:match("%.txt")
-          end,
-          separator = { -- Optional
-            style = require("bufferline.groups").separator.tab,
-          },
-        },
-        {
-          name = "Lua",
-          -- highlight = { undercurl = true, sp = "pink" },
-          auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
-          matcher = function(buf)
-            return buf.filename:match("%.lua")
-          end,
-          separator = { -- Optional
-            style = require("bufferline.groups").separator.tab,
-          },
-        },
-      },
-    },
+    -- groups = {
+    --   options = {
+    --     toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+    --   },
+      -- items = {
+      --   groups.builtin.pinned:with({ icon = "" }),
+      --   groups.builtin.ungrouped,
+      --   {
+      --     name = "Tests", -- Mandatory
+      --     -- highlight = { underline = true, sp = "blue" }, -- Optional
+      --     priority = 2, -- determines where it will appear relative to other groups (Optional)
+      --     icon = "", -- Optional
+      --     matcher = function(buf) -- Mandatory
+      --       return buf.filename:match("%_test") or buf.filename:match("%_spec")
+      --     end,
+      --   },
+      --   {
+      --     name = "Docs",
+      --     -- highlight = { undercurl = true, sp = "green" },
+      --     auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
+      --     matcher = function(buf)
+      --       return buf.filename:match("%.md") or buf.filename:match("%.txt")
+      --     end,
+      --     separator = { -- Optional
+      --       style = require("bufferline.groups").separator.tab,
+      --     },
+      --   },
+      --   {
+      --     name = "Lua",
+      --     -- highlight = { undercurl = true, sp = "pink" },
+      --     auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
+      --     matcher = function(buf)
+      --       return buf.filename:match("%.lua")
+      --     end,
+      --     separator = { -- Optional
+      --       style = require("bufferline.groups").separator.tab,
+      --     },
+      --   },
+    --   },
+    -- },
     options = {
       groups = {
         items = {
