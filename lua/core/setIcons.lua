1bcf9b4328e8e3b472a0f5d338cff2542e43c5b3
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:17 +0900

    feat: Iconify LSP with iconic icons

---
+++ lua/core/setIcons.lua
@@ -0,0 +1,16 @@
+local icons = require("core.utils.icons.diagnostics")
+
+for name, icon in pairs(icons) do
+  name = "DiagnosticSign" .. name
+  vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+end
+
+vim.diagnostic.config({
+  diagnostics = {
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4 },
+    severity_sort = true,
+  },
+})
+
