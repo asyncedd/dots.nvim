4cb5333057a0cc017fa3a718d9b2479dfe090e8d
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:26 +0900

    feat(blankline): use treesitter :)
    Make indent-blankline be treesitter powered :D

--- lua/plugins/configs/ui/blankline.lua
+++ lua/plugins/configs/ui/blankline.lua
@@ -4,5 +4,5 @@ require("indent_blankline").setup({
   filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
   show_trailing_blankline_indent = false,
   show_current_context = false,
+  ues_treesitter = true,
 })
-
