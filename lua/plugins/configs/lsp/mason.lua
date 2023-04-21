20265b0bd2a1c44dcd9348e3181a0ec52f8bd0ef
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:24 +0900

    feat(lsp): don't disable omnifunc even though it's enabled by default
    Don't disable omnifunc, it's enabled by default. I tried liking Omnifunc
    but, it's a little buggy for me.

--- lua/plugins/configs/lsp/mason.lua
+++ lua/plugins/configs/lsp/mason.lua
@@ -21,8 +21,6 @@ capabilities.textDocument.completion.completionItem = {
 }
 
 local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
 end
 
 local mlsp = require("mason-lspconfig")
