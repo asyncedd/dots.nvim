999f84d3adb8551f9c935016e2cd20f60c918d91
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:09 +0900

    modified:   CODE_OF_CONDUCT.md modified:   lua/core/init.lua modified:   lua/plugins/configs/completion/cmp.lua deleted:    lua/plugins/spec/colorscheme.lua deleted:    lua/plugins/spec/completion.lua deleted:    lua/plugins/spec/core.lua deleted:    lua/plugins/spec/editor.lua modified:   lua/plugins/spec/init.lua deleted:    lua/plugins/spec/lsp.lua deleted:    lua/plugins/spec/motion.lua deleted:    lua/plugins/spec/tool.lua deleted:    lua/plugins/spec/treesitter.lua deleted:    lua/plugins/spec/ui.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,17 +1,15 @@
 local o = vim.api.nvim_set_option_value
 
-o("shadafile", "NONE", {})
-
 vim.g.mapleader = " "
+o("shadafile", "NONE", {})
 
 require("core.lazy")
 
 vim.api.nvim_command("colorscheme catppuccin")
 
+require("core.options")
+
 vim.defer_fn(function()
-
-  require("core.options")
-
   require("core.autocmd")
   require("mappings.movement")
   require("mappings.lspsaga")
