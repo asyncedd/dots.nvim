367105f193abb0abd61f8e70ea77d2b0482960e8
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:26 +0900

    feat(luasnip): add more snippets!
    Why not add more and, even more snippets? ;)

--- lua/plugins/specs/coding.lua
+++ lua/plugins/specs/coding.lua
@@ -70,6 +70,18 @@ return {
           require("luasnip.loaders.from_vscode").lazy_load()
         end,
       },
+      {
+        "honza/vim-snippets",
+        config = function()
+          require("luasnip.loaders.from_snipmate").lazy_load()
+        end,
+      },
+      {
+        "molleweide/LuaSnip-snippets.nvim",
+        config = function()
+          require("luasnip.loaders.from_lua").lazy_load()
+        end,
+      },
     },
   },
   {
