ec33cfac06387eefe52ee99025a56d28e8b10a30
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:25 +0900

    feat(heirline): minimalize the statusline
    Minimalize.

--- lua/plugins/configs/ui/heirline/statusline/file.lua
+++ lua/plugins/configs/ui/heirline/statusline/file.lua
@@ -28,24 +28,9 @@ M.FileEncoding = {
 
 M.FileType = {
   provider = function()
-    return string.upper(vim.bo.filetype) .. " "
+    return string.upper(vim.bo.filetype)
   end,
   hl = { fg = "black", bold = true },
 }
 
-M.FileSize = {
-  provider = function()
-    -- stackoverflow, compute human readable file size
-    local suffix = { 'b', 'k', 'M', 'G', 'T', 'P', 'E' }
-    local fsize = vim.fn.getfsize(vim.api.nvim_buf_get_name(0))
-    fsize = (fsize < 0 and 0) or fsize
-    if fsize < 1024 then
-      return fsize..suffix[1]
-    end
-    local i = math.floor((math.log(fsize) / math.log(1024)))
-    return string.format("%.2g%s", fsize / math.pow(1024, i), suffix[i + 1]) .. " "
-  end,
-  hl = { fg = "black" },
-}
-
-return utils.surround({ "", "" }, function(self) return self:mode_color() end, { M.FileEncoding, M.FileFormat, M.FileType, M.FileSize })
+return utils.surround({ "", "" }, function(self) return self:mode_color() end, { M.FileEncoding, M.FileFormat, M.FileType })
