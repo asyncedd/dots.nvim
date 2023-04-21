f085ebfe9c85df84353cdcbbc14cc45cad3f025f
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:10 +0900

    modified:   init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+-- ============================================Ignore ============================================
 -- vim.go.loadplugins = false
+-- ============================================Please don't ignore this! :3 ======================
 -- Call setup
+-- The setup module does:
+-- Set the options
+-- initalize lazy.nvim
+-- Load plugins via lazy.nvim
+-- Add some autocmds and keymaps
 require("core")
