f026da11263c14d3e4d45f6ace0b69204e7302ae
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:13 +0900

    (feat) Add basic support for Lua since I can't get it go and running. modified:   lazy-lock.json modified:   lua/plugins/configs/dap/dap.lua new file:   lua/plugins/configs/dap/dapUi.lua modified:   lua/plugins/specs/dap.lua

--- lua/plugins/specs/dap.lua
+++ lua/plugins/specs/dap.lua
@@ -3,15 +3,32 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      "rcarriga/nvim-dap-ui",
+      {
+        "rcarriga/nvim-dap-ui",
+        config = require("dap.dapUi"),
+      },
       "nvim-treesitter/nvim-treesitter",
       "folke/neodev.nvim",
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = "nvim-dap",
+        opts = { automatic_setup = true },
+        config = function (_, opts)
+          require("mason-nvim-dap").setup(opts)
+          require("mason-nvim-dap").setup_handlers({})
+        end,
+      },
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        dependencies = {
+          "mfussenegger/nvim-dap",
+        },
+        config = require("dap.dap-vt"),
+        event = "VeryLazy"
+      },
+
     },
-    config = require("dap.dap")
+    config = require("dap.dap"),
+    event = "VeryLazy"
   },
-  -- Nvim-DAP-vt - See some variables stuff in your editor!
-  {
-    "theHamsta/nvim-dap-virtual-text",
-    config = require("dap.dap-vt")
-  }
 }
