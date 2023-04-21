0e2b5b163db308f3f36bb5cadd57dd9f797d8781
asyncedd <isynqquwu@proton.me>
2023-04-21 19:41:09 +0900

    modified:   lazy-lock.json modified:   lua/core/init.lua modified:   lua/plugins/configs/editor/treesitter.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -3,11 +3,11 @@ local o = vim.api.nvim_set_option_value
 vim.g.mapleader = " "
 o("shadafile", "NONE", {})
 
+require("core.options")
+
 require("core.lazy")
 
 vim.api.nvim_command("colorscheme catppuccin")
-
-require("core.options")
 
 vim.defer_fn(function()
   require("core.autocmd")
