1fb35fbf3c8438b185d043f943db99a598314777
asynecedd <isynqquwu@proton.me>
2023-04-23 15:06:21 +0900

    feat(treesitter): improve highlights.
    Via, the Catppuccin highlighter!

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -1,6 +1,15 @@
-local mocha = require("catppuccin.palettes").get_palette("mocha")
+local colors = require("catppuccin.palettes").get_palette() -- fetch colors from palette
 
-vim.api.nvim_set_hl(0, "@variable", { fg = mocha.pink })
+require("catppuccin.lib.highlighter").syntax({
+  -- { "@variable", { fg = colors.red } },
+  ["@variable"] = { fg = colors.red, style = { "italic" } },
+  -- ["@field"] = { fg = colors.lavender },
+  -- ["@property"] = { fg = colors.yellow },
+  ["@variable.builtin"] = { fg = colors.maroon, style = { "italic" } },
+  ["@punctuation.bracket"] = { fg = colors.maroon },
+  -- ["@enum"] = { link = "@type" },
+  -- ["@text"] = { fg = colors.text },
+})
 
 require("nvim-treesitter.configs").setup({
   -- Ensure both markdown and markdown_inline is installed.
