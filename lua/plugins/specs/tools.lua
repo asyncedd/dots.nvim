8a172b40fa14714f9e7f16fb30f5577ec8d1ad16
asynecedd <isynqquwu@proton.me>
2023-04-26 19:20:44 +0900

    feat(neogit): initial commit.

---
+++ lua/plugins/specs/tools.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "TimUntersberger/neogit",
+    config = true,
+    keys = {
+      { "<leader>gt", "<cmd>Neogit<CR>" },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
