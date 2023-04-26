131c717456c04463aeb718ed128f1dfcf0f350b5
asynecedd <isynqquwu@proton.me>
2023-04-26 21:13:27 +0900

    fix(neogit): disable confirmation menu bc noice

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -1,6 +1,9 @@
 return {
   {
     "TimUntersberger/neogit",
+    opts = {
+      disable_commit_confirmation = true,
+    },
     config = true,
     keys = {
       { "<leader>gt", "<cmd>Neogit<CR>" },
