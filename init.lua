69b821b19d8a041e4ab7a15853849ad94ac9cbbe
asynecedd <isynqquwu@proton.me>
2023-04-26 17:01:31 +0900

    feat(lazy): initial commit.

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
+-- Neoinit v2.0
+
+-- Set options.
 require("settings.options")
+
+-- Create some autocommands.
 require("settings.autocmd")
 
+-- Setup lazy.nvim
+require("plugins")
+
+-- Defer time-consuming tasks later.
 vim.defer_fn(function()
   require("core.after")
 end, 0)
