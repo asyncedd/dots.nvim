871ae494385bc4cfdb718d65f9992f420ca24404
asynecedd <isynqquwu@proton.me>
2023-04-27 17:00:18 +0900

    feat(mini.indentscope): disable in neogit.

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -70,7 +70,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     init = function()
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "neogit" },
         callback = function()
           vim.b.miniindentscope_disable = true
         end,
