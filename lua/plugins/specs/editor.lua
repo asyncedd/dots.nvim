f550076a201e292b22cdf70cb468819c248f3fa1
asynecedd <isynqquwu@proton.me>
2023-04-28 20:48:26 +0900

    feat(matchup): initial commit

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -10,4 +10,16 @@ return {
     build = ":TSUpdate",
     event = { "BufReadPost" }
   },
+  {
+    "andymass/vim-matchup",
+    opts = function()
+      return require("plugins.configs.editor.matchup")
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      
+      vim.cmd("silent! do FileType")
+    end,
+    event = "VeryLazy",
+  },
 }
