4d17c7204603e8e1c6f4caa8fe2f247e65e5fbdf
asynecedd <isynqquwu@proton.me>
2023-04-29 06:52:08 +0900

    feat(spider): lazy-load on keys.

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -1,9 +1,14 @@
 return {
   {
     "chrisgrieser/nvim-spider",
-    event = "VeryLazy",
     config = function()
       require("plugins.configs.motion.spider")
     end,
+    keys = {
+      { "w", mode = { "n", "x", "o" } },
+      { "e", mode = { "n", "x", "o" } },
+      { "b", mode = { "n", "x", "o" } },
+      { "ge", mode = { "n", "x", "o" } },
+    },
   },
 }
