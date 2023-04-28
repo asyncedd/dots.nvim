3636dd2cbacd36c54f977467924a5ee1ab059bd0
asynecedd <isynqquwu@proton.me>
2023-04-28 20:24:48 +0900

    refactor(treesitter): mv config to new file.

---
+++ lua/plugins/configs/editor/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  ensure_installed = { "lua", "luap", "markdown_inline", "markdown", "vim" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = {
+    enable = true,
+  },
+}
--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -1,20 +1,11 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "lua", "vim", "luap", "markdown", "markdown_inline" },
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        -- Enable highlighting
-        highlight = {
-          enable = true,
-        },
-      })
+    opts = function()
+      return require("plugins.configs.editor.treesitter")
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
     end,
     build = ":TSUpdate",
     event = { "BufReadPost" }
