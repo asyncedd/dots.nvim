2d25ee14ae8f6d7ce75756aaba568b85dd5851c1
asynecedd <isynqquwu@proton.me>
2023-04-28 20:10:42 +0900

    feat(catppuccin): initial commit

--- init.lua
+++ init.lua
@@ -5,3 +5,9 @@ vim.loader.enable()
 
 -- Set some options.
 require("settings.options")
+
+-- Enable lazy.nvim
+require("plugins")
+
+-- Set the colorscheme to catppuccin.
+vim.api.nvim_command("colorscheme catppuccin")
