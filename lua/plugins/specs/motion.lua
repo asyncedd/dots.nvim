21931f0be23619f75c12c2ce2b77ab99787828d5
asynecedd <isynqquwu@proton.me>
2023-04-29 07:02:58 +0900

    feat(nvim-treesitter-textsubjects): initial commit.

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -27,4 +27,19 @@ return {
       { "as", mode = { "o", "x" } },
     },
   },
+  {
+    "RRethy/nvim-treesitter-textsubjects",
+    opts = function()
+      return require("plugins.configs.motion.textsubjects")
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+    keys = {
+      { ",", mode = { "o", "x" } },
+      { ".", mode = { "o", "x" } },
+      { ";", mode = { "o", "x" } },
+      { "i;", mode = { "o", "x" } },
+    },
+  },
 }
