37b9c3e8b12598182cbe4d21ebe6ab65a9bf1ff4
asynecedd <isynqquwu@proton.me>
2023-04-28 21:25:43 +0900

    feat(noice): initial commit

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -1,3 +1,20 @@
 return {
   "catppuccin/nvim",
+  {
+    "folke/noice.nvim",
+    config = true,
+    init = function()
+      vim.api.nvim_create_autocmd({ "BufRead", "UIEnter" }, {
+        callback = function()
+          vim.schedule(function()
+            require("lazy").load({ plugins = "noice.nvim" })
+          end)
+        end,
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
 }
