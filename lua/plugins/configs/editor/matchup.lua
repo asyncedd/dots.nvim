e6b375f3d6bfc1a4cdac9dea9584ab26cef2f55f
asynecedd <isynqquwu@proton.me>
2023-04-28 20:55:25 +0900

    feat(matchup): always show matching pairs

--- lua/plugins/configs/editor/matchup.lua
+++ lua/plugins/configs/editor/matchup.lua
@@ -13,6 +13,9 @@ g.matchup_matchparen_offscreen = {}
 -- Defer highlighting to improve cursor movement performance.
 g.matchup_matchparen_deferred = 1
 
+-- Always show surround delimiters always as the cursor moves.
+g.matchup_matchparen_hi_surround_always = 1
+
 return {
   matchup = {
     enable = true,
