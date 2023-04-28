3808d05265cdedab98371610c8f88465e078179f
asynecedd <isynqquwu@proton.me>
2023-04-28 22:17:15 +0900

    feat(lua_ls): improve settings.

--- lua/plugins/configs/lsp/config.lua
+++ lua/plugins/configs/lsp/config.lua
@@ -1,5 +1,7 @@
 local lspconfig = require("lspconfig")
 
-lspconfig["lua_ls"].setup({})
+lspconfig["lua_ls"].setup({
+  settings = require("plugins.configs.lsp.servers.lua_ls"),
+})
 
 vim.cmd("silent! do FileType")
