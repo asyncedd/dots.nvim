a11f4cd6f766f3f5be5b9b04c8fb0d3167b6861d
asynecedd <isynqquwu@proton.me>
2023-04-28 21:27:38 +0900

    feat(treesitter): Improve @variable highlighting.

--- lua/plugins/highlight/editor/treesitter.lua
+++ lua/plugins/highlight/editor/treesitter.lua
@@ -1,5 +1,5 @@
 local colors = require("catppuccin.palettes").get_palette() -- fetch colors from palette
 
 require("catppuccin.lib.highlighter").syntax({
-  ["@variable"] = { fg = colors.red },
+  ["@variable"] = { fg = colors.maroon },
 })
