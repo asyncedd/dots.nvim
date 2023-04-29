95b97a9e9ac0c0d000a0f29901c724f21d9b8ae9
asynecedd <isynqquwu@proton.me>
2023-04-29 14:05:29 +0900

    feat(luasnip): add alot of snippets.

--- lua/plugins/specs/coding.lua
+++ lua/plugins/specs/coding.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+      },
+      {
+        "honza/vim-snippets",
+        config = function()
+          require("luasnip.loaders.from_snipmate").lazy_load()
+        end,
+      },
+      {
+        "molleweide/LuaSnip-snippets.nvim",
+        config = function()
+          require("luasnip.loaders.from_lua").lazy_load()
+        end,
+      },
+    },
+    event = { "VeryLazy", "InsertEnter" },
+  },
+  {
     "hrsh7th/nvim-cmp",
     opts = function()
       return require("plugins.configs.coding.cmp")
