ea37a9b2d18e9d63458e64ccca62a2879726d267
asynecedd <isynqquwu@proton.me>
2023-04-29 20:13:45 +0900

    feat(git): initial commit.

--- lua/plugins/configs/ui/heirline/statusline/init.lua
+++ lua/plugins/configs/ui/heirline/statusline/init.lua
@@ -379,6 +379,59 @@ local Diagnostics = {
   },
 }
 
+local Git = {
+  condition = conditions.is_git_repo,
+
+  init = function(self)
+    self.status_dict = vim.b.gitsigns_status_dict
+    self.has_changes = self.status_dict.added ~= 0 or self.status_dict.removed ~= 0 or self.status_dict.changed ~= 0
+  end,
+
+  hl = { fg = "orange" },
+
+
+  {   -- git branch name
+    provider = function(self)
+      return " " .. self.status_dict.head
+    end,
+    hl = { bold = true }
+  },
+  -- You could handle delimiters, icons and counts similar to Diagnostics
+  {
+    condition = function(self)
+      return self.has_changes
+    end,
+    provider = "("
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.added or 0
+      return count > 0 and ("+" .. count)
+    end,
+    hl = { fg = "git_add" },
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.removed or 0
+      return count > 0 and ("-" .. count)
+    end,
+    hl = { fg = "git_del" },
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.changed or 0
+      return count > 0 and ("~" .. count)
+    end,
+    hl = { fg = "git_change" },
+  },
+  {
+    condition = function(self)
+      return self.has_changes
+    end,
+    provider = ")",
+  },
+}
+
 -- ViMode = utils.surround({ "", "" }, function(self) return self:mode_color() end, { hl = { fg = "black" } })
 
 local DefaultStatusline = {
@@ -386,6 +439,7 @@ local DefaultStatusline = {
   Space,
   FileNameBlock,
   Space,
+  Git,
   Space,
   LSPActive,
   Align,
