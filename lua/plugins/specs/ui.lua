e25774369d87fdb998ee507cc3674b7f35dcf063
asynecedd <isynqquwu@proton.me>
2023-04-29 12:01:40 +0900

    fix(accerleatejk): remove.
    As it conflicts with mini.animate.

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -24,12 +24,4 @@ return {
     end,
     event = "VeryLazy",
   },
-  {
-    "rainbowhxch/accelerated-jk.nvim",
-    config = true,
-    keys = {
-      { "j", "<Plug>(accelerated_jk_gj)" },
-      { "k", "<Plug>(accelerated_jk_gk)" },
-    }
-  },
 }
