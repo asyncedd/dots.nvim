c180c005dd7e789438d96f22601a2d5d2bde0e33
asynecedd <isynqquwu@proton.me>
2023-04-29 12:54:57 +0900

    feat(npairs-integrate-upair): integrate cmp now.

--- lua/plugins/specs/coding.lua
+++ lua/plugins/specs/coding.lua
@@ -19,18 +19,26 @@ return {
     opts = function()
       return require("plugins.configs.coding.autopairs")
     end,
-    config = true,
+    config = function(_, opts)
+      require("npairs-int-upair").setup(opts)
+
+      local event = require('cmp').event
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+      event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
+    end,
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
       "windwp/nvim-autopairs",
       "altermo/ultimate-autopair.nvim",
+      "hrsh7th/nvim-cmp",
     },
   },
   {
     "kylechui/nvim-surround",
     config = true,
     keys = {
-      { "s", mode = "o" },
+      { "s", mode = { "o" } },
     },
   },
   {
