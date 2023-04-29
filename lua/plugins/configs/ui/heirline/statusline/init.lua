cd439def724bb7afa3befab81b4097b4d76a408e
asynecedd <isynqquwu@proton.me>
2023-04-29 20:17:01 +0900

    feat(heirline): properly set colors.

--- lua/plugins/configs/ui/heirline/statusline/init.lua
+++ lua/plugins/configs/ui/heirline/statusline/init.lua
@@ -392,43 +392,31 @@ local Git = {
 
   {   -- git branch name
     provider = function(self)
-      return " " .. self.status_dict.head
+      return " " .. self.status_dict.head .. " "
     end,
     hl = { bold = true }
   },
   -- You could handle delimiters, icons and counts similar to Diagnostics
   {
-    condition = function(self)
-      return self.has_changes
-    end,
-    provider = "("
-  },
-  {
     provider = function(self)
       local count = self.status_dict.added or 0
-      return count > 0 and ("+" .. count)
+      return count > 0 and (" " .. count .. " ")
     end,
     hl = { fg = "git_add" },
   },
   {
     provider = function(self)
       local count = self.status_dict.removed or 0
-      return count > 0 and ("-" .. count)
+      return count > 0 and (" " .. count .. " ")
     end,
     hl = { fg = "git_del" },
   },
   {
     provider = function(self)
       local count = self.status_dict.changed or 0
-      return count > 0 and ("~" .. count)
+      return count > 0 and (" " .. count .. " ")
     end,
     hl = { fg = "git_change" },
-  },
-  {
-    condition = function(self)
-      return self.has_changes
-    end,
-    provider = ")",
   },
 }
 
