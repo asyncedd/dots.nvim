3dd57e602c98e36160070223d655efdf36d092f7
asynecedd <isynqquwu@proton.me>
2023-04-30 21:52:39 +0900

    feat(codeium): initial commit.
    Apperntly, it won't slow down the whole completion engine, and I'm happy
    with that.

--- lua/plugins/specs/coding.lua
+++ lua/plugins/specs/coding.lua
@@ -16,6 +16,15 @@ return {
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
       "onsails/lspkind.nvim",
+      "codeium.nvim",
+    },
+  },
+  {
+    "jcdickinson/codeium.nvim",
+    config = true,
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
   },
 }
