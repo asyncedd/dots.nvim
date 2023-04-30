13a5827bb2d2d3e19f9f746d23edecf5a7b8acd7
asynecedd <isynqquwu@proton.me>
2023-04-30 17:46:33 +0900

    fix(nvim-treesitter): Fix not loading on new file.
    Add a "BufNewFile" and "BufWinEnter" events.

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -7,6 +7,6 @@ return {
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
     end,
-    event = { "BufReadPost" },
+    event = { "BufReadPost", "BufNewFile", "BufWinEnter" },
   },
 }
