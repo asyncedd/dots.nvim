e0ba2beae22be95d8f91588033280da07bf9bc61
asynecedd <isynqquwu@proton.me>
2023-04-30 19:41:28 +0900

    feat(cmp): add icons via lspkind.nvim

--- lua/plugins/configs/coding/cmp.lua
+++ lua/plugins/configs/coding/cmp.lua
@@ -1,4 +1,5 @@
 local luasnip = require("luasnip")
+local lspkind = require("lspkind")
 local cmp = require("cmp")
 
 return {
@@ -46,4 +47,11 @@ return {
     { name = "luasnip" },
     { name = "buffer" },
   }),
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = "symbol_text", -- show only symbol annotations
+      maxwidth = 10, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+    })
+  },
 }
