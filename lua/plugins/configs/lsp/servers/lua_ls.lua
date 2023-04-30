1af8301383e6ffe1293bc2c5fe3f85e3f00dfb89
asynecedd <isynqquwu@proton.me>
2023-04-30 18:14:47 +0900

    feat(sumneko_lua): rm unneeded workspace obj
    TY NVChad!

--- lua/plugins/configs/lsp/servers/lua_ls.lua
+++ lua/plugins/configs/lsp/servers/lua_ls.lua
@@ -9,7 +9,6 @@ return {
       library = {
         [vim.fn.expand "$VIMRUNTIME/lua"] = true,
         [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-        [vim.fn.stdpath "data" .. "/lazy/extensions/nvchad_types"] = true,
         [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
       },
       maxPreload = 100000,
