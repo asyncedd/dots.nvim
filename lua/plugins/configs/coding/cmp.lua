fd1c1386d676a97fc300d59960dd3e0e46bfbb06
asynecedd <isynqquwu@proton.me>
2023-04-30 19:52:28 +0900

    feat(cmp): Make max length longer.
    Useful for looooooong paths!

--- lua/plugins/configs/coding/cmp.lua
+++ lua/plugins/configs/coding/cmp.lua
@@ -50,7 +50,7 @@ return {
   formatting = {
     format = lspkind.cmp_format({
       mode = "symbol_text", -- show only symbol annotations
-      maxwidth = 10, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      maxwidth = 30, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
     })
   },
