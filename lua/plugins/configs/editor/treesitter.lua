287e3ebf7a02143063fea7e769ae1718272aee0e
asynecedd <isynqquwu@proton.me>
2023-04-30 17:40:51 +0900

    feat(nvim-treesitter): initial commit.

---
+++ lua/plugins/configs/editor/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "vimdoc",
+    "query",
+    "regex",
+    "bash",
+    "markdown",
+    "markdown_inline",
+    "luap",
+    "luadoc",
+  },
+
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+  },
+
+  indent = {
+    enable = true,
+  },
+}
---
+++ lua/plugins/specs/editor.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      return require("plugins.configs.editor.treesitter")
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+    event = { "BufReadPost" },
+  },
+}
