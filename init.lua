2e64892d9ff87fb43c28a4f8f04eec7b7a47b86d
asynecedd <isynqquwu@proton.me>
2023-04-30 14:15:04 +0900

    feat(options): initial commit.
    Map <Leader> to <Space>

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- Enable the experimental new loader.
 vim.loader.enable()
+
+-- Set some options.
+require("settings.options")
---
+++ lua/settings/options.lua
@@ -0,0 +1,7 @@
+local o = vim.opt
+local g = vim.g
+
+-- 1. General
+
+-- Set <leader> to be <Space>.
+g.mapleader = " "
