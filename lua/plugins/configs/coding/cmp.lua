b53f9aaaae35dd8bbe3672170a9f1e429e55a71c
asynecedd <isynqquwu@proton.me>
2023-05-01 20:23:02 +0900

    perf(cmp): remove autocmd.
    For performance!

--- lua/plugins/configs/coding/cmp.lua
+++ lua/plugins/configs/coding/cmp.lua
@@ -2,29 +2,6 @@ local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 local cmp = require("cmp")
 
-vim.api.nvim_create_autocmd(
-  { "TextChangedI", "TextChangedP" },
-  {
-    callback = function()
-      local line = vim.api.nvim_get_current_line()
-      local cursor = vim.api.nvim_win_get_cursor(0)[2]
-
-      local current = string.sub(line, cursor, cursor + 1)
-      if current == "." or current == "," or current == " " then
-        require("cmp").close()
-      end
-
-      local before_line = string.sub(line, 1, cursor + 1)
-      local after_line = string.sub(line, cursor + 1, -1)
-      if not string.match(before_line, '^%s+$') then
-        if after_line == "" or string.match(before_line, " $") or string.match(before_line, "%.$") then
-          require("cmp").complete()
-        end
-      end
-    end,
-    pattern = "*"
-  })
-
 return {
   snippet = {
     expand = function(args)
