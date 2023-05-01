9c0479770ca5d8677c7d9bf4c55b7c17df03e1f1
asynecedd <isynqquwu@proton.me>
2023-05-01 20:29:11 +0900

    style(options): fix indentation for gcr array.

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -293,9 +293,9 @@ o.tgc = true
 --
 -- This is from the help docs, it enables mode shapes, "Cursor" highlight, and blinking
 o.gcr = {
-    [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]],
-    [[a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor]],
-    [[sm:block-blinkwait175-blinkoff150-blinkon175]],
+  [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]],
+  [[a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor]],
+  [[sm:block-blinkwait175-blinkoff150-blinkon175]],
 }
 
 --- Pum options. (Popup menu)
