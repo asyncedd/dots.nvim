db0ec5e79d2b01bef14371179cfef5c1de04e734
asynecedd <isynqquwu@proton.me>
2023-05-02 16:46:31 +0900

    feat(luasnip): enable history and some cool events.

--- lua/plugins/configs/coding/luasnip.lua
+++ lua/plugins/configs/coding/luasnip.lua
@@ -1,3 +1,10 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
 require("luasnip.loaders.from_lua").lazy_load({ paths = { "./luasnip" } })
+
+require("luasnip").setup({
+  history = true,
+  update_events = { "TextChanged", "TextChangedI" },
+  region_check_events = { "CursorMoved", "CursorHold", "InsertEnter" },
+  delete_check_events = { "TextChanged", "InsertLeave" },
+})
