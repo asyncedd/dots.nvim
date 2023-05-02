0b8b011c2b9dcc5486d7181b4bc5f17d2a5887e3
asynecedd <isynqquwu@proton.me>
2023-05-02 20:45:18 +0900

    feat(nvim-spider): initial commit.

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -72,4 +72,13 @@ return {
     end,
     event = "VeryLazy",
   },
+  {
+    "chrisgrieser/nvim-spider",
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+      { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+    }
+  },
 }
