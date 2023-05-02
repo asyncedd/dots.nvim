cda6f93751f3b92b77eb7871b7a9f3b93ed226fb
asynecedd <isynqquwu@proton.me>
2023-05-02 20:22:59 +0900

    feat(mini-ai): initial commit.
    Also don't load nvim-treesitter-textobjects but, use mini.ai for the
    textobj stuff.

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -8,6 +8,23 @@ return {
       require("nvim-treesitter.configs").setup(opts)
     end,
     event = "VeryLazy",
+    init = function()
+      -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+      local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      local enabled = false
+      if opts.textobjects then
+        for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+          if opts.textobjects[mod] and opts.textobjects[mod].enable then
+            enabled = true
+            break
+          end
+        end
+      end
+      if not enabled then
+        require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+      end
+    end,
   },
   {
     "ggandor/leap.nvim",
@@ -25,6 +42,16 @@ return {
     dependencies = {
       "leap.nvim",
     },
+    event = "VeryLazy",
+  },
+  {
+    "echasnovski/mini.ai",
+    opts = function()
+      return require("plugins.configs.motion.ai")
+    end,
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+    end,
     event = "VeryLazy",
   },
 }
