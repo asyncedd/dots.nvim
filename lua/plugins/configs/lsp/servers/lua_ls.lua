f5f388a04da5a9cde1702d063e19db92d7fa1d73
asynecedd <isynqquwu@proton.me>
2023-05-04 08:25:50 +0900

    feat(lsp): imporve loading time.

--- lua/plugins/configs/lsp/servers/lua_ls.lua
+++ lua/plugins/configs/lsp/servers/lua_ls.lua
@@ -8,7 +8,7 @@ return {
     workspace = {
       library = {
         [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-        [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+        -- [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
         [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
       },
       maxPreload = 100000,
