71e6fbb6bd019488efca69e0483050f1cf618ecb
asynecedd <isynqquwu@proton.me>
2023-05-04 08:57:57 +0900

    perf(nvim-treesitter-textobjects): remove textobj for classes.
    Also mini.ai takes care of this.

--- lua/plugins/configs/motion/nvim-treesitter-textobjects.lua
+++ lua/plugins/configs/motion/nvim-treesitter-textobjects.lua
@@ -8,10 +8,6 @@ return {
 
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
-        ["ac"] = "@class.outer",
-        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-        -- nvim_buf_set_keymap) which plugins like which-key display
-        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
         -- You can also use captures from other query groups like `locals.scm`
         ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
       },
