ad0b637e2839e9d8015a5dc593566926a481006a
asyncedd <isynqquwu@proton.me>
2023-05-04 17:16:23 +0900

    feat(readline): initial commit.

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -129,4 +129,18 @@ return {
       "<A-s>",
     },
   },
+  {
+    "linty-org/readline.nvim",
+    keys = {
+      { "<M-f>", function() require("readline").forward_word() end, mode = "!" },
+      { "<M-b>", function() require("readline").backward_word() end, mode = "!" },
+      { "<C-a>", function() require("readline").beginning_of_line() end, mode = "!" },
+      { "<C-e>", function() require("readline").end_of_line() end, mode = "!" },
+      { "<M-d>", function() require("readline").kill_word() end, mode = "!" },
+      { "<M-BS>", function() require("readline").backward_kill_word() end, mode = "!" },
+      { "<C-w>", function() require("readline").unix_word_rubout() end, mode = "!" },
+      { "<C-k>", function() require("readline").kill_line() end, mode = "!" },
+      { "<C-u>", function() require("readline").backward_kill_line() end, mode = "!" },
+    },
+  },
 }
