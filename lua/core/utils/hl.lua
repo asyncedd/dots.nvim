3c1a0e2c7fc94bbf71d0e5d2f204ebe12ca2ac18
asyncedd <isynqquwu@proton.me>
2023-05-04 19:10:32 +0900

    fix(pummenu): fix typo.
    fg -> bg

--- lua/core/utils/hl.lua
+++ lua/core/utils/hl.lua
@@ -1,4 +1,4 @@
 local colors = require("catppuccin.palettes").get_palette()
 
-vim.api.nvim_set_hl(0, "Pmenu", { fg = colors.crust })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = colors.crust })
 -- vim.api.nvim_set_hl(0, "Pmenusel", { blend = 0 })
