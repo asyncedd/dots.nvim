76ba1748e74d5fc217b3194bae5c5628ee179f9f
asynecedd <isynqquwu@proton.me>
2023-05-04 09:03:22 +0900

    refactor(nvim-treesitter-textobjects): remove config
    it's only needed for mini.ai and doesn't need to cease to exist.

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -1,12 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    opts = function()
-      return require("plugins.configs.motion.nvim-treesitter-textobjects")
-    end,
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
     event = "VeryLazy",
     init = function()
       -- PERF: no need to load the plugin, if we only need its queries for mini.ai
