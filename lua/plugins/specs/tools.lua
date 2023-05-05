015431b5634272aee864d50e112064c401ceb663
asyncedd <isynqquwu@proton.me>
2023-05-06 06:57:51 +0900

    feat(neogit): actually change the ft to gitcommit.

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -7,13 +7,13 @@ return {
     config = function(_, opts)
       require("neogit").setup(opts)
 
-      -- vim.api.nvim_create_augroup("neogit-additions", {})
-      --
-      -- vim.api.nvim_create_autocmd("FileType", {
-      --   group = "neogit-additions",
-      --   pattern = "NeogitCommitMessage",
-      --   command = "silent! set filetype=gitcommit",
-      -- })
+      vim.api.nvim_create_augroup("neogit-additions", {})
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = "neogit-additions",
+        pattern = "NeogitCommitMessage",
+        command = "silent! set filetype=gitcommit",
+      })
     end,
     keys = {
       { "gt", "<cmd>Neogit<CR>" },
