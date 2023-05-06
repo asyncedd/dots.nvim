ce74787fc11e5e8ed563417575acdf04b2347150
asyncedd <isynqquwu@proton.me>
2023-05-06 10:20:47 +0900

    feat(cmp): add some cool icons for tabnine and codeium

--- lua/plugins/configs/coding/cmp.lua
+++ lua/plugins/configs/coding/cmp.lua
@@ -57,6 +57,10 @@ return {
       mode = "symbol_text", -- show only symbol annotations
       maxwidth = 30, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      symbol_map = {
+        Codeium = " ",
+        TabNine = "󱡄 ",
+      },
     }),
   },
   experimental = {
