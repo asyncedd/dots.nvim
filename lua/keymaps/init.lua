46a88050c1c0bd644bf1d008f4ccb143d0824e8d
asyncedd <isynqquwu@proton.me>
2023-05-07 12:09:59 +0900

    feat(keymaps): add search keymaps

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,1 +1,2 @@
 require("keymaps.motion")
+require("keymaps.search")
---
+++ lua/keymaps/search.lua
@@ -0,0 +1,5 @@
+local map = vim.keymap.set
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map({ "n", "x", "o" }, "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map({ "n", "x", "o" }, "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
