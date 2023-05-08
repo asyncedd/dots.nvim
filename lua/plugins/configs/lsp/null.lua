d4a9543029e8e5f8926f837eb1e002e1c59d3b19
asyncedd <isynqquwu@proton.me>
2023-05-09 07:11:21 +0900

    feat(null-ls): ✨ Auto-format on write

--- lua/plugins/configs/lsp/null.lua
+++ lua/plugins/configs/lsp/null.lua
@@ -1,7 +1,24 @@
 local nls = require("null-ls")
+local group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
 
 return {
   sources = {
     nls.builtins.formatting.stylua,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = group,
+        buffer = bufnr,
+      })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = group,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 }
