3f9a9ad0bc392d6b6768c67502a4294672aef7fc
asyncedd <isynqquwu@proton.me>
2023-05-08 15:17:18 +0900

    feat(catppuccin): load catppuccin without a flavor argument.
    require("catppuccin").load(x) -> require("catppuccin").load()

--- init.lua
+++ init.lua
@@ -9,11 +9,14 @@ require("plugins")
 
 -- Catppuccinify the editor.
 -- vim.cmd("colorscheme catppuccin")
-require("catppuccin").load("mocha")
+-- PERF: directly call catppuccin. (not via `vim.cmd("colorscheme x")`)
+-- HACK: Try to make this less hacky?
+require("catppuccin").load()
 
 -- Set up highlights.
 require("core.utils.hl")
 
 vim.schedule(function()
+  -- TODO: Find a better way to load keymaps?
   require("keymaps")
 end)
