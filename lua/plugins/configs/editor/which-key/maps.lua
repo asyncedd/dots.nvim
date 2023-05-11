54d683b9f627d7e9f40dd3a6e1fb2839f09ff8e8
asyncedd <isynqquwu@proton.me>
2023-05-11 18:15:28 +0900

    feat(leap-spooky): ✨ Add custom text object 🦘

--- lua/plugins/configs/editor/which-key/maps.lua
+++ lua/plugins/configs/editor/which-key/maps.lua
@@ -1,1 +1,41 @@
 local wk = require("which-key")
+
+wk.register({
+  [","] = {
+    name = "Local leader",
+    j = "Jump to Treesitter nodes.",
+  },
+}, {
+  mode = { "n", "x", "o" },
+})
+
+wk.register({
+  ["i"] = {
+    name = "Inside... textobject",
+    rf = {
+      function()
+        require("leap").leap({
+          target_windows = { vim.fn.win_getid() },
+          action = require("leap-spooky").spooky_action(function()
+            return "vif"
+          end, { keeppos = true, on_return = (vim.v.operator == "y") and "p" }),
+        })
+      end,
+      "Select inside function",
+    },
+  },
+  ["a"] = {
+    name = "Inside... textobject",
+    rf = {
+      function()
+        require("leap").leap({
+          target_windows = { vim.fn.win_getid() },
+          action = require("leap-spooky").spooky_action(function()
+            return "vaf"
+          end, { keeppos = true, on_return = (vim.v.operator == "y") and "p" }),
+        })
+      end,
+      "Select inside function",
+    },
+  },
+}, { mode = "o" })
