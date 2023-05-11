2327fb2e64d5963a81b71e1e9c09ceee29f72826
asyncedd <isynqquwu@proton.me>
2023-05-11 18:46:59 +0900

    perf(which-key): 🔥 Remove dead code

--- lua/plugins/configs/editor/which-key/maps.lua
+++ lua/plugins/configs/editor/which-key/maps.lua
@@ -1,15 +1,6 @@
 local wk = require("which-key")
 
 wk.register({
-  [","] = {
-    name = "Local leader",
-    j = "Jump to Treesitter nodes.",
-  },
-}, {
-  mode = { "n", "x", "o" },
-})
-
-wk.register({
   ["i"] = {
     name = "inner textobject",
     rf = {
