394c31f3c05cc6641beedbff23ada2e8e87c2a11
asyncedd <isynqquwu@proton.me>
2023-05-12 22:15:56 +0900

    docs(leap): add documentation for what default keymaps there are

--- lua/plugins/configs/motion/leap.lua
+++ lua/plugins/configs/motion/leap.lua
@@ -1,3 +1,8 @@
+-- Add leap.nvim's default mappings.
+--
+-- Which is:
+-- `s`/`S` to leap foward and backwards respectively. (for normal mode)
+-- `x`/`X` to leap foward and backwards until x respectively. (for visual and operator mode)
 require("leap").add_default_mappings()
 
 require("leap").opts.highlight_unlabeled_phase_one_targets = true
