9e48b91b3bf7536ac5e11cad03f76f78b50bdcbe
asyncedd <isynqquwu@proton.me>
2023-05-14 18:00:11 +0900

    perf(treesitter-incremental): 🔥 Remove

--- lua/plugins/configs/editor/treesitter.lua
+++ lua/plugins/configs/editor/treesitter.lua
@@ -49,14 +49,4 @@ return {
     -- It'll override the `=` operator and, others.
     enable = true,
   },
-
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<CR>", -- set to `false` to disable one of the mappings
-      node_incremental = "<CR>",
-      scope_incremental = "<CR>",
-      node_decremental = "<CR>",
-    },
-  },
 }
