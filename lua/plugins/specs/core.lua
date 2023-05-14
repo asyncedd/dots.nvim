6ffe496099fad7a558185316302bb325aa29a1f8
asyncedd <isynqquwu@proton.me>
2023-05-15 06:40:59 +0900

    feat(nvim-web-devicons): ✨ Improve

--- lua/plugins/specs/core.lua
+++ lua/plugins/specs/core.lua
@@ -1,6 +1,14 @@
 return {
   "nvim-lua/plenary.nvim",
-  "nvim-tree/nvim-web-devicons",
+  {
+    "nvim-tree/nvim-web-devicons",
+    dependencies = { "DaikyXendo/nvim-material-icon" },
+    config = function()
+      require("nvim-web-devicons").setup {
+        override = require("nvim-material-icon").get_icons(),
+      }
+    end,
+  },
   "MunifTanjim/nui.nvim",
   "kkharji/sqlite.lua",
   "stevearc/dressing.nvim",
