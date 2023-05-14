6281850cf1a377c6618ec92d01fe728e7995bf55
asyncedd <isynqquwu@proton.me>
2023-05-14 21:44:13 +0900

    feat(RUST): 🦀 Improve config 🦀

---
+++ lua/plugins/configs/coding/rust-tools.lua
@@ -0,0 +1,31 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.completion.completionItem = {
+  documentationFormat = { "markdown", "plaintext" },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolveSupport = {
+    properties = {
+      "documentation",
+      "detail",
+      "additionalTextEdits",
+    },
+  },
+}
+
+return {
+  server = {
+    capabilities = capabilities,
+    on_attach = function()
+      require("rust-tools").inlay_hints.enable()
+    end,
+  },
+  hover_actions = {
+    auto_focus = false,
+  },
+}
