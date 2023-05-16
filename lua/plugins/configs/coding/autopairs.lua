0f5337f8672888366511dbddbaa7156dc2d798f6
asyncedd <isynqquwu@proton.me>
2023-05-16 17:50:23 +0900

    fix(autopairs): 🐛 Fix weird behaviour with "'"

--- lua/plugins/configs/coding/autopairs.lua
+++ lua/plugins/configs/coding/autopairs.lua
@@ -6,7 +6,67 @@ M.upair = {
   },
   space2 = {
     enable = true,
-  }
+  },
+  internal_pairs = {
+    {
+      "[",
+      "]",
+      fly = true,
+      dosuround = true,
+      newline = true,
+      space = true,
+      fastwarp = true,
+      backspace_suround = true,
+    },
+    {
+      "(",
+      ")",
+      fly = true,
+      dosuround = true,
+      newline = true,
+      space = true,
+      fastwarp = true,
+      backspace_suround = true,
+    },
+    {
+      "{",
+      "}",
+      fly = true,
+      dosuround = true,
+      newline = true,
+      space = true,
+      fastwarp = true,
+      backspace_suround = true,
+    },
+    {
+      '"',
+      '"',
+      suround = true,
+      rules = { { "when", { "filetype", "vim" }, { "not", { "regex", "^%s*$" } } } },
+      string = true,
+    },
+    {
+      "'",
+      "'",
+      suround = true,
+      rules = {
+        { "when", { "option", "lisp" }, { "not", { "regex", "^%s*$" } }, { "instring" } },
+      },
+      alpha = true,
+      nft = {
+        "tex",
+      },
+      string = true,
+    },
+    { "`", "`", nft = { "tex" } },
+    { "``", "''", ft = { "tex" } },
+    { "```", "```", newline = true, ft = { "markdown" } },
+    { "<!--", "-->", ft = { "markdown", "html" } },
+    { '"""', '"""', newline = true, ft = { "python" } },
+    { "'''", "'''", newline = true, ft = { "python" } },
+    { "string", type = "tsnode", string = true },
+    { "raw_string", type = "tsnode", string = true },
+  },
 }
 
 return M
