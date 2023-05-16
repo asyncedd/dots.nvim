5d5dca4a34759b171f85759268141d11944cc57f
asyncedd <isynqquwu@proton.me>
2023-05-16 22:44:51 +0900

    feat(mini-ai): 🌸 Use `c` for comments
    I really don't use classes in my workflow. (I don't with langauges with
    classes)

--- lua/plugins/configs/motion/ai.lua
+++ lua/plugins/configs/motion/ai.lua
@@ -8,7 +8,7 @@ return {
       i = { "@block.inner", "@conditional.inner", "@loop.inner" },
     }, {}),
     f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+    c = ai.gen_spec.treesitter({ a = "@comment.outer", i = "@comment.inner" }, {}),
     s = ai.gen_spec.treesitter({ a = "@scope", i = "@scope.inner" }, {}),
   },
   search_method = "cover_or_nearest",
