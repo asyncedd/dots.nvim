9f5b79726ecd86da0f0d2723a2ca340391a2995e
asyncedd <isynqquwu@proton.me>
2023-05-16 18:24:30 +0900

    feat(heirline): 🌸 Remove working directory

--- lua/plugins/configs/ui/heirline.lua
+++ lua/plugins/configs/ui/heirline.lua
@@ -202,20 +202,6 @@ M.FileFlags = {
   },
 }
 
-M.WorkDir = {
-  provider = function()
-    local icon = (vim.fn.haslocaldir(0) == 1 and "l" or "g") .. " " .. " "
-    local cwd = vim.fn.getcwd(0)
-    cwd = vim.fn.fnamemodify(cwd, ":~")
-    if not conditions.width_percent_below(#cwd, 0.25) then
-      cwd = vim.fn.pathshorten(cwd)
-    end
-    local trail = cwd:sub(-1) == "/" and "" or "/"
-    return icon .. cwd .. trail
-  end,
-  hl = { fg = "blue", bold = true },
-}
-
 -- let's add the children to our FileNameBlock component
 M.FileNameBlock = utils.insert(
   M.FileNameBlock,
@@ -419,7 +405,6 @@ M.wpm = {
 M.StatusLine = {
   M.Vim,
   M.Space,
-  M.WorkDir,
   M.Space,
   M.FileNameBlock,
   M.Space,
