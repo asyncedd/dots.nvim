a5346ee58a8238575dd43f50af118a3d5220cb99
asyncedd <isynqquwu@proton.me>
2023-05-17 16:07:56 +0900

    perf(neogit): 🔥 Remove
    Now, I've learned (I promise) Lazygit's potential, now I feel I just
    might not need it!

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -1,26 +1,5 @@
 return {
   {
-    "TimUntersberger/neogit",
-    opts = function()
-      return require("plugins.configs.tools.neogit")
-    end,
-    config = function(_, opts)
-      require("neogit").setup(opts)
-
-      vim.api.nvim_create_augroup("neogit-additions", {})
-
-      vim.api.nvim_create_autocmd("FileType", {
-        group = "neogit-additions",
-        pattern = "NeogitCommitMessage",
-        command = "silent! set filetype=gitcommit",
-      })
-    end,
-    keys = {
-      { "gt", "<cmd>Neogit<CR>", desc = "Toggle Neogit" },
-    },
-    cmd = "Neogit",
-  },
-  {
     "nvim-telescope/telescope.nvim",
     config = function()
       require("telescope").setup()
