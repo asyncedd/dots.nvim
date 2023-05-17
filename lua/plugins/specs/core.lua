7f89fbd8c58ff8b34ef2cf74b7f0c08e1bd871ca
asyncedd <isynqquwu@proton.me>
2023-05-17 15:22:43 +0900

    perf(material-icons): 🔥 Remove

--- lua/plugins/specs/core.lua
+++ lua/plugins/specs/core.lua
@@ -1,14 +1,6 @@
 return {
   "nvim-lua/plenary.nvim",
-  {
-    "nvim-tree/nvim-web-devicons",
-    dependencies = { "DaikyXendo/nvim-material-icon" },
-    config = function()
-      require("nvim-web-devicons").setup({
-        override = require("nvim-material-icon").get_icons(),
-      })
-    end,
-  },
+  "nvim-tree/nvim-web-devicons",
   "MunifTanjim/nui.nvim",
   "kkharji/sqlite.lua",
   "stevearc/dressing.nvim",
