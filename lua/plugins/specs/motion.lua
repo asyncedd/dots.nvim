87a803b6998f7754c9698934769e378bf3a6f41b
asyncedd <isynqquwu@proton.me>
2023-05-19 16:07:34 +0900

    feat(leap): ✨ Add super cool keybind for illuminate jumping

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -31,6 +31,27 @@ return {
       { "X", "", mode = { "x", "o" }, desc = "Leap backward till" },
       { "gs", mode = { "x", "o" }, desc = "Leap from window" },
       { "gS", mode = { "x", "o" }, desc = "Leap backward to" },
+      {
+        "gz",
+        function()
+          local ref = require("illuminate.reference").buf_get_references(vim.api.nvim_get_current_buf())
+          if not ref or #ref == 0 then
+            return false
+          end
+
+          local targets = {}
+          for _, v in pairs(ref) do
+            table.insert(targets, {
+              pos = { v[1][1] + 1, v[1][2] + 1 },
+            })
+          end
+
+          require("leap").leap({ targets = targets, target_windows = { vim.api.nvim_get_current_win() } })
+
+          return true
+        end,
+        mode = { "n", "x", "o" },
+      },
     },
   },
   {
