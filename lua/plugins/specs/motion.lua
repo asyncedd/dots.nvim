3ece501aae8d780549062b33cb52d076350599fd
asyncedd <isynqquwu@proton.me>
2023-05-20 08:42:12 +0900

    feat(leap-search): ✨ Initial commit

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -316,4 +316,49 @@ return {
       { "gn", "<cmd>lua require('leap-ast').leap()<CR>", mode = { "x", "n", "o" } },
     },
   },
+  {
+    "atusy/leap-search.nvim",
+    dependencies = {
+      "leap.nvim",
+    },
+    keys = {
+      {
+        "<leader>n",
+        function()
+          local pat = vim.fn.getreg("/")
+          local leapable = require("leap-search").leap(pat)
+          if not leapable then
+            return vim.fn.search(pat)
+          end
+        end,
+        mode = { "x", "n", "o" },
+      },
+      {
+        "<leader>N",
+        function()
+          local pat = vim.fn.getreg("/")
+          local leapable = require("leap-search").leap(pat, {}, { backward = true })
+          if not leapable then
+            return vim.fn.search(pat, "b")
+          end
+        end,
+        mode = { "x", "n", "o" },
+      },
+      {
+        "<leader>mn",
+        function()
+          require("leap-search").leap(
+            nil,
+            {
+              engines = {
+                { name = "string.find", plain = true, ignorecase = true },
+                -- { name = "kensaku.query" }, -- to search Japanese string with romaji with https://github.com/lambdalisue/kensaku.vim
+              },
+            },
+            { target_windows = { vim.api.nvim_get_current_win() } }
+          )
+        end,
+      },
+    },
+  },
 }
