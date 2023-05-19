99f5d3bef4a060933ccb711ce891cd5756e5c616
asyncedd <isynqquwu@proton.me>
2023-05-19 15:38:05 +0900

    feat(killersheep): ✨ Catppuccinify killersheep

--- lua/plugins/configs/ui/catppuccin.lua
+++ lua/plugins/configs/ui/catppuccin.lua
@@ -135,6 +135,17 @@ return {
       -- Harpoon
       HarpoonWindow = { fg = C.text, bg = C.mantle },
       HarpoonBorder = { fg = C.mantle, bg = C.mantle },
+
+      -- Killersheep
+      introHL = { fg = C.teal, style = { "bold" } },
+      KillerBlood = { fg = C.red },
+      killerCannon = { fg = C.blue },
+      killerBullet = { bg = C.red },
+      KillerSheep = { fg = C.green },
+      KillerSheep2 = { fg = C.cyan },
+      KillerPoop = { bg = C.text, fg = C.text },
+      KillerLevel = { bg = C.blue, fg = C.text },
+      KillerLevelX = { bg = C.yellow, fg = C.blue },
     }
   end,
   integrations = {
