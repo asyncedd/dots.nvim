7a85c510ca50f19ae276d401255f035dcce66fdc
asyncedd <isynqquwu@proton.me>
2023-05-20 10:31:32 +0900

    syntax-tree-surfer: 🔥 Remove
    CHANGES:
    - Remove the unneeded config file

--- lua/plugins/configs/motion/syntax-tree-surfer.lua
+++
@@ -1,9 +0,0 @@
-local colors = require("catppuccin.palettes").get_palette()
-
-require("catppuccin.lib.highlighter").syntax({
-  STS_hl = { fg = colors.sky },
-})
-
-return {
-  highlight_group = "STS_hl",
-}
