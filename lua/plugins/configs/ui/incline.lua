132e0fd3daaa10f070f61d89a53714a1155b5901
asyncedd <isynqquwu@proton.me>
2023-05-20 10:05:04 +0900

    feat(incline): ✨ Add Gitsigns diff
    CHANGES:
    - Add a gitsigns diff

--- lua/plugins/configs/ui/incline.lua
+++ lua/plugins/configs/ui/incline.lua
@@ -16,13 +16,62 @@ end
 return {
   debounce_threshold = { falling = 500, rising = 250 },
   render = function(props)
+    if not props.focused then
+      return ""
+    end
+
+    local count = vim.fn.searchcount({ recompute = 1, maxcount = -1 })
+    local contents = vim.fn.getreg("/")
+    if string.len(contents) == 0 then
+      return ""
+    end
+
     local bufname = vim.api.nvim_buf_get_name(props.buf)
     local filename = vim.fn.fnamemodify(bufname, ":t")
     local diagnostics = get_diagnostic_label(props)
     local modified = vim.api.nvim_buf_get_option(props.buf, "modified") and "bold,italic" or "None"
     local filetype_icon, color = require("nvim-web-devicons").get_icon_color(filename)
 
+    local function get_git_diff()
+      local git_icon = {
+        removed = "",
+        changed = "",
+        added = "",
+      }
+      local labels = {}
+      local signs = vim.api.nvim_buf_get_var(props.buf, "gitsigns_status_dict")
+      -- local signs = vim.b.gitsigns_status_dict
+      for name, icon in pairs(git_icon) do
+        if tonumber(signs[name]) and signs[name] > 0 then
+          table.insert(labels, {
+            icon .. " " .. signs[name] .. " ",
+            group = "Diff" .. name,
+          })
+        end
+      end
+      if #labels > 0 then
+        table.insert(labels, { "| " })
+      end
+      return labels
+    end
+
     local buffer = {
+      -- https://github.com/davidosomething/dotfiles/issues/498#issuecomment-1458400313
+      {
+        " ? ",
+        group = "dkoStatusKey",
+      },
+      {
+        (" %s "):format(contents),
+        group = "IncSearch",
+      },
+      {
+        (" %d/%d "):format(count.current, count.total),
+        group = "dkoStatusValue",
+      },
+      { " " },
+      { get_git_diff() },
+      { " " },
       { filetype_icon, guifg = color },
       { " " },
       { filename, gui = modified },
@@ -36,4 +85,8 @@ return {
     end
     return diagnostics
   end,
+  window = {
+    margin = { horizontal = 0 },
+    padding = 0,
+  },
 }
