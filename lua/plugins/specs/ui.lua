0ece9fbf5bf0b052221751b15941faa67c584408
asyncedd <isynqquwu@proton.me>
2023-05-22 07:29:00 +0900

    perf(neoscroll): ⚡ Only load on keys
    CHANGES:
    - Load Neoscroll on keys.
    
    THE WARNING:
    - It may cause slowdowns on initial load (since, they all need to be
      created on startup but, it shouldn't introduce as much overhead)

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -140,7 +140,17 @@ return {
     opts = function()
       return require("plugins.configs.ui.neoscroll")
     end,
-    event = "VeryLazy",
+    keys = {
+      { "<C-u>", modes = { "n", "x", "o" } },
+      { "<C-d>", modes = { "n", "x", "o" } },
+      { "<C-b>", modes = { "n", "x", "o" } },
+      { "<C-f>", modes = { "n", "x", "o" } },
+      { "<C-y>", modes = { "n", "x", "o" } },
+      { "<C-e>", modes = { "n", "x", "o" } },
+      { "zt", modes = { "n", "x", "o" } },
+      { "zz", modes = { "n", "x", "o" } },
+      { "zb", modes = { "n", "x", "o" } },
+    },
   },
   {
     "folke/drop.nvim",
