4bf70321c0eabfb705de6f5ef0061793e038c2fc
asyncedd <isynqquwu@proton.me>
2023-05-26 15:26:34 +0900

    perf(null-ls): ⚡ Improve formatting performance

--- lua/plugins/configs/lsp/null.lua
+++ lua/plugins/configs/lsp/null.lua
@@ -14,17 +14,31 @@ return {
     nls.builtins.formatting.prettier,
   },
   on_attach = function(client, bufnr)
+    if
+      client.config
+      and client.config.capabilities
+      and client.config.capabilities.documentFormattingProvider == false
+    then
+      return
+    end
     if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
-        group = group,
-        buffer = bufnr,
-      })
-
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = group,
-        buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
+          local buf = vim.api.nvim_get_current_buf()
+          local ft = vim.bo[buf].filetype
+          local have_nls = package.loaded["null-ls"]
+            and (#require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0)
+
+          vim.lsp.buf.format(vim.tbl_deep_extend("force", {
+            bufnr = buf,
+            filter = function(clientn)
+              if have_nls then
+                return clientn.name == "null-ls"
+              end
+              return clientn.name ~= "null-ls"
+            end,
+          }, {}))
         end,
       })
     end
