c2d0d4078ce3a35afa5370773adacdf15980e7f6
asyncedd <isynqquwu@proton.me>
2023-05-27 14:53:29 +0900

    perf(lsp-inlayhints): 🔥 Remove

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -74,7 +74,6 @@ return {
       "mason-lspconfig.nvim",
       "neodev.nvim",
       "b0o/SchemaStore.nvim",
-      "lsp-inlayhints.nvim",
     },
     event = "User After",
   },
@@ -176,26 +175,5 @@ return {
       { "<leader>xq", "<cmd>TroubleToggle loclist<cr>", desc = "Show loclist" },
       { "<leader>xl", "<cmd>TroubleToggle quickfix<cr>", desc = "Show Quickfix" },
     },
-  },
-  {
-    "lvimuser/lsp-inlayhints.nvim",
-    opts = {},
-    config = function(_, opts)
-      require("lsp-inlayhints").setup(opts)
-
-      vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = "LspAttach_inlayhints",
-        callback = function(args)
-          if not (args.data and args.data.client_id) then
-            return
-          end
-
-          local bufnr = args.buf
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          require("lsp-inlayhints").on_attach(client, bufnr)
-        end,
-      })
-    end,
   },
 }
