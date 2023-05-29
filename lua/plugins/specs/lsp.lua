ed085ed19945657d688cb84df4b9941e8efc82d9
asyncedd <isynqquwu@proton.me>
2023-05-29 22:20:06 +0900

    feat(tsserver): ✨ Initial commit
    Add a language server for Typescript.
    I've used folke's config :)

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -77,6 +77,33 @@ return {
             },
           },
           svelte = {},
+          -- https://github.com/folke/dot/blob/master/nvim/lua/plugins/lsp.lua
+          tsserver = {
+            settings = {
+              typescript = {
+                inlayHints = {
+                  includeInlayParameterNameHints = "literal",
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                  includeInlayFunctionParameterTypeHints = true,
+                  includeInlayVariableTypeHints = false,
+                  includeInlayPropertyDeclarationTypeHints = true,
+                  includeInlayFunctionLikeReturnTypeHints = true,
+                  includeInlayEnumMemberValueHints = true,
+                },
+              },
+              javascript = {
+                inlayHints = {
+                  includeInlayParameterNameHints = "all",
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                  includeInlayFunctionParameterTypeHints = true,
+                  includeInlayVariableTypeHints = true,
+                  includeInlayPropertyDeclarationTypeHints = true,
+                  includeInlayFunctionLikeReturnTypeHints = true,
+                  includeInlayEnumMemberValueHints = true,
+                },
+              },
+            },
+          },
         },
       }
     end,
