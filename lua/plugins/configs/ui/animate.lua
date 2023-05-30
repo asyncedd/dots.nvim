beec38642f0d1ad8cf3a26ce5e2f17836733eea8
asyncedd <isynqquwu@proton.me>
2023-05-30 16:42:35 +0900

    feat(mini-animate): ✨ Make it disable on <MouseScrolls>
    It's kinda annoying elsewhere

--- lua/plugins/configs/ui/animate.lua
+++ lua/plugins/configs/ui/animate.lua
@@ -1,7 +1,27 @@
-local animate = require("mini.animate")
+local mouse_scrolled = false
+for _, scroll in ipairs({ "Up", "Down" }) do
+  local key = "<ScrollWheel" .. scroll .. ">"
+  vim.keymap.set({ "", "i" }, key, function()
+    mouse_scrolled = true
+    return key
+  end, { expr = true })
+end
 
+local animate = require("mini.animate")
 return {
   resize = {
     timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+  },
+  scroll = {
+    timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+    subscroll = animate.gen_subscroll.equal({
+      predicate = function(total_scroll)
+        if mouse_scrolled then
+          mouse_scrolled = false
+          return false
+        end
+        return total_scroll > 1
+      end,
+    }),
   },
 }
