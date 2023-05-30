5d07903c8031e6a9be267ff9c457c05b431462fd
asyncedd <isynqquwu@proton.me>
2023-05-30 14:12:30 +0900

    feat(hipatterns): ✨ Add Tailwind colors
    Nvim-colorizer has problems colorizing tailwind colors if `names` is off

--- lua/plugins/configs/ui/hipatterns/init.lua
+++ lua/plugins/configs/ui/hipatterns/init.lua
@@ -45,5 +45,9 @@ return {
         return M.hipatterns.compute_hex_color_group(hex, "bg")
       end,
     },
+    tailwind = {
+      pattern = require("plugins.configs.ui.hipatterns.tailwind").pattern,
+      group = require("plugins.configs.ui.hipatterns.tailwind").group,
+    },
   },
 }
