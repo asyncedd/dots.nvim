7f1a530dccc437b5f8508d5e2c3b4c3ea7967b10
asyncedd <isynqquwu@proton.me>
2023-05-31 14:46:17 +0900

    fix(indent-blankline): Disable in certain filetypes
    Stole this from LazyVim so, kudos to them :)

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -68,6 +68,14 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "VeryLazy",
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "notify" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
   },
   {
     "echasnovski/mini.indentscope",
