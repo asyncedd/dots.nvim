175031f2a49956bc248e22d689f5bf669b4e2ad8
asyncedd <isynqquwu@proton.me>
2023-05-31 20:34:25 +0900

    feat(catppuccin): ✨ Make cmp's kind look cleaner
    Now, it's better :D

--- lua/plugins/configs/ui/catppuccin.lua
+++ lua/plugins/configs/ui/catppuccin.lua
@@ -31,11 +31,6 @@ return {
     },
   },
   custom_highlights = function(C)
-    local utils = require("catppuccin.utils.colors")
-    local tint = function(tint)
-      return utils.blend(tint, C.base, 0.2)
-    end
-
     local U = require("catppuccin.utils.colors")
     return {
       -- General
@@ -60,45 +55,8 @@ return {
       CmpItemAbbrMatchFuzzy = { fg = C.sky, style = { "bold" } },
       PmenuSel = { bg = C.sky, fg = C.mantle, style = { "bold" } },
 
-      -- kind support
-      CmpItemKindField = { fg = C.rosewater, bg = tint(C.rosewater) },
-      CmpItemKindProperty = { fg = C.rosewater, bg = tint(C.rosewater) },
-      CmpItemKindEvent = { fg = C.rosewater, bg = tint(C.rosewater) },
-
-      CmpItemKindText = { fg = C.green, bg = tint(C.green) },
-      CmpItemKindModule = { fg = C.text, bg = tint(C.text) },
-      CmpItemKindVariable = { fg = C.rosewater, bg = tint(C.rosewater) },
-      CmpItemKindUnit = { fg = C.text, bg = tint(C.text) },
-      CmpItemKindValue = { fg = C.text, bg = tint(C.text) },
-
-      CmpItemKindEnum = { fg = C.yellow, bg = tint(C.yellow) },
-      CmpItemKindReference = { fg = C.yellow, bg = tint(C.yellow) },
-      CmpItemKindClass = { fg = C.yellow, bg = tint(C.yellow) },
-      CmpItemKindFolder = { fg = C.yellow, bg = tint(C.yellow) },
-      CmpItemKindEnumMember = { fg = C.yellow, bg = tint(C.yellow) },
-      CmpItemKindInterface = { fg = C.yellow, bg = tint(C.yellow) },
-
-      CmpItemKindKeyword = { fg = C.mauve, bg = tint(C.mauve) },
-
-      CmpItemKindConstant = { fg = C.peach, bg = tint(C.peach) },
-
-      CmpItemKindConstructor = { fg = C.lavender, bg = tint(C.lavender) },
-
-      CmpItemKindFunction = { fg = C.blue, bg = tint(C.blue) },
-      CmpItemKindMethod = { fg = C.blue, bg = tint(C.blue) },
-
-      CmpItemKindStruct = { fg = C.teal, bg = tint(C.teal) },
-      CmpItemKindOperator = { fg = C.teal, bg = tint(C.teal) },
-
-      CmpItemKindSnippet = { fg = C.flamingo, bg = tint(C.flamingo) },
-
-      CmpItemKindFile = { fg = C.sky, bg = tint(C.sky) },
-
-      CmpItemKindColor = { fg = C.pink, bg = tint(C.pink) },
-      CmpItemKindTypeParameter = { fg = C.maroon, bg = tint(C.maroon) },
-
-      CmpItemKindCodeium = { fg = C.green, bg = tint(C.green) },
-      CmpItemKindTabNine = { fg = C.lavender, bg = tint(C.lavender) },
+      CmpItemKindCodeium = { fg = C.green },
+      CmpItemKindTabNine = { fg = C.lavender },
 
       -- Telescope
       TelescopeMatching = { fg = C.sky },
@@ -121,57 +79,13 @@ return {
       IlluminatedWordWrite = { bg = U.darken(C.surface1, 0.7, C.base) },
 
       -- Indentscope
-      MiniIndentscopeSymbol = { fg = utils.blend(C.sky, C.base, 0.3) },
-
-      -- Semantic tokens
-
-      ["@lsp.type.interface"] = { fg = C.flamingo },
-      -- Semantic tokens
-      ["@class"] = {},
-      ["@struct"] = {},
-      ["@enum"] = {},
-      ["@enumMember"] = {},
-      ["@event"] = {},
-      ["@interface"] = {},
-      ["@modifier"] = {},
-      ["@regexp"] = {},
-      ["@typeParameter"] = {},
-      ["@decorator"] = {},
-
-      -- Language spectific
-      -- bash
-      ["@function.builtin.bash"] = { fg = C.red, style = { "italic" } },
-
-      -- java
-      ["@constant.java"] = { fg = C.teal },
-
-      ["@constructor.lua"] = { fg = C.flamingo }, -- For constructor calls and definitions: = { } in Lua.
-      ["@field.lua"] = { fg = C.lavender },
-
-      -- C/CPP
-      ["@type.builtin.c"] = { style = {} },
-      ["@property.cpp"] = { fg = C.text },
-      ["@type.builtin.cpp"] = { style = {} },
-
-      -- Misc
-      gitcommitSummary = { fg = C.rosewater, style = { "italic" } },
-      zshKSHFunction = { link = "Function" },
-
-      -- Lazy.nvim
-      LazyH1 = { bg = C.sky, fg = C.crust, style = { "bold", "italic" } },
+      MiniIndentscopeSymbol = { fg = U.blend(C.sky, C.base, 0.3) },
 
       -- Leap
       LeapBackdrop = { link = "Comment" },
       LeapMatch = { fg = C.sky, style = { "bold", "nocombine" } },
       LeapLabelPrimary = { fg = C.blue, style = { "bold", "nocombine" } },
       LeapLabelSecondary = { fg = C.sapphire, style = { "bold", "nocombine" } },
-
-      -- Harpoon
-      HarpoonWindow = { fg = C.text, bg = C.mantle },
-      HarpoonBorder = { fg = C.mantle, bg = C.mantle },
-
-      -- LazyGit
-      LazyGitFloat = { bg = C.mantle },
     }
   end,
   integrations = {
--- lua/settings/settings.lua
+++ lua/settings/settings.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 -- It can be catppuccin tokyonight, onedarkpro, or kanagawa
-M.colorscheme = "onedark"
+M.colorscheme = "catppuccin"
 
 M.tokyonight = {
   -- Storm, Night, Moon or Day
