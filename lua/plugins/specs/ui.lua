71c36f56848bdbea52a9845239b6a2d9a7e8aa8b
asyncedd <isynqquwu@proton.me>
2023-06-06 07:37:32 +0900

    feat(edgy): ✨ Initial commit
    Add Neotree.nvim

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -206,4 +206,28 @@ return {
     opts = true,
     event = "VeryLazy",
   },
+  {
+    "folke/edgy.nvim",
+    opts = {
+      left = {
+        -- Neo-tree filesystem always takes half the screen height
+        {
+          title = "Neo-Tree",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          size = { height = 0.5 },
+        },
+      },
+    },
+    event = "VeryLazy",
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = true,
+    keys = {
+      { "<leader>tt", "<cmd>Neotree<CR>" },
+    },
+  },
 }
