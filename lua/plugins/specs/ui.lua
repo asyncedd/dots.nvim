7bba64bb6fcc66250bc6e9a25f62c50ba31ed27d
asyncedd <isynqquwu@proton.me>
2023-06-06 07:50:37 +0900

    feat(bufferline): ✨ Fix offsets via edgy.nvim

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -227,7 +227,36 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = true,
     keys = {
-      { "<leader>tt", "<cmd>Neotree<CR>" },
+      { "<leader>tt", "<cmd>NeoTreeShowToggle<CR>" },
     },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = function()
+      local Offset = require("bufferline.offset")
+      if not Offset.edgy then
+        local get = Offset.get
+        Offset.get = function()
+          if package.loaded.edgy then
+            local layout = require("edgy.config").layout
+            local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+            for _, pos in ipairs({ "left", "right" }) do
+              local sb = layout[pos]
+              if sb and #sb.wins > 0 then
+                local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+                ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
+                ret[pos .. "_size"] = sb.bounds.width
+              end
+            end
+            ret.total_size = ret.left_size + ret.right_size
+            if ret.total_size > 0 then
+              return ret
+            end
+          end
+          return get()
+        end
+        Offset.edgy = true
+      end
+    end,
   },
 }
