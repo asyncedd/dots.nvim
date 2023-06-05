1bdface08197a407ede7e2b56e128e96c47bffee
asyncedd <isynqquwu@proton.me>
2023-06-05 19:24:02 +0900

    feat(noice): ✨ Show more information in cmp documentations

--- lua/plugins/configs/ui/noice.lua
+++ lua/plugins/configs/ui/noice.lua
@@ -5,7 +5,7 @@ return {
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
+      ["cmp.entry.get_documentation"] = false,
     },
     signature = {
       enabled = false,
