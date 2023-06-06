ad3abf3c40a774fde3c8539cb017bf76e8c8ac5a
asyncedd <isynqquwu@proton.me>
2023-06-06 18:29:29 +0900

    perf(lsp-inlayhints): 🔥 Remove
    Remove it since, it's really annoying

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -305,26 +305,6 @@ return {
     end,
   },
   {
-    "lvimuser/lsp-inlayhints.nvim",
-    init = function()
-      vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = "LspAttach_inlayhints",
-        callback = function(args)
-          if not (args.data and args.data.client_id) then
-            return
-          end
-
-          local bufnr = args.buf
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          require("lsp-inlayhints").on_attach(client, bufnr)
-        end,
-      })
-    end,
-    opts = {},
-    branch = "anticonceal",
-  },
-  {
     "saecki/crates.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
