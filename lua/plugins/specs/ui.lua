9701cbd1fd859aadb5b87999a42c8b151b708119
asyncedd <isynqquwu@proton.me>
2023-06-06 17:10:55 +0900

    feat(edgy): ✨ Add a fancy folder icon for Neo-tree

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -212,7 +212,7 @@ return {
       left = {
         -- Neo-tree filesystem always takes half the screen height
         {
-          title = "Neo-Tree",
+          title = " Tree",
           ft = "neo-tree",
           filter = function(buf)
             return vim.b[buf].neo_tree_source == "filesystem"
