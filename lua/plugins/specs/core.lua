43b4dcda7c27c435ef7a1f1e345092cbe97eb6ab
asyncedd <isynqquwu@proton.me>
2023-06-07 21:20:41 +0900

    feat(notify): Make the notification look more better

--- lua/plugins/specs/core.lua
+++ lua/plugins/specs/core.lua
@@ -11,4 +11,26 @@ return {
   "kkharji/sqlite.lua",
   "stevearc/dressing.nvim",
   "tpope/vim-repeat",
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      local stages = require("notify.stages.fade_in_slide_out")("top_down")
+      local notify = require("notify")
+      notify.setup({
+        on_open = function(win)
+          vim.api.nvim_win_set_config(win, { focusable = false })
+        end,
+        stages = {
+          function(...)
+            local opts = stages[1](...)
+            if opts then
+              opts.border = "none"
+            end
+            return opts
+          end,
+          unpack(stages, 2),
+        },
+      })
+    end,
+  },
 }
