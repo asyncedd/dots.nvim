54809c5a03877462d63edddbfa02f4fec0ff4b6e
asyncedd <isynqquwu@proton.me>
2023-06-09 07:56:51 +0900

    cmp: 🔥 Remove codeium and tabnine
    Im sick of AI stuff and, Im sticking to it without AI.
    (unless i get copilot ofc)

--- lua/plugins/configs/coding/cmp.lua
+++ lua/plugins/configs/coding/cmp.lua
@@ -88,8 +88,6 @@ M.cmp = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
     { name = "buffer", keyword_length = 3 },
-    { name = "cmp_tabnine", keyword_length = 3 },
-    { name = "codeium", keyword_length = 3 },
     -- { name = "rg", keyword_length = 3 },
   }),
   formatting = {
--- lua/plugins/specs/coding.lua
+++ lua/plugins/specs/coding.lua
@@ -45,10 +45,8 @@ return {
       "LuaSnip",
       "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-cmdline",
-      "cmp-tabnine",
       "hrsh7th/cmp-emoji",
       "tailwindcss-colorizer-cmp.nvim",
-      "codeium.nvim",
     },
     branch = "filter_sync",
   },
@@ -323,34 +321,5 @@ return {
       require("plugins.configs.coding.autotag")(opts)
     end,
     event = "InsertEnter",
-  },
-  {
-    "jcdickinson/codeium.nvim",
-    opts = true,
-  },
-  {
-    "tzachar/cmp-tabnine",
-    opts = {
-      -- Add options here.
-      -- If you're curious about the options check: https://github.com/tzachar/cmp-tabnine
-      max_lines = 500,
-      -- For example if you want to show the prediction strength, you can:
-      show_prediction_strength = true,
-    },
-    config = function(_, opts)
-      require("cmp_tabnine.config"):setup(opts)
-
-      -- Tabnine allows prefetching results before the User asks for completions so, we'll be prefetching it for the current file.
-      require("cmp_tabnine"):prefetch(vim.fn.expand("%:p"))
-
-      -- And on BufRead (Which will not be executed on the initial load since the Autocmd is created on VeryLazy) we'll make it so, it'll prefetch too.
-      vim.api.nvim_create_autocmd("BufRead", {
-        group = vim.api.nvim_create_augroup("prefetch", { clear = true }),
-        callback = function()
-          require("cmp_tabnine"):prefetch(vim.fn.expand("%:p"))
-        end,
-      })
-    end,
-    build = (not jit.os:find("Windows") and "./install.sh" or "powershell ./install.ps1"),
   },
 }
