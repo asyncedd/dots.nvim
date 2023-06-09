a3e9f39bc902342ff3c6c4a10aaed4b95ad4b363
asyncedd <isynqquwu@proton.me>
2023-06-10 07:58:00 +0900

    refactor(notify): move it to the ui spec

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -290,4 +290,26 @@ return {
       end
     end,
   },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      local stages = require("notify.stages.fade_in_slide_out")("top_down")
+      local notify = require("notify")
+      notify.setup({
+        on_open = function(win)
+          vim.api.nvim_win_set_config(win, { focusable = false })
+        end,
+        stages = {
+          function(...)
+            local opts = stages[1](...)
+            if opts then
+              opts.border = "none"
+            end
+            return opts
+          end,
+          unpack(stages, 2),
+        },
+      })
+    end,
+  },
 }
