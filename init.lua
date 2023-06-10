08610cef5d2955f77959efaa7d8b8b55f187a66d
asyncedd <isynqquwu@proton.me>
2023-06-10 10:22:20 +0900

    docs(init): add more documentation
    docs(init): add more doucmentation
    
    Merged commits

--- init.lua
+++ init.lua
@@ -29,15 +29,45 @@ require("settings.autocmds")
 
 local colorscheme = require("settings.settings").colorscheme
 
-if colorscheme == "catppuccin" or colorscheme == "kanagawa" or colorscheme == "onedark" then
+if colorscheme == "catppuccin" or colorscheme == "onedark" then
   -- In Catppuccin, the colorscheme file is a Lua block (via vimscript) that requires the exact same thing.
   -- So, by doing this we can optimize whilst not loosing maintainability.
+  --
+  -- for example:
+  -- [
+  -- # color/some_colorscheme.vim
+  --
+  -- lua >>
+  -- require("some_colorscheme").load()
+  --
+  -- ]
+  --
+  -- For Catppuccin and Onedarkpro it's that.
+  -- Because of that small typo of "onedark", we'll just go ahead and go fix it.
   require(colorscheme == "onedark" and "onedarkpro" or colorscheme).load()
+  -- In Tokyonight it needs a "_" for some reason so, we just put that in a "elseif" block.
 elseif colorscheme == "tokyonight" then
   require("tokyonight")._load()
 else
+  -- If the colroscheme local doesn't match the ones in the if/elseif blocks,
+  -- we'll just go ahead and `pcall` "vim.cmd.colorscheme" to try and load it.
+  --
+  -- The only thing that matters is the "ok" variable below so, we can just define the other with a blank variable
   local ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
+  --            ^^^^  ^^^^^^^^^^^^^^^^^^^  ^^^^^^^^^^^
+  --            │              │               │
+  --            │              │               │
+  --      protected call       │               │
+  --                           │               │
+  --           Define what function we want to do
+  --                                           │
+  --                                       the colorscheme local.
+  --                             Defined in `lua/settings/settings.colorscheme`
+  --
+  -- If it loads, we'll just go continue on, no problems :)
   if not ok then
+    -- But if it doesn't we'll just go ahead and print that, "hey, there's no colorscheme named <colorscheme>"
+    -- And, after that we'll just load catppuccin as a fallback.
     print("Unknown colorscheme.")
     require("catppuccin").load()
   end
@@ -45,10 +75,12 @@ end
 
 -- Execute "Colorscheme" autocommands as they may be needed
 -- Some plugins may use this autocommand to synchronize the colorscheme.
+--
+-- This is normally executed when we do a "vim.cmd.colorscheme" so, how about we do that as well.
 vim.api.nvim_exec_autocmds("Colorscheme", {})
 
 -- The possibility of one pressing a keymap under a few ms is unlikely.
--- So, to improve responsiveness, we wrap it inside a function.
+-- So, to improve responsiveness, we wrap it inside a vim.schedule function.
 --
 -- vim.schedule, schedules the function inside it to be involed soon by the main event-loop.
 -- It's useful to avoid textlock or other temporary restrictions.
