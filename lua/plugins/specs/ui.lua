566a4157f2df14445cb4023cdeaa487977f245b3
asyncedd <isynqquwu@proton.me>
2023-06-13 22:46:30 +0900

    feat: Manage DAP_UI's UI part with Nvim-dap-ui

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -273,6 +273,10 @@ return {
           end,
           size = { height = 0.5 },
         },
+        { ft = "dapui_watches" },
+        { ft = "dapui_stacks" },
+        { ft = "dapui_breakpoints" },
+        { ft = "dapui_scopes" },
       },
       right = {
         {
@@ -296,6 +300,8 @@ return {
             height = 0.4,
           },
         },
+        { ft = "dapui_console" },
+        { ft = "dap-repl" },
       },
     },
     event = "VeryLazy",
