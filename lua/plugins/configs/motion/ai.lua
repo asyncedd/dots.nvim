26be599d4c6ceb9c28f1ecbe6352907fa29ebe39
asyncedd <isynqquwu@proton.me>
2023-06-16 20:55:54 +0900

    feat(mini.ai): add a textobjects for commas

--- lua/plugins/configs/motion/ai.lua
+++ lua/plugins/configs/motion/ai.lua
@@ -6,6 +6,7 @@ return {
     f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
     c = ai.gen_spec.treesitter({ a = "@comment.outer", i = "@comment.inner" }, {}),
     s = ai.gen_spec.treesitter({ a = "@scope", i = "@scope.inner" }, {}),
+    [","] = ai.gen_spec.argument(),
   },
   search_method = "cover_or_next",
 }
