687aaffc667effc05dcbe25772cc39b2f3adc10a
asyncedd <isynqquwu@proton.me>
2023-06-18 06:37:57 +0900

    perf(lsp-inlayhints): remove it
    I never used it really so, I'm going to remove it.
    
    rm

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -266,36 +266,6 @@ return {
       })
     end,
   },
-  -- NOTE: Really only needed until https://github.com/neovim/neovim/pull/23984 is merged
-  {
-    "lvimuser/lsp-inlayhints.nvim",
-    opts = {
-      inlay_hints = {
-        remove_colon_start = true,
-        remove_colon_end = true,
-      },
-      type_hints = {
-        remove_colon_start = true,
-        remove_colon_end = true,
-      },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("LspAttach_inlayhints", {}),
-        callback = function(args)
-          if not (args.data and args.data.client_id) then
-            return
-          end
-
-          local bufnr = args.buf
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          require("lsp-inlayhints").on_attach(client, bufnr)
-        end,
-      })
-    end,
-    event = "LspAttach",
-    branch = "anticonceal",
-  },
   {
     "smjonas/inc-rename.nvim",
     keys = {
