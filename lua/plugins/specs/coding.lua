5a6221060e983d537f4d45f88085c6c59b8e09e4
asyncedd <isynqquwu@proton.me>
2023-06-18 06:10:08 +0900

    perf(luasnip): remove the honza's vim-snippets snippet collection
    I really don't use vim-snippets's snippet collection so, I've removed
    it.

--- lua/plugins/specs/coding.lua
+++ lua/plugins/specs/coding.lua
@@ -16,13 +16,11 @@ return {
     end,
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
-      require("luasnip.loaders.from_snipmate").lazy_load()
       require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
       require("luasnip.loaders.from_lua").lazy_load({ paths = { "./luasnip" } })
     end,
     dependencies = {
       "rafamadriz/friendly-snippets",
-      "honza/vim-snippets",
     },
   },
   {
