2afa82ab62459fafcca7366e1d1cb89babf73a49
asyncedd <isynqquwu@proton.me>
2023-06-21 19:09:59 +0900

    fix: Disable the cursorline autocommand
    It's kinda buggy so, I've removed it.

--- lua/settings/autocmds.lua
+++ lua/settings/autocmds.lua
@@ -31,26 +31,6 @@ autocmd({ "BufRead", "BufWinEnter", "BufNewFile", "WinNew" }, {
   end,
 })
 
--- show cursor line only in active window
-vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-  callback = function()
-    local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
-    if ok and cl then
-      vim.wo.cursorline = true
-      vim.api.nvim_win_del_var(0, "auto-cursorline")
-    end
-  end,
-})
-vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
-  callback = function()
-    local cl = vim.wo.cursorline
-    if cl then
-      vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
-      vim.wo.cursorline = false
-    end
-  end,
-})
-
 -- Make Bash language stuff baked right inside zsh (since they have a similar syntax)
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "zsh",
