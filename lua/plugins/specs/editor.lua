31686d1e2d135a1f303fa6655159e5d88f4fbfab
asyncedd <isynqquwu@proton.me>
2023-06-21 17:44:56 +0900

    fix(statuscol): Start it faster

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -44,20 +44,6 @@ return {
     },
     dependencies = {
       "kevinhwang91/promise-async",
-      {
-        "luukvbaal/statuscol.nvim",
-        config = function()
-          local builtin = require("statuscol.builtin")
-          require("statuscol").setup({
-            relculright = true,
-            segments = {
-              { text = { "%s" }, click = "v:lua.ScSa" },
-              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-            },
-          })
-        end,
-      },
     },
     init = function()
       vim.api.nvim_create_autocmd({ "BufRead", "BufWinEnter", "BufNewFile", "WinEnter" }, {
--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -352,4 +352,19 @@ return {
     opts = true,
     event = "VeryLazy",
   },
+  {
+    "luukvbaal/statuscol.nvim",
+    config = function()
+      local builtin = require("statuscol.builtin")
+      require("statuscol").setup({
+        relculright = true,
+        segments = {
+          { text = { "%s" }, click = "v:lua.ScSa" },
+          { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+          { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+        },
+      })
+    end,
+    event = "User UI",
+  },
 }
