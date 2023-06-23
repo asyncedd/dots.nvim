b0f5e5d26c617fadba4f4fb1bcb90fcc394af5c0
asyncedd <isynqquwu@proton.me>
2023-06-23 16:17:46 +0900

    feat(flashy): add a flashy treesitter

--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -78,6 +78,23 @@ return {
         end,
         mode = "o",
       },
+      {
+        "<leader>t",
+        function()
+          require("flash").jump({
+            action = function(match, state)
+              vim.api.nvim_win_call(match.win, function()
+                vim.api.nvim_win_set_cursor(match.win, match.pos)
+                require("flash").treesitter()
+                vim.schedule(function()
+                  vim.api.nvim_win_set_cursor(match.win, state.pos)
+                end)
+              end)
+            end,
+          })
+        end,
+        mode = "o",
+      },
     },
   },
   {
