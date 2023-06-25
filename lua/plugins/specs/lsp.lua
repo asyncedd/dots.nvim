2ed7770fd6dde1d79bf22ca1fb3f44cd6151a5e7
asyncedd <isynqquwu@proton.me>
2023-06-25 18:54:10 +0900

    null-ls: Remove unneeded import
    In the plugin specifications for `null-ls.nvim`, I've locally assigned
    the `null-ls.builtins`.
    I don't need it, thus I've removed the import.
    
    TL;DR
    I've removed dead code.
    
    Signed-off-by: async <isynqquwu@proton.me>

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -108,7 +108,6 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function()
-      local nls = require("null-ls")
       return {
         -- A list of sources to install
         sources = {
