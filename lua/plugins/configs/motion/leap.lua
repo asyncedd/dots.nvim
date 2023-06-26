75acc071f4c667057c3708ab9e2a5d9c25046ec5
asyncedd <isynqquwu@proton.me>
2023-06-26 19:43:31 +0900

    refactor: Remove unneeded code

--- lua/plugins/configs/motion/leap.lua
+++
@@ -1,9 +0,0 @@
--- Add leap.nvim's default mappings.
---
--- Which is:
--- `s`/`S` to leap foward and backwards respectively. (for normal mode)
--- `x`/`X` to leap foward and backwards until x respectively. (for visual and operator mode)
-
-require("leap").opts.highlight_unlabeled_phase_one_targets = true
-
-require("leap").add_default_mappings()
--- lua/plugins/specs/motion.lua
+++ lua/plugins/specs/motion.lua
@@ -74,7 +74,18 @@ return {
   {
     "echasnovski/mini.ai",
     opts = function()
-      return require("plugins.configs.motion.ai")
+      local ai = require("mini.ai")
+
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+          c = ai.gen_spec.treesitter({ a = "@comment.outer", i = "@comment.inner" }, {}),
+          s = ai.gen_spec.treesitter({ a = "@scope", i = "@scope.inner" }, {}),
+          [","] = ai.gen_spec.argument(),
+        },
+        search_method = "cover_or_next",
+      }
     end,
     event = "VeryLazy",
     dependencies = {
