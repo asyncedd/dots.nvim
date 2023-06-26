cfed3a28caf10ba888ab4e950f5ddab7840c8e50
asyncedd <isynqquwu@proton.me>
2023-06-26 20:48:47 +0900

    fix(mini.comment): properly use commentstring

--- lua/plugins/configs/coding/comment.lua
+++ lua/plugins/configs/coding/comment.lua
@@ -12,6 +12,11 @@ M.Comment = {
 }
 
 M.Mini = {
+  options = {
+    custom_commentstring = function()
+      return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+    end,
+  },
   hooks = {
     pre = function()
       require("ts_context_commentstring.internal").update_commentstring()
