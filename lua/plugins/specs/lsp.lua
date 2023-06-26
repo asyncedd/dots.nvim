450d79399c23e92363876440c7f530e2be7fe214
asyncedd <isynqquwu@proton.me>
2023-06-27 05:58:33 +0900

    feat(eslint): add function to auto-fix eslint

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -91,7 +91,22 @@ return {
           nil_ls = {},
           rnix = {},
         },
-        setup = {},
+        setup = {
+          eslint = function()
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              callback = function(event)
+                local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+                if client then
+                  local diag =
+                    vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+                  if #diag > 0 then
+                    vim.cmd("EslintFixAll")
+                  end
+                end
+              end,
+            })
+          end,
+        },
       }
     end,
     init = function()
