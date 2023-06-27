62958fb10c512995bc75be283c435cecede89d9e
asyncedd <isynqquwu@proton.me>
2023-06-27 17:46:56 +0900

    feat(neorg): Enable Telescope integration

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -220,7 +220,33 @@ return {
           },
         },
         ["core.integrations.truezen"] = {},
+        ["core.integrations.telescope"] = {},
       },
+    },
+    config = function(_, opts)
+      require("neorg").setup(opts)
+
+      local neorg_callbacks = require("neorg.callbacks")
+
+      neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
+        -- Map all the below keybinds only when the "norg" mode is active
+        keybinds.map_event_to_mode("norg", {
+          n = { -- Bind keys in normal mode
+            { "<C-s>", "core.integrations.telescope.find_linkable" },
+          },
+
+          i = { -- Bind in insert mode
+            { "<C-l>", "core.integrations.telescope.insert_link" },
+          },
+        }, {
+          silent = true,
+          noremap = true,
+        })
+      end)
+    end,
+    dependencies = {
+      "plenary.nvim",
+      "nvim-neorg/neorg-telescope",
     },
     ft = "norg",
   },
