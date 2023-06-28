cf1c625b92fc2dc504efa3196a543a1f273e1ca3
asyncedd <isynqquwu@proton.me>
2023-06-28 15:43:43 +0900

    feat(lsp-lens): initial commit

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -340,4 +340,20 @@ return {
       })
     end,
   },
+  {
+    "VidocqH/lsp-lens.nvim",
+    opts = {
+      enable = true,
+      include_declaration = true, -- Reference include declaration
+      sections = { -- Enable / Disable specific request
+        definition = true,
+        references = true,
+        implementation = true,
+      },
+      ignore_filetype = {
+        "prisma",
+      },
+    },
+    event = "LspAttach",
+  },
 }
