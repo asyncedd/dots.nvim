12893155063ea1a82c24e2b8debbb71d5c8494be
asyncedd <isynqquwu@proton.me>
2023-06-30 07:53:25 +0900

    feat(windows.nvim): use mini.animate for the animations

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -423,7 +423,6 @@ return {
     "anuvyklack/windows.nvim",
     dependencies = {
       "anuvyklack/middleclass",
-      "anuvyklack/animation.nvim",
     },
     config = true,
     event = "VeryLazy",
