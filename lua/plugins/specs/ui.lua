2545fb71717c09516017ac52e8ebe5e526a7500a
asyncedd <isynqquwu@proton.me>
2023-06-30 07:20:36 +0900

    fix(tint): ignore floating windows

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -416,7 +416,17 @@ return {
   },
   {
     "levouh/tint.nvim",
-    opts = true,
+    opts = {
+      highlight_ignore_patterns = { "WinSeparator", "Status.*" }, -- Highlight group patterns to ignore, see `string.find`
+      window_ignore_function = function(winid)
+        local bufid = vim.api.nvim_win_get_buf(winid)
+        local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+        local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+
+        -- Do not tint `terminal` or floating windows, tint everything else
+        return buftype == "terminal" or floating
+      end,
+    },
     event = "VeryLazy",
   },
 }
