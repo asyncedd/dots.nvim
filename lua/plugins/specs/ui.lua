9b9ad4e033a25e14cdbdd5d2343889e67ae0c1c5
asyncedd <isynqquwu@proton.me>
2023-06-30 18:28:05 +0900

    feat(tint): darken the background a little bit more

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -420,17 +420,21 @@ return {
   },
   {
     "levouh/tint.nvim",
-    opts = {
-      highlight_ignore_patterns = { "WinSeparator", "Status.*" }, -- Highlight group patterns to ignore, see `string.find`
-      window_ignore_function = function(winid)
-        local bufid = vim.api.nvim_win_get_buf(winid)
-        local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
-        local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+    opts = function()
+      return {
+        tint = -7, -- Darken colors, use a positive value to brighten
+        tint_background_colors = true, -- Tint background portions of highlight groups
+        highlight_ignore_patterns = { "WinSeparator", "Status.*" }, -- Highlight group patterns to ignore, see `string.find`
+        window_ignore_function = function(winid)
+          local bufid = vim.api.nvim_win_get_buf(winid)
+          local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+          local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
 
-        -- Do not tint `terminal` or floating windows, tint everything else
-        return buftype == "terminal" or floating
-      end,
-    },
+          -- Do not tint `terminal` or floating windows, tint everything else
+          return buftype == "terminal" or floating
+        end,
+      }
+    end,
     event = "VeryLazy",
   },
   {
