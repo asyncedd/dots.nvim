012ece0ae51b619b4afaaef028540cbcf1bcafa2
asyncedd <isynqquwu@proton.me>
2023-06-30 19:18:38 +0900

    fix(rust-tools): properly configure nvim-dap

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -211,6 +211,30 @@ return {
   {
     "simrat39/rust-tools.nvim",
     opts = function()
+      local adapter
+      local success, package = pcall(function()
+        return require("mason-registry").get_package("codelldb")
+      end)
+      if success then
+        local package_path = package:get_install_path()
+        local codelldb_path = package_path .. "/codelldb"
+        local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+        local this_os = vim.loop.os_uname().sysname
+
+        -- The path in windows is different
+        if this_os:find("Windows") then
+          codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
+          liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+        else
+          -- The liblldb extension is .so for linux and .dylib for macOS
+          liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+        end
+        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+      else
+        adapter = require("rust-tools.dap").get_codelldb_adapter()
+      end
+
+      print(vim.inspect(adapter))
       return {
         tools = {
           inlay_hints = {
@@ -228,13 +252,19 @@ return {
         server = {
           on_attach = require("plugins.configs.lsp.config").on_attach,
         },
+        dap = {
+          adapter = adapter,
+        },
       }
     end,
     config = function(_, opts)
       require("rust-tools").setup(opts)
       vim.cmd("silent! do FileType")
     end,
-    dependencies = "nvim-lspconfig",
+    dependencies = {
+      "nvim-lspconfig",
+      "nvim-dap",
+    },
     init = function()
       vim.api.nvim_create_autocmd({ "BufRead", "BufWinEnter", "BufNewFile", "WinEnter" }, {
         callback = function()
