428de7b5a4c1d51d2998e307741ca33ac8d0f005
asyncedd <isynqquwu@proton.me>
2023-06-30 22:20:55 +0900

    feat(mini.animate): Anime scrolls
    I've decided that I want eye-candy so, I've added back some mini.animate
    scrolls

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -189,7 +189,34 @@ return {
   {
     "echasnovski/mini.animate",
     opts = function()
-      return require("plugins.configs.ui.animate")
+      -- don't use animate when scrolling with the mouse
+      local mouse_scrolled = false
+      for _, scroll in ipairs({ "Up", "Down" }) do
+        local key = "<ScrollWheel" .. scroll .. ">"
+        vim.keymap.set({ "", "i" }, key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
+
+      local animate = require("mini.animate")
+      return {
+        resize = {
+          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+        },
+        scroll = {
+          timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+          subscroll = animate.gen_subscroll.equal({
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1
+            end,
+          }),
+        },
+      }
     end,
     event = "VeryLazy",
   },
