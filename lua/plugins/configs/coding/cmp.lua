e33aa424e755e79846d201c31795ae9d1212e6fe
asyncedd <isynqquwu@proton.me>
2023-07-01 16:41:57 +0900

    feat(cmp): Improve sorting

--- lua/plugins/configs/coding/cmp.lua
+++ lua/plugins/configs/coding/cmp.lua
@@ -114,15 +114,27 @@ M.cmp = {
   sorting = {
     priority_weight = 0.8,
     comparators = {
-      compare.score,
-      compare.exact,
       compare.offset,
-      compare.sort_text,
-      compare.scopes,
+      compare.exact,
+      -- compare.scopes,
+      compare.score,
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find("^_+")
+        local _, entry2_under = entry2.completion_item.label:find("^_+")
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
       compare.recently_used,
-      compare.order,
+      compare.locality,
       compare.kind,
+      -- compare.sort_text,
       compare.length,
+      compare.order,
     },
   },
   performance = {
