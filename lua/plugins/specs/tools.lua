02b5ad18c94e96f2a486d08c040efa4fe425a157
asyncedd <isynqquwu@proton.me>
2023-07-05 21:10:02 +0900

    feat(sniprun): initial commit

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -275,4 +275,11 @@ return {
     "vimwiki/vimwiki",
     event = "VeryLazy",
   },
+  {
+    "michaelb/sniprun",
+    keys = {},
+    opts = {},
+    build = "bash ./install.sh 1",
+    cmd = "SnipRun",
+  },
 }
