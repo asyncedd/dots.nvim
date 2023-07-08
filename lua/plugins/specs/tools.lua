a1baffe9fda996b9000450a23664383e7cdfc1fb
asyncedd <isynqquwu@proton.me>
2023-07-08 15:55:34 +0900

    feat(neotree): initial commit

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -280,4 +280,45 @@ return {
       "AdvancedGitSearch",
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = true,
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+        icon = {
+          folder_empty = "󰜌",
+          folder_empty_open = "󰜌",
+        },
+        git_status = {
+          symbols = {
+            renamed = "󰁕",
+            unstaged = "󰄱",
+          },
+        },
+      },
+    },
+    cmd = {
+      "Neotree",
+    },
+    keys = {
+      { "<leader>tn", "<cmd>Neotree<CR>" },
+    },
+  },
 }
