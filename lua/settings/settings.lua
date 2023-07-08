f71d73b8c4736e30c895a699992523a001dc32c4
asyncedd <isynqquwu@proton.me>
2023-07-08 16:26:08 +0900

    docs: Add comments in the settings file

--- lua/settings/settings.lua
+++ lua/settings/settings.lua
@@ -1,8 +1,19 @@
+-- WARNING: DO NOT REMOVE THE `local M = {}` OR THERE'LL BE SPOOKY ERRORS!
+-- START
 local M = {}
+-- END
 
--- It can be catppuccin, tokyonight, nightfox, or onedarkpro
+-- The colorscheme to use. It can be: [catppuccin, tokyonight, nightfox or onedarkpro]
+--
+-- TIP: Installed some other colorscheme? Will, just add it!
+-- for eg. You can use the vim.cmd.colorscheme (:colorscheme) <name>
+-- and just put the name below and it'll be used!
 M.colorscheme = "catppuccin"
 
+-- Language integration to use.
+-- The filetypes below are the only supported ones for now.
+-- For example to disable Lua integration change the `lua = true,` to `lua = false,`
+-- FUN FACT: The language servers will still start so, make sure to go `:Mason` and uninstall them!
 M.filetypes = {
   lua = true,
   typescript = true,
@@ -17,9 +28,15 @@ M.filetypes = {
   astro = true,
 }
 
+-- A list of plugins to disable.
+-- You simply add plugins to disable, that's it!
+-- (You might break things so beware)
 M.disabled_plugins = {
   "nvim-neo-tree/neo-tree.nvim",
 }
+
+-- WARNING: DO NOT MODIFY STUFF UNDER THIS!
+-- It'll lead to unexpected behavior!
 
 dots = M
 
