171912cf12ac1e6a7bd8907f531a5872c51983b3
asyncedd <isynqquwu@proton.me>
2023-07-09 21:36:15 +0900

    perf(lsp-lens): removal

--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -178,20 +178,4 @@ return {
       { "K", "<cmd>Lspsaga hover_doc ++keep<CR>" },
     },
   },
-  {
-    "VidocqH/lsp-lens.nvim",
-    opts = {
-      enable = true,
-      include_declaration = true, -- Reference include declaration
-      sections = { -- Enable / Disable specific request
-        definition = true,
-        references = true,
-        implementation = true,
-      },
-      ignore_filetype = {
-        "prisma",
-      },
-    },
-    event = "LspAttach",
-  },
 }
