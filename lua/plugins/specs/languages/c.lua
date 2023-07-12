bc85232858da066d9286abec49a0de732ab8f409
asyncedd <isynqquwu@proton.me>
2023-07-12 15:34:42 +0900

    refactor: Refactor C++ setup

--- lua/plugins/specs/languages/c.lua
+++ lua/plugins/specs/languages/c.lua
@@ -1,67 +1,28 @@
 return dots.languages.c ~= true and {}
   or {
     {
-      "p00f/clangd_extensions.nvim",
+      "neovim/nvim-lspconfig",
       opts = {
-        server = {
-          -- Ensure mason installs the server
-          clangd = {
-            keys = {
-              { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-            },
-            root_dir = function(...)
-              -- using a root .clang-format or .clang-tidy file messes up projects, so remove them
-              return require("lspconfig.util").root_pattern(
-                "compile_commands.json",
-                "compile_flags.txt",
-                "configure.ac",
-                ".git"
-              )(...)
-            end,
-            capabilities = {
-              offsetEncoding = { "utf-16" },
-            },
-            cmd = {
-              "clangd",
-              "--background-index",
-              "--clang-tidy",
-              "--header-insertion=iwyu",
-              "--completion-style=detailed",
-              "--function-arg-placeholders",
-              "--fallback-style=llvm",
-            },
-            init_options = {
-              usePlaceholders = true,
-              completeUnimported = true,
-              clangdFileStatus = true,
-            },
-          },
+        clangd = {
+          on_attach = function(client, bufnr)
+            client.server_capabilities.signatureHelpProvider = false
+          end,
         },
-        extensions = {
-          inlay_hints = {
-            inline = false,
-          },
-          ast = {
-            --These require codicons (https://github.com/microsoft/vscode-codicons)
-            role_icons = {
-              type = "",
-              declaration = "",
-              expression = "",
-              specifier = "",
-              statement = "",
-              ["template argument"] = "",
-            },
-            kind_icons = {
-              Compound = "",
-              Recovery = "",
-              TranslationUnit = "",
-              PackExpansion = "",
-              TemplateTypeParm = "",
-              TemplateTemplateParm = "",
-              TemplateParamObject = "",
-            },
-          },
+      },
+    },
+    {
+      "jose-elias-alvarez/null-ls.nvim",
+      opts = {
+        sources = {
+          clang_format = {},
         },
+      },
+    },
+    {
+      "jay-babu/mason-nvim-dap.nvim",
+      dependencies = {
+        "mason.nvim",
+        "nvim-dap",
       },
     },
   }
