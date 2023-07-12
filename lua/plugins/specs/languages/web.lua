689aedf69c56a7c4f9365c681e4c34d537946ef5
asyncedd <isynqquwu@proton.me>
2023-07-13 06:48:37 +0900

    perf: Remove deliberate.nvim

--- lua/plugins/specs/languages/web.lua
+++ lua/plugins/specs/languages/web.lua
@@ -20,30 +20,4 @@ return dots.languages.web ~= true and {}
         "thenbe/neotest-playwright",
       },
     },
-    {
-      "ziontee113/deliberate.nvim",
-      dependencies = {
-        "anuvyklack/hydra.nvim",
-      },
-      config = function()
-        local supported_filetypes = { "typescriptreact", "svelte" }
-        local augroup = vim.api.nvim_create_augroup("DeliberateEntryPoint", { clear = true })
-        vim.api.nvim_create_autocmd({ "FileType" }, {
-          pattern = supported_filetypes,
-          group = augroup,
-          callback = function()
-            local bufnr = vim.api.nvim_get_current_buf()
-            if vim.tbl_contains(supported_filetypes, vim.bo.ft) then
-              vim.keymap.set("n", "<Esc>", function()
-                vim.api.nvim_input("<Plug>DeliberateHydraEsc")
-              end, { buffer = bufnr })
-              vim.keymap.set("i", "<Plug>DeliberateHydraEsc", "<Nop>", {})
-            end
-          end,
-        })
-
-        require("deliberate.hydra")
-      end,
-      event = "VeryLazy",
-    },
   }
