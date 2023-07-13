edc9090a61a2d36f174679a60a7e654952181b94
asyncedd <isynqquwu@proton.me>
2023-07-13 16:33:46 +0900

    feat(clangd_extensions.nvim): initial commit

--- lua/plugins/specs/languages/c.lua
+++ lua/plugins/specs/languages/c.lua
@@ -3,14 +3,32 @@ return dots.languages.c ~= true and {}
     {
       "neovim/nvim-lspconfig",
       opts = {
-        clangd = {
-          on_attach = function(client, bufnr)
-            client.server_capabilities.signatureHelpProvider = false
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
+        servers = {
+          clangd = {
+            on_attach = function(client, bufnr)
+              client.server_capabilities.signatureHelpProvider = false
+              client.server_capabilities.documentFormattingProvider = false
+              client.server_capabilities.documentRangeFormattingProvider = false
+            end,
+          },
+        },
+        setup = {
+          clangd = function(_, opts)
+            local clangd_ext_opts = require("core.utils.lazyvim").opts("clangd_extensions.nvim")
+            require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+            return true
           end,
         },
       },
+    },
+    {
+      "p00f/clangd_extensions.nvim",
+      opts = {
+        extensions = {
+          inline_hints = false,
+        },
+      },
+      setup = function() end,
     },
     {
       "jose-elias-alvarez/null-ls.nvim",
