d427cab5f7648bc70fab32c11316be99533fecb6
asyncedd <isynqquwu@proton.me>
2023-07-17 07:05:08 +0900

    refactor: Directly use Neovim's builtin features

--- init.lua
+++ init.lua
@@ -36,63 +36,31 @@ require("settings.autocmds")
 
 -- Catppuccinify the editor.
 -- vim.cmd("colorscheme catppuccin")
--- PERF: directly call the colorscheme. (not via `vim.cmd("colorscheme x")`)
--- lua blocks are expensive as I heard.
--- this actually seems like an unnesseary premature optimization though
-
 local colorscheme = dots.colorscheme.current
 
--- Define a table of colorscheme loaders.
-local colorscheme_loaders = {
-  "catppuccin",
-  "onedarkpro",
-  "nightfox",
-  "tokyonight",
-}
-
-if colorscheme_loaders[colorscheme] then
-  -- In Catppuccin, the colorscheme file is a Lua block (via vimscript) that requires the exact same thing.
-  -- So, by doing this we can optimize whilst not loosing maintainability.
-  --
-  -- for example:
-  -- [
-  -- # color/some_colorscheme.vim
-  --
-  -- lua >>
-  -- require("some_colorscheme").load()
-  --
-  -- ]
-  --
-  -- For Catppuccin and Onedarkpro it's that.
-  require(colorscheme).load()
-  -- In Tokyonight it needs a "_" for some reason so, we just put that in a "elseif" block.
-elseif colorscheme == "tokyonight" then
-  require("tokyonight")._load()
-else
-  -- If the colroscheme local doesn't match the ones in the if/elseif blocks,
-  -- we'll just go ahead and `pcall` "vim.cmd.colorscheme" to try and load it.
-  --
-  -- We'll go and use the variable "ok".
-  -- It'll be false if the colorscheme doesn't exist.
-  -- It'll be true if it does.
-  local ok = pcall(vim.cmd.colorscheme, colorscheme)
-  --         ^^^^^ ^^^^^^^^^^^^^^^^^^^  ^^^^^^^^^^^
-  --           │             │               │
-  --           │             │               │
-  --      protected call     │               │
-  --                         │               │
-  --        Define what function we want to do
-  --                                         │
-  --                                the colorscheme local.
-  --                        Defined in `lua/settings/settings.colorscheme`
-  --
-  -- If it loads, we'll just go continue on, no problems :)
-  if not ok then
-    -- But if it doesn't we'll just go ahead and print that, "hey, there's no colorscheme named <colorscheme>"
-    -- And, after that we'll just load catppuccin as a fallback.
-    vim.notify("Hey, there's no colorscheme named " .. colorscheme, vim.log.levels.ERROR, {})
-    require("catppuccin").load()
-  end
+-- If the colroscheme local doesn't match the ones in the if/elseif blocks,
+-- we'll just go ahead and `pcall` "vim.cmd.colorscheme" to try and load it.
+--
+-- We'll go and use the variable "ok".
+-- It'll be false if the colorscheme doesn't exist.
+-- It'll be true if it does.
+local ok = pcall(vim.cmd.colorscheme, colorscheme)
+--         ^^^^^ ^^^^^^^^^^^^^^^^^^^  ^^^^^^^^^^^
+--           │             │               │
+--           │             │               │
+--      protected call     │               │
+--                         │               │
+--        Define what function we want to do
+--                                         │
+--                                the colorscheme local.
+--                        Defined in `lua/settings/settings.colorscheme`
+--
+-- If it loads, we'll just go continue on, no problems :)
+if not ok then
+  -- But if it doesn't we'll just go ahead and print that, "hey, there's no colorscheme named <colorscheme>"
+  -- And, after that we'll just load catppuccin as a fallback.
+  vim.notify("Hey, there's no colorscheme named " .. colorscheme, vim.log.levels.ERROR, {})
+  require("catppuccin").load()
 end
 
 -- Execute "Colorscheme" autocommands as they may be needed
