dd2ee57abaae0eee439a5a35e8e54f6d5290dd0e
asyncedd <isynqquwu@proton.me>
2023-07-17 19:51:57 +0900

    feat(lua_ls): initial commit

--- lua/plugins/specs/languages/lua.lua
+++ lua/plugins/specs/languages/lua.lua
@@ -5,4 +5,12 @@ return not (dots.languages.lua or dots.languages.lua.enable) and {} or {
       table.insert(opts.ensure_installed, { dots.languages.lua.treesitter.base and "lua", dots.languages.lua.treesitter.luadoc and "luadoc", dots.languages.lua.treesitter.patterns and "luap" })
     end,
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {}
+      },
+    }
+  },
 }
