481d1710c7511aeee3911c16aed8d6f75b7f9c61
asyncedd <isynqquwu@proton.me>
2023-07-17 10:13:49 +0900

    refactor: Refactor init.lua

--- init.lua
+++ init.lua
@@ -1,79 +1,29 @@
---[[
--- File: init.lua
--- Purpose: Initialize the Neovim configuration?
--- Date: 04/26/2023
--- Last modified: 06/21/2023
--- License: Unlicense
---
--- The code provided below is licenced on the Unlicense licence.
---
---]]
 -- Enable the experimental Lua loader.
---
--- What it does is:
--- - Overrides loadfile
--- - Adds the Lua loader using the byte-compilation cache
--- - Adds the libs loader
--- - Removes the default Neovim loader
---
--- The default Neovim loader is:
--- https://neovim.io/doc/user/starting#initialization
---
--- This only starts from step 7 in the initialization process.
 vim.loader.enable()
 
--- Check it at lua/settings/options.lua as I've documented heavily
 -- Set some options.
 require("settings.options")
-
-vim.ui.inputsecret = vim.ui.inputsecret or vim.ui.input
 
 -- Require lazy.nvim.
 require("plugins")
 
--- Set up autocmds
+-- Set up autocmds.
 require("settings.autocmds")
 
--- Catppuccinify the editor.
--- vim.cmd("colorscheme catppuccin")
+-- Set the colorscheme.
 local colorscheme = dots.colorscheme.current
 
--- If the colroscheme local doesn't match the ones in the if/elseif blocks,
--- we'll just go ahead and `pcall` "vim.cmd.colorscheme" to try and load it.
---
--- We'll go and use the variable "ok".
--- It'll be false if the colorscheme doesn't exist.
--- It'll be true if it does.
 local ok = pcall(vim.cmd.colorscheme, colorscheme)
---         ^^^^^ ^^^^^^^^^^^^^^^^^^^  ^^^^^^^^^^^
---           │             │               │
---           │             │               │
---      protected call     │               │
---                         │               │
---        Define what function we want to do
---                                         │
---                                the colorscheme local.
---                        Defined in `lua/settings/settings.colorscheme`
---
--- If it loads, we'll just go continue on, no problems :)
+
 if not ok then
-  -- But if it doesn't we'll just go ahead and print that, "hey, there's no colorscheme named <colorscheme>"
-  -- And, after that we'll just load catppuccin as a fallback.
   vim.notify("Hey, there's no colorscheme named " .. colorscheme, vim.log.levels.ERROR, {})
   require("catppuccin").load()
 end
 
--- Execute "Colorscheme" autocommands as they may be needed
--- Some plugins may use this autocommand to synchronize the colorscheme.
---
--- This is normally executed when we do a "vim.cmd.colorscheme" so, how about we do that as well.
+-- Execute "Colorscheme" autocommands.
 vim.api.nvim_exec_autocmds("Colorscheme", {})
 
--- The possibility of one pressing a keymap under a few ms is unlikely.
--- So, to improve responsiveness, we wrap it inside a vim.schedule function.
---
--- vim.schedule, schedules the function inside it to be involed soon by the main event-loop.
--- It's useful to avoid textlock or other temporary restrictions.
+-- Schedule the keymaps to improve responsiveness.
 vim.schedule(function()
   require("keymaps")
 end)
