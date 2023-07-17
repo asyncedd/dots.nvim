d80fce7f1c54d3fe6b39b181f3b6a2470e047de6
asyncedd <isynqquwu@proton.me>
2023-07-17 20:16:01 +0900

    feat(lua_ls): Upstream the original configuration

--- lua/plugins/specs/languages/lua.lua
+++ lua/plugins/specs/languages/lua.lua
@@ -9,14 +9,44 @@ return not (dots.languages.lua or dots.languages.lua.enable) and {} or {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        lua_ls = {}
+        lua_ls = {
+          settings = {
+            Lua = {
+              hint = {
+                enable = true,
+                arrayIndex = "Disable",
+              },
+              runtime = {
+                pathStrict = true,
+              },
+              completion = {
+                callSnippet = "Both",
+              },
+              diagnostics = {
+                globals = {
+                  "vim",
+                },
+              },
+              workspace = {
+                library = {
+                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                  -- [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+                  [vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+                checkThirdParty = false,
+              },
+            },
+          },
+        },
       },
     },
-    dependencies = dots.languages.lua.LSP.neodev.enable and {
-      {
-        "folke/neodev.nvim",
-        opts = dots.languages.lua.LSP.neodev.opts or {},
-      },
+  },
+  dependencies = dots.languages.lua.LSP.neodev.enable and {
+    {
+      "folke/neodev.nvim",
+      opts = dots.languages.lua.LSP.neodev.opts or {},
     },
   },
 }
