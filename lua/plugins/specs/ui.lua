da7633d02d3e0a414dc8a2671b71cddd1b40a740
asyncedd <isynqquwu@proton.me>
2023-07-18 17:33:24 +0900

    feat(heirline): Add an LSP section

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -300,6 +300,81 @@ return {
         },
       }
 
+      M.Diagnostics = {
+
+        condition = conditions.has_diagnostics,
+
+        static = {
+          error_icon = dots.UI.icons.LSP.Error,
+          warn_icon = dots.UI.icons.LSP.Warn,
+          info_icon = dots.UI.icons.LSP.Info,
+          hint_icon = dots.UI.icons.LSP.Hint,
+        },
+
+        init = function(self)
+          self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+          self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+          self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+          self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+        end,
+
+        update = { "DiagnosticChanged", "BufEnter" },
+
+        {
+          provider = function(self)
+            -- 0 is just another output, we can decide to print it or not!
+            return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+          end,
+          hl = { fg = "diag_error" },
+        },
+        {
+          provider = function(self)
+            return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+          end,
+          hl = { fg = "diag_warn" },
+        },
+        {
+          provider = function(self)
+            return self.info > 0 and (self.info_icon .. self.info .. " ")
+          end,
+          hl = { fg = "diag_info" },
+        },
+        {
+          provider = function(self)
+            return self.hints > 0 and (self.hint_icon .. self.hints)
+          end,
+          hl = { fg = "diag_hint" },
+        },
+      }
+
+      M.LSPActive = {
+        condition = conditions.lsp_attached,
+        update = { "LspAttach", "LspDetach" },
+
+        {
+          provider = "",
+          hl = { fg = "green" },
+        },
+        {
+          provider = " ",
+          hl = { bg = "green", fg = "Normal" },
+        },
+        {
+          provider = function()
+            local names = {}
+            for i, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+              table.insert(names, server.name)
+            end
+            return " " .. table.concat(names, " ")
+          end,
+          hl = { fg = "green", bg = "bright_bg", bold = true },
+        },
+        {
+          provider = "",
+          hl = { fg = "bright_bg" },
+        },
+      }
+
       M.StatusLine = {
         M.ViMode,
         Space,
@@ -307,6 +382,9 @@ return {
         Space,
         M.Git,
         Align,
+        M.Diagnostics,
+        Space,
+        M.LSPActive,
       }
 
       M.StatusLines = {
