1c6302a6c1cb0a577756c749cfd75be4da27f740
asyncedd <isynqquwu@proton.me>
2023-07-19 05:53:33 +0900

    perf(gitsigns): Fix slowdown of the full editor

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -78,23 +78,8 @@ return not dots.editor.enabled and {}
     },
     {
       "lewis6991/gitsigns.nvim",
-      ft = { "gitcommit", "diff" },
       enabled = dots.editor.gitsigns.enabled,
-      init = function()
-        -- load gitsigns only when a git file is opened
-        vim.api.nvim_create_autocmd({ "BufRead" }, {
-          group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
-          callback = function()
-            vim.fn.system("git -C " .. '"' .. vim.fn.expand("%:p:h") .. '"' .. " rev-parse")
-            if vim.v.shell_error == 0 then
-              vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
-              vim.schedule(function()
-                require("lazy").load({ plugins = { "gitsigns.nvim" } })
-              end)
-            end
-          end,
-        })
-      end,
+      event = "VeryLazy",
       opts = {
         signs = {
           add = { text = dots.UI.icons.Git.Signs.add },
