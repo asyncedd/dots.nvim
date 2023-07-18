4eeedc4c820edd92764b1788dd0555d44349bb7c
asyncedd <isynqquwu@proton.me>
2023-07-18 15:32:07 +0900

    feat(gitsigns): initial commit

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -89,4 +89,33 @@ return not dots.editor.enabled and {}
       opts = true,
       evnet = "VeryLazy",
     },
+    {
+      "lewis6991/gitsigns.nvim",
+      ft = { "gitcommit", "diff" },
+      init = function()
+        -- load gitsigns only when a git file is opened
+        vim.api.nvim_create_autocmd({ "BufRead" }, {
+          group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+          callback = function()
+            vim.fn.system("git -C " .. '"' .. vim.fn.expand("%:p:h") .. '"' .. " rev-parse")
+            if vim.v.shell_error == 0 then
+              vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
+              vim.schedule(function()
+                require("lazy").load({ plugins = { "gitsigns.nvim" } })
+              end)
+            end
+          end,
+        })
+      end,
+      opts = {
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+          delete = { text = "󰍵" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "│" },
+        },
+      },
+    },
   }
