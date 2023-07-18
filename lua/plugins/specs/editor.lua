794a03f99e0f116b6f94ce472f92ec6d97528d76
asyncedd <isynqquwu@proton.me>
2023-07-18 17:16:01 +0900

    feat(gitsigns): Use LazyVim icons

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -109,12 +109,12 @@ return not dots.editor.enabled and {}
       end,
       opts = {
         signs = {
-          add = { text = "│" },
-          change = { text = "│" },
-          delete = { text = "󰍵" },
-          topdelete = { text = "‾" },
-          changedelete = { text = "~" },
-          untracked = { text = "│" },
+          add = { text = "▎" },
+          change = { text = "▎" },
+          delete = { text = "" },
+          topdelete = { text = "" },
+          changedelete = { text = "▎" },
+          untracked = { text = "▎" },
         },
       },
     },
