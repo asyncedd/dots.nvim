7b1e242480766904b9488e420736fe9254dd99ca
asyncedd <isynqquwu@proton.me>
2023-07-18 18:40:43 +0900

    fix(yanky): Fix Telescope errors

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -157,23 +157,13 @@ return not dots.editor.enabled and {}
       "gbprod/yanky.nvim",
       dependencies = { { "kkharji/sqlite.lua", enabled = not jit.os:find("Windows") } },
       opts = function()
-        local mapping = require("yanky.telescope.mapping")
-        local mappings = mapping.get_defaults()
-        mappings.i["<c-p>"] = nil
         return {
           highlight = { timer = 200 },
           ring = { storage = jit.os:find("Windows") and "shada" or "sqlite" },
-          picker = {
-            telescope = {
-              use_default_mappings = false,
-              mappings = mappings,
-            },
-          },
         }
       end,
       keys = {
         -- stylua: ignore
-        { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
         { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
         { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
         { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
