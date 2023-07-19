00b9b21ae9f9972f784e8933b3b9564be72df1da
asyncedd <isynqquwu@proton.me>
2023-07-19 19:05:34 +0900

    fix(heirline): Use non-deprecated methods

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -455,7 +455,7 @@ return {
 
       local LSPActive = {
         condition = function()
-          return next(vim.lsp.get_active_clients()) ~= nil
+          return next(vim.lsp.get_clients()) ~= nil
         end,
         update = { "LspAttach", "LspDetach" },
 
@@ -470,7 +470,7 @@ return {
         {
           provider = function()
             local names = {}
-            for _, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+            for _, server in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
               table.insert(names, server.name)
             end
             return " " .. table.concat(names, " ")
