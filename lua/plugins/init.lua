88191ff691cc9b61345402f94c035c5745215c3a
asyncedd <isynqquwu@proton.me>
2023-07-21 13:34:06 +0900

    perf(lazy): Use a custom crawler for the plugin specs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local modules_dir = vim.fn.stdpath("config") .. "/lua/plugins"
 
 if not vim.uv.fs_stat(lazypath) then
   print(" Bootstrapping lazy.nvim!")
@@ -8,9 +9,35 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+local get_plugins = function()
+  local ls = {}
+
+  local get_plugins_list = function()
+    local list = {}
+    local plugins_list = vim.split(
+      vim.fn.glob(modules_dir .. "/specs/*.lua") .. "\n" .. vim.fn.glob(modules_dir .. "/specs/**/*.lua"),
+      "\n"
+    )
+
+    for _, f in ipairs(plugins_list) do
+      list[#list + 1] = f:sub(#modules_dir - 6, -1)
+    end
+    return list
+  end
+
+  local plugins_file = get_plugins_list()
+  for _, m in ipairs(plugins_file) do
+    local modules = require(m:sub(0, #m - 4))
+    for i, v in pairs(modules) do
+      ls[#ls + 1] = v
+    end
+  end
+
+  return ls
+end
+
 require("lazy").setup({
-  { import = "plugins.specs" },
-  { import = "plugins.specs.languages" },
+  get_plugins(),
 }, {
   defaults = {
     lazy = dots.lazy.defaults.lazy,
