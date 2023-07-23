5aca1b7a2aa01a4a87d7be4cc92bc8da920eb896
asyncedd <isynqquwu@proton.me>
2023-07-23 16:53:14 +0900

    refactor(lazy): Just simplify it

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,4 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local modules_dir = vim.fn.stdpath("config") .. "/lua/plugins"
 
 if not vim.uv.fs_stat(lazypath) then
   vim.notify(" Bootstrapping lazy.nvim!")
@@ -9,35 +8,9 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-local get_plugins = function()
-  local ls = {}
-
-  local get_plugins_specs = function()
-    local list = {}
-    local plugins_specs = vim.split(
-      vim.fn.glob(modules_dir .. "/specs/*.lua") .. "\n" .. vim.fn.glob(modules_dir .. "/specs/**/*.lua"),
-      "\n"
-    )
-
-    for _, f in ipairs(plugins_specs) do
-      list[#list + 1] = f:sub(#modules_dir - 6, -1)
-    end
-    return list
-  end
-
-  local plugins_file = get_plugins_specs()
-  for _, m in ipairs(plugins_file) do
-    local modules = require(m:sub(0, #m - 4))
-    for _, v in pairs(modules) do
-      ls[#ls + 1] = v
-    end
-  end
-
-  return ls
-end
-
 require("lazy").setup({
-  get_plugins(),
+  { import = "plugins.specs" },
+  { import = "plugins.specs.languages" },
 }, {
   defaults = {
     lazy = dots.lazy.defaults.lazy,
