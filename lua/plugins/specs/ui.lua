8fbc8ddbf6de3170ce7eac6deb48a119add4816d
asyncedd <isynqquwu@proton.me>
2023-07-26 08:30:30 +0900

    feat(mini.animate): initial commit

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -254,4 +254,51 @@ return {
     },
     event = "VeryLazy",
   },
+  {
+    "echasnovski/mini.animate",
+    opts = function()
+      -- don't use animate when scrolling with the mouse
+      local mouse_scrolled = false
+      for _, scroll in ipairs({ "Up", "Down" }) do
+        local key = "<ScrollWheel" .. scroll .. ">"
+        vim.keymap.set({ "", "i" }, key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
+
+      local animate = require("mini.animate")
+      return {
+        resize = {
+          enable = false,
+        },
+        scroll = {
+          timing = animate.gen_timing.exponential({ duration = 50, unit = "total" }),
+          subscroll = animate.gen_subscroll.equal({
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1 and total_scroll < 50
+            end,
+          }),
+        },
+        cursor = {
+          subscroll = animate.gen_path.spiral(),
+        },
+        open = {
+          winconfig = animate.gen_winconfig.wipe({
+            direction = "from_edge",
+          }),
+        },
+        close = {
+          winconfig = animate.gen_winconfig.wipe({
+            direction = "to_edge",
+          }),
+        },
+      }
+    end,
+    event = "VeryLazy",
+  },
 }
