e17384ba8e335a28171e2f0574c95c7cda46f696
asyncedd <isynqquwu@proton.me>
2023-07-26 11:23:50 +0900

    feat(gitsigns): Add everything else

--- lua/plugins/configs/ui/heirline.lua
+++ lua/plugins/configs/ui/heirline.lua
@@ -543,9 +543,76 @@ local git_signs = {
   },
 }
 
+local signs = {
+  -- condition = function() return conditions.has_diagnostics() end,
+  init = function(self)
+    local signs = vim.fn.sign_getplaced(vim.api.nvim_get_current_buf(), {
+      group = "*",
+      lnum = vim.v.lnum,
+    })
+
+    if #signs == 0 or signs[1].signs == nil then
+      self.sign = nil
+      self.has_sign = false
+      return
+    end
+
+    -- Filter out git signs
+    signs = vim.tbl_filter(function(sign)
+      return not vim.startswith(sign.group, "gitsigns")
+    end, signs[1].signs)
+
+    if #signs == 0 then
+      self.sign = nil
+    else
+      self.sign = signs[1]
+    end
+
+    self.has_sign = self.sign ~= nil
+  end,
+  provider = function(self)
+    if self.has_sign then
+      return vim.fn.sign_getdefined(self.sign.name)[1].text
+    end
+    return "  "
+  end,
+  hl = function(self)
+    if self.has_sign then
+      -- Neotest signs
+      if self.sign.group == "neotest-status" then
+        if self.sign.name == "neotest_running" then
+          return "NeotestRunning"
+        end
+        if self.sign.name == "neotest_failed" then
+          return "NeotestFailed"
+        end
+        if self.sign.name == "neotest_passed" then
+          return "NeotestPassed"
+        end
+        return "NeotestSkipped"
+      end
+
+      -- Everything else
+      local hl = self.sign.name
+      return (vim.fn.hlexists(hl) ~= 0 and hl)
+    end
+  end,
+  on_click = {
+    name = "sign_click",
+    callback = function(self, ...)
+      if self.handlers.signs then
+        self.handlers.signs(self.click_args(self, ...))
+      end
+    end,
+  },
+}
+
 local Statuscolumns = {
   folds,
   Align,
+  Space,
+  signs,
+  Space,
   line_numbers,
   Space,
   git_signs,
