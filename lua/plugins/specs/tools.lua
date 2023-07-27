319aaee2f6d20777151f1cd5de4e00a02e89c055
asyncedd <isynqquwu@proton.me>
2023-07-27 13:19:43 +0900

    feat(telescope): use the nvchad settings

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -7,6 +7,23 @@ return not dots.tools.enabled and {}
           prompt_prefix = "   ",
           selection_caret = "  ",
           entry_prefix = "  ",
+          initial_mode = "insert",
+          selection_strategy = "reset",
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
         },
       },
       dependencies = "nvim-lua/plenary.nvim",
