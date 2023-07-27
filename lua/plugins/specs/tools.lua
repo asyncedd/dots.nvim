c62fd8c0359662a8b2b7379ed6c6e44330ece31e
asyncedd <isynqquwu@proton.me>
2023-07-27 13:18:57 +0900

    feat(telescope): Use some pretty icons for text box

--- lua/plugins/specs/tools.lua
+++ lua/plugins/specs/tools.lua
@@ -2,7 +2,13 @@ return not dots.tools.enabled and {}
   or {
     {
       "nvim-telescope/telescope.nvim",
-      opts = true,
+      opts = {
+        defaults = {
+          prompt_prefix = "   ",
+          selection_caret = "  ",
+          entry_prefix = "  ",
+        },
+      },
       dependencies = "nvim-lua/plenary.nvim",
       keys = {
         { "<leader>ff", "<cmd>Telescope find_files<CR>" },
