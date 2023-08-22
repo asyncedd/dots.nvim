e9a7f5ec2848195be2c93e898af753c0c308468c
asyncedd <isynqquwu@proton.me>
2023-08-22 10:06:55 +0900

    perf: Remove mini.animate
    I've figured I didn't need it.

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -247,40 +247,4 @@ return {
     },
     event = "VeryLazy",
   },
-  {
-    "echasnovski/mini.animate",
-    opts = function()
-      -- don't use animate when scrolling with the mouse
-      local mouse_scrolled = false
-      for _, scroll in ipairs({ "Up", "Down" }) do
-        local key = "<ScrollWheel" .. scroll .. ">"
-        vim.keymap.set({ "", "i" }, key, function()
-          mouse_scrolled = true
-          return key
-        end, { expr = true })
-      end
-
-      local animate = require("mini.animate")
-      return {
-        scroll = {
-          timing = animate.gen_timing.exponential({ duration = 50, unit = "total" }),
-          subscroll = animate.gen_subscroll.equal({
-            predicate = function(total_scroll)
-              if mouse_scrolled then
-                mouse_scrolled = false
-                return false
-              end
-              return total_scroll > 1 and total_scroll < 50
-            end,
-          }),
-        },
-        cursor = {
-          timing = function(_, n)
-            return 150 / n
-          end,
-        },
-      }
-    end,
-    event = "VeryLazy",
-  },
 }
