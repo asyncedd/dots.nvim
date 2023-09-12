e74da9f59668e1399eaadd4c068eccf0339f6d2f
asyncedd <isynqquwu@proton.me>
2023-09-12 18:23:46 +0900

    feat(ultimate-auotpairs): initial commit

---
+++ lua/plugins/specs/coding.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "altermo/ultimate-autopair.nvim",
+    opts = {
+      bs = {
+        indent_ignore = true,
+      },
+    },
+    event = {
+      "InsertEnter",
+      "CmdlineEnter"
+    },
+  },
+}
