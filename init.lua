07cf6452662a4aa8ce53dd219245fc10b99fc425
asyncedd <isynqquwu@proton.me>
2023-09-13 16:49:24 +0900

    feat: Add LSP icons

--- init.lua
+++ init.lua
@@ -5,3 +5,13 @@ require("plugins")
 if not vim.g.vscode then
   vim.cmd("colorscheme onedark")
 end
+
+for name, icon in pairs({
+  Error = " ",
+  Warn = " ",
+  Hint = "󰌵 ",
+  Info = " ",
+}) do
+  name = "DiagnosticSign" .. name
+  vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+end
