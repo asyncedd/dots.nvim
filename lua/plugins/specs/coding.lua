0ec61a1303fdc75044fd80626709039b64734f21
asyncedd <isynqquwu@proton.me>
2023-09-13 16:40:13 +0900

    feat(cmp): initial commit

--- lua/plugins/specs/coding.lua
+++ lua/plugins/specs/coding.lua
@@ -1,3 +1,33 @@
+local icons = {
+  Text = "󰉿",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "󰜢",
+  Variable = "󰀫",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "󰑭",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "󰈇",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "󰙅",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = "",
+  Codeium = "",
+  TabNine = "",
+}
+
 return {
   {
     "altermo/ultimate-autopair.nvim",
@@ -8,20 +38,87 @@ return {
     },
     event = {
       "InsertEnter",
-      "CmdlineEnter"
+      "CmdlineEnter",
     },
   },
   {
     "windwp/nvim-ts-autotag",
     opts = {
       filetypes = {
-	"html",
-	"xml",
-	"svelte"
+        "html",
+        "xml",
+        "svelte",
       },
     },
     event = {
       "InsertEnter",
     },
+  },
+  {
+    "L3MON4D3/LuaSnip",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      opts.snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      }
+      opts.sources = {
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "buffer" },
+      }
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- they way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      })
+      opts.formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          local item = vim_item.kind
+          vim_item.kind = icons[item]
+          vim_item.menu = "(" .. item .. ")"
+
+          return vim_item
+        end,
+      }
+      opts.experimental = {
+        ghost_text = true,
+      }
+    end,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    event = "InsertEnter",
   },
 }
