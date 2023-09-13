8644dfbe4a11ad99b987e604e87f2bd3fe8e937b
asyncedd <isynqquwu@proton.me>
2023-09-13 12:56:19 +0900

    refactor: Move the `motion` into `editor`

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -5,12 +5,55 @@ return {
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
       auto_install = true,
       highlight = {
-	enable = true,
+        enable = true,
       },
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
     end,
     event = { "BufReadPost", "BufNewFile" },
+  },
+  {
+    "folke/flash.nvim",
+    opts = {},
+    keys = {
+      {
+        "s",
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash tree-sitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+      { "f", mode = { "n", "x", "o" } },
+      { "F", mode = { "n", "x", "o" } },
+      { "t", mode = { "n", "x", "o" } },
+      { "T", mode = { "n", "x", "o" } },
+    },
+    event = {
+      "CmdlineEnter",
+    },
   },
 }
