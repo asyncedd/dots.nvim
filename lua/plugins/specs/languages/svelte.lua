734b6edee576339cb36f591e528730652359d3dd
asyncedd <isynqquwu@proton.me>
2023-09-14 18:07:47 +0900

    feat(svelte): initialize language pack

---
+++ lua/plugins/specs/languages/svelte.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, {
+        "svelte",
+        "css",
+        "javascript",
+        "typescript",
+        "rust",
+      })
+    end,
+  },
+  {
+    "nvim-lspconfig",
+    optional = true,
+    opts = {
+      servers = {
+        svelte = {},
+        emmet_ls = {},
+        tailwindcss = {},
+      },
+    },
+  },
+  {
+    "null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if opts.sources.prettierd == nil then
+        opts.sources.prettierd = {}
+      end
+      if opts.sources.prettierd.extra_filetypes == nil then
+        opts.sources.prettierd = { extra_filetypes = {} }
+      end
+
+      table.insert(opts.sources.prettierd.extra_filetypes, "svelte")
+    end,
+  },
+}
