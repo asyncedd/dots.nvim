e187d4274a6741b16073cf2a98516060d7b8e816
asyncedd <isynqquwu@proton.me>
2023-09-18 09:02:20 +0900

    feat(mini.operators): initial commit

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -117,36 +117,6 @@ return {
     },
   },
   {
-    "gbprod/yanky.nvim",
-    dependencies = { { "kkharji/sqlite.lua", enabled = not jit.os:find("Windows") } },
-    opts = function()
-      return {
-        highlight = { timer = 200 },
-        ring = { storage = jit.os:find("Windows") and "shada" or "sqlite" },
-      }
-    end,
-    keys = {
-      -- stylua: ignore
-      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
-      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
-      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
-      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
-      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
-      { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle forward through yank history" },
-      { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle backward through yank history" },
-      { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
-      { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
-      { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
-      { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
-      { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
-      { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
-      { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
-      { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
-      { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
-      { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
-    },
-  },
-  {
     "kylechui/nvim-surround",
     opts = true,
     keys = {
@@ -208,6 +178,17 @@ return {
     keys = {
       { "i", mode = { "x", "o" } },
       { "a", mode = { "x", "o" } },
+    },
+  },
+  {
+    "echasnovski/mini.operators",
+    opts = true,
+    keys = {
+      { "g=", mode = { "x", "o", "n" }, desc = "Evaluate text and replace with output" },
+      { "gx", mode = { "x", "o", "n" }, desc = "Exchange text regions" },
+      { "gm", mode = { "x", "o", "n" }, desc = "Multiply (duplicate) text" },
+      { "gr", mode = { "x", "o", "n" }, desc = "Replace text with register" },
+      { "gs", mode = { "x", "o", "n" }, desc = "Sort text" },
     },
   },
 }
