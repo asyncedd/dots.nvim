4435588a1f60bcde24c2cd11634d484b61b9025c
asyncedd <isynqquwu@proton.me>
2023-09-23 20:35:42 +0900

    refactor: Remove mini.operators

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -134,24 +134,6 @@ return {
     event = "VeryLazy",
   },
   {
-    "gbprod/substitute.nvim",
-    opts = true,
-    keys = {
-      -- stylua: ignore start
-      { "gx", function(...) require("substitute").operator(...) end, desc = "Substitute: Operator" },
-      { "gxx", function(...) require("substitute").line(...) end, desc = "Substitute: Line" },
-      { "gX", function(...) require("substitute").eol(...) end, desc = "Substitute: EOL" },
-      { "X", function(...) require("substitute").visual(...) end, mode = "x", desc = "Substitute: Visual" },
-      { "<leader>x", function(...) require("substitute.range").operator(...) end, desc = "Substitute: Range-Operator" },
-      { "<leader>x", function(...) require("substitute.range").visual(...) end, mode = "x", desc = "Substitute: Range-Visual" },
-      { "zx", function(...) require("substitute.exchange").operator(...) end, desc = "Substitute: Exchange-Operator" },
-      { "zxx", function(...) require("substitute.exchange").line(...) end, desc = "Substitute: Exchange-Line" },
-      { "zX", function(...) require("substitute.exchange").visual(...) end, mode = "x", desc = "Substitute: Exchange-Visual" },
-      { "zxc", function() require("substitute.exchange").cancel() end, desc = "Substitute: Exchange-Cancel" },
-      -- stylua: ignore stop
-    },
-  },
-  {
     "echasnovski/mini.clue",
     opts = function()
       return require("plugins.configs.editor.mini-clue")
