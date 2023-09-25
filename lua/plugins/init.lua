6f1bad88bd891f345752fe7c7be695d7d9816909
asyncedd <isynqquwu@proton.me>
2023-09-25 17:05:34 +0900

    Revert "feat(hotspot): initial commit"
    This reverts commit 78cd4ac9e8df055679519f9fc6dbc072885f0ae6.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,23 +9,9 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
--- Bootstap hotpot into lazy plugin dir if it does not exist yet.
-local hotpotpath = vim.fn.stdpath("data") .. "/lazy/hotpot.nvim"
-if not vim.loop.fs_stat(hotpotpath) then
-  vim.notify("Bootstrapping hotpot.nvim...", vim.log.levels.INFO)
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/rktjmp/hotpot.nvim.git",
-    hotpotpath,
-  })
-end
-vim.opt.rtp:prepend({ lazypath, hotpotpath })
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "rktjmp/hotpot.nvim",
   { import = "plugins.specs" },
   dots.languages.enable,
   dots.UI.colorscheme.themes,
