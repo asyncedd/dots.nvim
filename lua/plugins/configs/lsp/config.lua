a61b4c792154014cf8e06ef1ee7e0aba7c57f7b0
asyncedd <isynqquwu@proton.me>
2023-10-09 18:19:38 +0900

    perf(LSP): Abstract the nvim-lspconfig config in their own modules

---
+++ lua/plugins/configs/lsp/config.lua
@@ -0,0 +1,86 @@
+return function(opts)
+  local inlay_hint = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
+
+  if inlay_hint then
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.server_capabilities.inlayHintProvider then
+          inlay_hint(bufnr, true)
+        end
+      end,
+    })
+  end
+
+  local servers = opts.servers
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+  capabilities.textDocument.completion.completionItem = {
+    documentationFormat = { "markdown", "plaintext" },
+    snippetSupport = true,
+    preselectSupport = true,
+    insertReplaceSupport = true,
+    labelDetailsSupport = true,
+    deprecatedSupport = true,
+    commitCharactersSupport = true,
+    tagSupport = { valueSet = { 1 } },
+    resolveSupport = {
+      properties = {
+        "documentation",
+        "detail",
+        "additionalTextEdits",
+      },
+    },
+  }
+
+  local setup = function(server)
+    local server_opts = vim.tbl_deep_extend("force", {
+      capabilities = vim.deepcopy(capabilities),
+    }, servers[server] or {})
+
+    if opts.setup[server] then
+      if opts.setup[server](server, server_opts) then
+        return
+      end
+    elseif opts.setup["*"] then
+      if opts.setup["*"](server, server_opts) then
+        return
+      end
+    end
+    if opts.servers[server] then
+      require("lspconfig")[server].setup(server_opts)
+    end
+  end
+
+  local have_mason, mlsp = pcall(require, "mason-lspconfig")
+  local all_mlsp_servers = {}
+  if have_mason then
+    all_mlsp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
+  end
+
+  local ensure_installed = {}
+  for server, server_opts in pairs(servers) do
+    if server_opts then
+      server_opts = server_opts == true and {} or server_opts
+      if server_opts.mason == false or not vim.tbl_contains(all_mlsp_servers, server) then
+        setup(server)
+      else
+        ensure_installed[#ensure_installed + 1] = server
+      end
+    end
+  end
+
+  if have_mason then
+    mlsp.setup({ ensure_installed = ensure_installed, handlers = { setup } })
+  end
+
+  vim.cmd("silent! do FileType")
+  vim.api.nvim_create_autocmd("WinEnter", {
+    group = vim.api.nvim_create_augroup("LSP_FileType_Reset", {}),
+    callback = function()
+      vim.api.nvim_del_augroup_by_name("LSP_FileType_Reset")
+      vim.cmd("silent! do FileType")
+    end,
+  })
+end
