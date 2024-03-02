a2b664da479110bc5484b01f9832073f616456a6
asyncedd <isynqquwu@proton.me>
2024-03-02 10:59:09 +0900

    feat(dressing): initial commit

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -132,4 +132,21 @@ return {
       },
     },
   },
+  -- CREDITS TO: https://www.lazyvim.org/plugins/ui#dressingnvim
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
 }
