0f4249641252962c3cfbd14e41a27ae94cc487f9
asyncedd <isynqquwu@proton.me>
2024-03-13 20:13:27 +0900

    fix(nvim-treesitter): rm unneeded init

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -11,16 +11,6 @@ return {
         enable = true,
       },
     },
-    -- CREDIT: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/treesitter.lua
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "syntax")
       dofile(vim.g.base46_cache .. "treesitter")
