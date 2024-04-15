6f2f6fd5e064161d45d1fee402b836a5b87a29b8
asyncedd <isynqquwu@proton.me>
2024-04-15 17:53:19 +0900

    fix(nvim-treesitter): fix hl

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -1,7 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    tag = "v0.9.2",
     opts = {
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
       auto_install = true,
