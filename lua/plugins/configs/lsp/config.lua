01818a3e5696741a0d2815aca0e8e32ffaad3f07
asyncedd <isynqquwu@proton.me>
2024-04-23 16:06:37 +0900

    fix lsp inlay hints

--- lua/plugins/configs/lsp/config.lua
+++ lua/plugins/configs/lsp/config.lua
@@ -4,10 +4,9 @@ return function(opts)
   if inlay_hint then
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(args)
-        local bufnr = args.buf
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if client and client.server_capabilities.inlayHintProvider then
-          inlay_hint.enable(bufnr, true)
+          inlay_hint.enable(true)
         end
       end,
     })
