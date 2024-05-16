95ce485c3490bbcf0ee7870651388a25e7e0e2da
asyncedd <isynqquwu@proton.me>
2024-05-16 10:57:28 +0900

    perf: use lazyvim optimizations for nvim-treesitter
    put mini.extras as a dependency of nvim-treesitter-textobjects

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -11,13 +11,37 @@ return {
         enable = true,
       },
     },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require("nvim-treesitter.query_predicates")
+    end,
+    build = ":TSUpdate",
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "syntax")
       dofile(vim.g.base46_cache .. "treesitter")
 
+      if type(opts.ensure_installed) == "table" then
+        ---@type table<string, boolean>
+        local added = {}
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          if added[lang] then
+            return false
+          end
+          added[lang] = true
+          return true
+        end, opts.ensure_installed)
+      end
       require("nvim-treesitter.configs").setup(opts)
+      vim.schedule(function()
+        require("lazy").load({ plugins = { "nvim-treesitter-textobjects" } })
+      end)
     end,
-    priority = 1000,
     event = "LazyFile",
   },
   {
@@ -118,11 +142,10 @@ return {
     dependencies = {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
-        init = function()
-          require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-        end,
+        dependencies = {
+          { "echasnovski/mini.extra", opts = true },
+        },
       },
-      { "echasnovski/mini.extra", opts = true },
     },
     keys = {
       { "i", mode = { "x", "o" } },
