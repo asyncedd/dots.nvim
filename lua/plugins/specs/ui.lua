59e443e30b100fff34a1adafce6965157c37b5cf
asyncedd <isynqquwu@proton.me>
2024-05-19 22:02:36 +0900

    perf: rm noice

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -59,40 +59,6 @@ return {
       end
     end,
   },
-  {
-    "folke/noice.nvim",
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = false,
-        lsp_doc_border = false,
-      },
-      cmdline = {
-        view = "cmdline",
-      },
-      views = {
-        mini = {
-          win_options = {
-            winblend = 0,
-          },
-        },
-      },
-    },
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-  },
   -- CREDITS TO: https://www.lazyvim.org/plugins/ui#dressingnvim
   {
     "stevearc/dressing.nvim",
