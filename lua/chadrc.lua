d76e4ec85ef31e4328b05fcb4386323dc579b0a5
asyncedd <isynqquwu@proton.me>
2024-06-12 17:49:50 +0900

    refactor(chadrc): refactor mini.git and mini.diff integrations

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,35 +2,6 @@
 local M = {}
 local utils = require("nvchad.stl.utils")
 
-local diff_format_summary = function(data)
-  local summary = vim.b[data.buf].minidiff_summary
-  if summary == nil then
-    vim.b[data.buf].minidiff_summary_string = ""
-    return
-  end
-  local t = {}
-  if summary.add ~= nil and summary.add > 0 then
-    table.insert(t, dots.UI.icons.Git.added .. summary.add)
-  end
-  if summary.change ~= nil and summary.change > 0 then
-    table.insert(t, dots.UI.icons.Git.changed .. summary.change)
-  end
-  if summary.delete ~= nil and summary.delete > 0 then
-    table.insert(t, dots.UI.icons.Git.remove .. summary.delete)
-  end
-  vim.b[data.buf].minidiff_summary_string = table.concat(t, " ")
-end
-vim.api.nvim_create_autocmd("User", { pattern = "MiniDiffUpdated", callback = diff_format_summary })
-
--- Use only HEAD name as summary string
-local git_format_summary = function(data)
-  -- Utilize buffer-local table summary
-  local summary = vim.b[data.buf].minigit_summary
-  vim.b[data.buf].minigit_summary_string = (summary ~= nil and summary.head_name) or ""
-end
-
-vim.api.nvim_create_autocmd("User", { pattern = "MiniGitUpdated", callback = git_format_summary })
-
 M.ui = {
   theme = "everblush",
   statusline = {
@@ -47,9 +18,17 @@ M.ui = {
       git = function()
         local git_status = vim.b[utils.stbufnr()].minigit_summary_string or ""
 
-        local branch_name = " " .. git_status
+        local summary = vim.b[utils.stbufnr()].minigit_summary
+        local branch_name = (summary ~= nil and summary.head_name) and (" " .. git_status) or ""
 
-        return "%#St_gitIcons# " .. branch_name .. " " .. (vim.b[utils.stbufnr()].minidiff_summary_string or "")
+        summary = vim.b.minidiff_summary
+        return "%#St_gitIcons# "
+          .. branch_name
+          .. (
+            summary ~= nil
+              and " " .. ((summary.add ~= nil and summary.add > 0) and dots.UI.icons.Git.added .. summary.add or "") .. ((summary.change ~= nil and summary.change > 0) and dots.UI.icons.Git.changed .. summary.change or "") .. ((summary.delete ~= nil and summary.delete > 0) and dots.UI.icons.Git.remove .. summary.delete or "")
+            or ""
+          )
       end,
     },
   },
