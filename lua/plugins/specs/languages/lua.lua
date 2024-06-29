bc88004bbc364e60369f1994321950b33d208d4c
asyncedd <isynqquwu@proton.me>
2024-06-29 14:37:26 +0900

    perf: potentially improve startup times for lazydev

--- lua/plugins/specs/languages/lua.lua
+++ lua/plugins/specs/languages/lua.lua
@@ -1,15 +1,26 @@
 return {
   {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-      },
-    },
-  },
-  {
     "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        group = vim.api.nvim_create_augroup("LoadLazyFile", { clear = true }),
+        pattern = "LazyFile",
+        callback = function()
+          local function load_lazydev()
+            if not require("lazy.core.config").plugins["lazydev.nvim"]._.loaded then
+              require("lazy").load({ plugins = "lazydev.nvim" })
+            end
+          end
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "*.lua" },
+            callback = load_lazydev,
+          })
+          if vim.bo.filetype == "lua" then
+            load_lazydev()
+          end
+        end,
+      })
+    end,
     opts = {
       library = {
         "ui/nvchad_types",
@@ -19,7 +30,6 @@ return {
     },
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-
   { -- optional completion source for require statements and module annotations
     "nvim-cmp",
     opts = function(_, opts)
